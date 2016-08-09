fbae6073ec7fa6612e81eb45cc1ef223418ad6a0
Thomas Wickham <twickham@octo.com>
2016-08-09 23:47:56 +0200

    Initial gh-pages release (5)

--- assets/pix-live.js
+++ assets/pix-live.js
@@ -7607,7 +7607,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"PIX_API_NAMESPACE":"api/live","LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"PIX_API_HOST":"http://localhost:4200","name":"pix-live","version":"0.0.0+8afe2afd"});
+  require("pix-live/app")["default"].create({"PIX_API_NAMESPACE":"api/live","LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"PIX_API_HOST":"http://localhost:4200","name":"pix-live","version":"0.0.0+5ee38628"});
 }
 
 /* jshint ignore:end */
