6942a808e07cdc90e5c92e462c99511e7f0fdc7c
Thomas Wickham <twickham@octo.com>
2016-08-09 23:12:54 +0200

    WIP

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'pix-live',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/pix-live/',
     locationType: 'auto',
     EmberENV: {
       // XXX because of a deprecation notice in the console
