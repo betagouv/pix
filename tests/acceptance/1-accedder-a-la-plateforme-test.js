7c1e03ee04537a1f0bb374ed9a9cf46fc1b6abc7
Thomas Wickham <twickham@octo.com>
2016-08-19 16:06:55 +0200

    add acceptance test + adapt template

---
+++ tests/acceptance/1-accedder-a-la-plateforme-test.js
@@ -0,0 +1,39 @@
+import {
+  describe,
+  it,
+  before,
+  after
+} from 'mocha';
+import { expect } from 'chai';
+import startApp from '../helpers/start-app';
+import destroyApp from '../helpers/destroy-app';
+
+describe('Acceptance | 1 - Accéder à la plateforme pour démarrer un test', function () {
+  let application;
+
+  before(function () {
+    application = startApp();
+  });
+
+  after(function () {
+    destroyApp(application);
+  });
+
+  before(function () {
+    return visit('/');
+  });
+
+  it('1.0 peut visiter /', function () {
+    expect(currentURL()).to.equal('/');
+  });
+
+  it('1.1 la landing page contient un pitch de présentation', function () {
+    expect(findWithAssert('.sales-pitch').text()).to.contains('PIX est un projet public de plateforme en ligne d\'évaluation');
+  });
+
+  it('1.3 la page d\'accueil contient un formulaire Nom / Prénom / Email et un bouton valider', function () {
+    expect(findWithAssert('#lastname').text()).to.contains('Nom');
+    expect(findWithAssert('#firstname').text()).to.contains('Prénom');
+    expect(findWithAssert('#email').text()).to.contains('Email');
+  })
+});
