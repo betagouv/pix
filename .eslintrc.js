e2df19c7409275e7d3a7055dc3b81cb75232e2df
Thomas Wickham <twickham@octo.com>
2016-08-19 14:55:03 +0200

    add eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 module.exports = {
   extends: [
-    require.resolve('ember-cli-eslint/coding-standard/ember-application.js')
+    require.resolve('ember-cli-eslint/coding-standard/ember-application.js'),
+    './.eslintrc'
   ]
 };
--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: '../node_modules/ember-cli-eslint/coding-standard/ember-testing.js'
+  extends: './.eslintrc'
 };
