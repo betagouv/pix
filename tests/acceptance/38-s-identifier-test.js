b1b520bffdc40ca8fe058e25de8115bfb3cb1d4c
Thomas Wickham <twickham@octo.com>
2016-08-23 15:43:45 +0200

    First draft of the user story

---
+++ tests/acceptance/38-s-identifier-test.js
@@ -0,0 +1,49 @@
+import {
+  describe,
+  it,
+  before,
+  beforeEach,
+  after
+} from 'mocha';
+import { expect } from 'chai';
+import startApp from '../helpers/start-app';
+import destroyApp from '../helpers/destroy-app';
+import RSVP from 'rsvp';
+
+describe("Acceptance | 38 - S'identifier sur la plateforme", function () {
+  let application;
+
+  before(function () {
+    application = startApp();
+  });
+
+  after(function () {
+    destroyApp(application);
+  });
+
+  beforeEach(function () {
+    return visit('/');
+  });
+
+  it("38.1 Depuis la page d'accueil, je saisie mon prénom + nom + email", function () {
+    return RSVP.all([
+      fillIn('#firstname_input', 'Jérémy'),
+      fillIn('#lastname_input', 'Buget'),
+      fillIn('#email_input', 'jbu@octo.com')
+
+    ]).then(() => {
+      expect(find('#firstname_input')[0].value).to.contains('Jérémy');
+      expect(find('#lastname_input')[0].value).to.eq('Buget');
+      expect(find('#email_input')[0].value).to.eq('jbu@octo.com');
+    });
+  });
+
+  it('38.2 Quand je valide mon identité, je suis redirigé vers la page des tests', function () {
+  });
+
+  it('38.3 Quand je suis identifié, je vois apparaître le libellé “Bonjour Prénom” (via session utilisateur)', function () {
+  });
+
+  it("38.4 En cas de champs manquant, un message d'erreur apparaît", function () {
+  });
+});
