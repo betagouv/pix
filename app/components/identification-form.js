d010c9f63142ff4745897843aa3f8ed6328b4c60
Thomas Wickham <twickham@octo.com>
2016-08-24 19:06:02 +0200

    hidde session information on index page

--- app/components/identification-form.js
+++ app/components/identification-form.js
@@ -5,6 +5,12 @@ export default Ember.Component.extend({
   routing: Ember.inject.service('-routing'),
   session: Ember.inject.service('session'),
 
+  init() {
+    this._super(...arguments);
+    this.set('session.isIdentified', false);
+    return this;
+  },
+
   actions: {
     identify() {
       this.set('session.isIdentified', true);
