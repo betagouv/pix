fcbe4ba16ee38454daf89c47a65fbcae3c8d1f1c
Thomas Wickham <twickham@octo.com>
2016-08-25 15:54:55 +0200

    refacto: Use page object in tests

--- tests/acceptance/38-s-identifier-test.js
+++ tests/acceptance/38-s-identifier-test.js
@@ -25,27 +25,43 @@ describe("Acceptance | 38 - S'identifier sur la plateforme", function () {
     return visit('/');
   });
 
+  let $firstname;
+  let $lastname;
+  let $email;
+  let $submit;
+  const $firstname_css = '.firstname_input';
+  const $lastname_css = '.lastname_input';
+  const $email_css = '.email_input';
+  const $submit_css = '.identification-form-actions button';
+
+  before(function () {
+    $firstname = findWithAssert($firstname_css)[0];
+    $lastname  = findWithAssert($lastname_css)[0];
+    $email     = findWithAssert($email_css)[0];
+    $submit    = findWithAssert($submit_css)[0];
+  });
+
   it("38.1 Depuis la page d'accueil, je saisie mon prénom + nom + email", function () {
     return RSVP.all([
-      fillIn('.firstname_input', 'Jérémy'),
-      fillIn('.lastname_input', 'Buget'),
-      fillIn('.email_input', 'jbu@octo.com')
+      fillIn($firstname_css, 'Jérémy'),
+      fillIn($lastname_css, 'Buget'),
+      fillIn($email_css, 'jbu@octo.com')
 
     ]).then(() => {
-      expect(find('.firstname_input')[0].value).to.contains('Jérémy');
-      expect(find('.lastname_input')[0].value).to.eq('Buget');
-      expect(find('.email_input')[0].value).to.eq('jbu@octo.com');
+      expect($firstname.value).to.contains('Jérémy');
+      expect($lastname.value).to.eq('Buget');
+      expect($email.value).to.eq('jbu@octo.com');
     });
   });
 
   it('38.2 Quand je valide mon identité, je suis redirigé vers la page des tests', function () {
     return RSVP.all([
-      fillIn('.firstname_input', 'Jérémy'),
-      fillIn('.lastname_input', 'Buget'),
-      fillIn('.email_input', 'jbu@octo.com')
+      fillIn($firstname_css, 'Jérémy'),
+      fillIn($lastname_css, 'Buget'),
+      fillIn($email_css, 'jbu@octo.com')
 
     ]).then(() => {
-      return click('.identification-form-actions button')
+      return click($submit_css)
 
     }).then(() => {
       expect(currentURL()).to.eq('/home');
@@ -58,7 +74,7 @@ describe("Acceptance | 38 - S'identifier sur la plateforme", function () {
     // Assert that 38.2 went OK
     expect(currentURL()).to.eq('/home');
 
-    expect(findWithAssert('.profile').text()).to.contains('Jérémy');
+    expect(findWithAssert('.profile').text()).to.contains('Bonjour Jérémy');
   });
 
   describe("38.4 En cas de champs manquant, un message d'erreur apparaît", function () {
@@ -66,15 +82,15 @@ describe("Acceptance | 38 - S'identifier sur la plateforme", function () {
       return visit('/')
         .then(() => {
           return RSVP.all([
-            fillIn('.firstname_input', 'Thomas'),
-            fillIn('.lastname_input', 'Wickham'),
-            fillIn('.email_input', 'twi@octo.com')
+            fillIn($firstname_css, 'Thomas'),
+            fillIn($lastname_css, 'Wickham'),
+            fillIn($email_css, 'twi@octo.com')
           ])
         });
     });
 
     it('missing firstname', function () {
-      return fillIn('.firstname_input', '')
+      return fillIn($firstname_css, '')
         .then(() => {
           expect(find('#firstname')[0].classList.contains('has-error')).to.be.true;
           expect(findWithAssert('#firstname .help-block').text()).to.contains('Champ requis');
@@ -82,7 +98,7 @@ describe("Acceptance | 38 - S'identifier sur la plateforme", function () {
     });
 
     it('missing lastname', function () {
-      return fillIn('.lastname_input', '')
+      return fillIn($lastname_css, '')
         .then(() => {
           expect(find('#lastname')[0].classList.contains('has-error')).to.be.true;
           expect(findWithAssert('#lastname .help-block').text()).to.contains('Champ requis');
@@ -90,7 +106,7 @@ describe("Acceptance | 38 - S'identifier sur la plateforme", function () {
     });
 
     it('missing email', function () {
-      return fillIn('.email_input', '')
+      return fillIn($email_css, '')
         .then(() => {
           expect(find('#email')[0].classList.contains('has-error')).to.be.true;
           expect(findWithAssert('#email .help-block').text()).to.contains('Champ requis');
@@ -98,7 +114,7 @@ describe("Acceptance | 38 - S'identifier sur la plateforme", function () {
     });
 
     it('bad email', function () {
-      return fillIn('.email_input', 'bad email')
+      return fillIn($email_css, 'bad email')
         .then(() => {
           expect(find('#email')[0].classList.contains('has-error')).to.be.true;
           expect(findWithAssert('#email .help-block').text()).to.contains('Entrez un email correct');
@@ -106,16 +122,16 @@ describe("Acceptance | 38 - S'identifier sur la plateforme", function () {
     });
 
     it("can't submit a form when an error is present", function () {
-      return fillIn('.firstname_input', '')
-        .then(() => expect(find('.identification-form-actions button')[0].disabled).to.be.true)
-        .then(() => click('.identification-form-actions button'))
+      return fillIn($firstname_css, '')
+        .then(() => expect(find($submit_css)[0].disabled).to.be.true)
+        .then(() => click($submit_css))
         .then(() => expect(currentURL()).to.eq('/'))
     });
 
     it("if the form is empty and the page has just been loaded, it can't submit the form", function () {
       return visit('/')
-        .then(() => { expect(find('.identification-form-actions button')[0].disabled).to.be.true })
-        .then(() => { click('.identification-form-actions button') })
+        .then(() => { expect(find($submit_css)[0] .disabled).to.be.true })
+        .then(() => { click($submit_css) })
         .then(() => { expect(currentURL()).to.eq('/') })
     })
 
