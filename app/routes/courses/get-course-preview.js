6a178651c35eedc0a0ffe768a88236ff773a17da
Jérémy Buget <jbuget@octo.com>
2016-08-25 23:02:51 +0200

    Add challenge-item omponenent (integration) tests

--- app/routes/courses/get-course-preview.js
+++ app/routes/courses/get-course-preview.js
@@ -1,15 +1,14 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
 
   model(params) {
-
-    const store = this.get('store');
-    const controller = this.controllerFor(this.routeName);
-
-    return store.findRecord('course', params.course_id).then((course) => {
-      controller.set('course', course);
-      controller.set('nextChallenge', course.get('challenges.firstObject'));
+    return this.get('store').findRecord('course', params.course_id).then((course) => {
+      return RSVP.hash({
+        course,
+        nextChallenge: course.get('challenges.firstObject')
+      });
     });
   }
 
--- tests/integration/components/challenge-item-test.js
+++ tests/integration/components/challenge-item-test.js
@@ -1,10 +1,7 @@
-/* jshint expr:true */
 import { expect } from 'chai';
-import {
-  describeComponent,
-  it
-} from 'ember-mocha';
+import { describeComponent, it } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 describeComponent(
   'challenge-item',
@@ -12,19 +9,39 @@ describeComponent(
   {
     integration: true
   },
-  function() {
-    it('renders', function() {
-      // Set any properties with this.set('myProperty', 'value');
-      // Handle any actions with this.on('myAction', function(val) { ... });
-      // Template block usage:
-      // this.render(hbs`
-      //   {{#challenge-item}}
-      //     template content
-      //   {{/challenge-item}}
-      // `);
+  function () {
 
-      this.render(hbs`{{challenge-item}}`);
-      expect(this.$()).to.have.length(1);
+    it('should render challenge instruction', function () {
+      // given
+      const challenge = Ember.Object.create({
+        instruction: 'My challenge instruction'
+      });
+      this.set('challenge', challenge);
+
+      // when
+      this.render(hbs`{{challenge-item challenge=challenge}}`);
+
+      // then
+      const $instruction = this.$('.challenge-instruction');
+      expect($instruction.text()).to.contains(challenge.get('instruction'));
+    });
+
+    it('should render challenge proposals', function () {
+      // given
+      const challenge = Ember.Object.create({
+        proposalsAsArray: ['Xi', 'Fu', 'Mi']
+      });
+      this.set('challenge', challenge);
+
+      // when
+      this.render(hbs`{{challenge-item challenge=challenge}}`);
+
+      // then
+      const $proposals = this.$('.challenge-proposal');
+      expect($proposals).to.have.lengthOf(3);
+      expect($proposals.eq(0).text()).to.contains('Xi');
+      expect($proposals.eq(1).text()).to.contains('Fu');
+      expect($proposals.eq(2).text()).to.contains('Mi');
     });
   }
 );
