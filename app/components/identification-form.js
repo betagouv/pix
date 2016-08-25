987bfc3fd44523b983cf66dcd239d8d29aba06a3
Thomas Wickham <twickham@octo.com>
2016-08-25 12:25:33 +0200

    Use a form lib + tests + usability

--- app/components/identification-form.js
+++ app/components/identification-form.js
@@ -4,10 +4,15 @@ export default Ember.Component.extend({
 
   routing: Ember.inject.service('-routing'),
   session: Ember.inject.service('session'),
+  user: null,
 
   init() {
     this._super(...arguments);
     this.set('session.isIdentified', false);
+    this.set('user', Ember.ObjectProxy.create({
+      content: this.get('session'),
+      errors: {}
+    }));
     return this;
   },
 
@@ -16,6 +21,20 @@ export default Ember.Component.extend({
       this.set('session.isIdentified', true);
       this.get('session').save();
       this.get('routing').transitionTo('home');
+      return false;
+    },
+
+    update(object, propertyPath, value) {
+      if (Ember.isEmpty(value)) {
+        this.set(`user.errors.${propertyPath}`, ['Champ requis']);
+        return;
+      } else {
+        this.set(`user.errors.${propertyPath}`, null);
+      }
+
+      if (propertyPath === 'email' && false === $('.email_input')[0].checkValidity()) {
+        this.set('user.errors.email', ['Entrez un email correct']);
+      }
     }
   }
 
--- tests/acceptance/38-s-identifier-test.js
+++ tests/acceptance/38-s-identifier-test.js
@@ -2,9 +2,10 @@ import {
   describe,
   it,
   before,
+  beforeEach,
   after
 } from 'mocha';
-import { expect } from 'chai';
+import {expect} from 'chai';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
 import RSVP from 'rsvp';
@@ -26,22 +27,22 @@ describe("Acceptance | 38 - S'identifier sur la plateforme", function () {
 
   it("38.1 Depuis la page d'accueil, je saisie mon prénom + nom + email", function () {
     return RSVP.all([
-      fillIn('#firstname_input', 'Jérémy'),
-      fillIn('#lastname_input', 'Buget'),
-      fillIn('#email_input', 'jbu@octo.com')
+      fillIn('.firstname_input', 'Jérémy'),
+      fillIn('.lastname_input', 'Buget'),
+      fillIn('.email_input', 'jbu@octo.com')
 
     ]).then(() => {
-      expect(find('#firstname_input')[0].value).to.contains('Jérémy');
-      expect(find('#lastname_input')[0].value).to.eq('Buget');
-      expect(find('#email_input')[0].value).to.eq('jbu@octo.com');
+      expect(find('.firstname_input')[0].value).to.contains('Jérémy');
+      expect(find('.lastname_input')[0].value).to.eq('Buget');
+      expect(find('.email_input')[0].value).to.eq('jbu@octo.com');
     });
   });
 
   it('38.2 Quand je valide mon identité, je suis redirigé vers la page des tests', function () {
     return RSVP.all([
-      fillIn('#firstname_input', 'Jérémy'),
-      fillIn('#lastname_input', 'Buget'),
-      fillIn('#email_input', 'jbu@octo.com')
+      fillIn('.firstname_input', 'Jérémy'),
+      fillIn('.lastname_input', 'Buget'),
+      fillIn('.email_input', 'jbu@octo.com')
 
     ]).then(() => {
       return click('.identification-form-actions button')
@@ -60,12 +61,50 @@ describe("Acceptance | 38 - S'identifier sur la plateforme", function () {
     expect(findWithAssert('.profile').text()).to.contains('Jérémy');
   });
 
-  it("38.4 En cas de champs manquant, un message d'erreur apparaît", function () {
-    return visit('/')
-      .then(() => {
-        expect(findWithAssert('#firstname_input').prop('required')).to.be.true;
-        expect(findWithAssert('#lastname_input').prop('required')).to.be.true;
-        expect(findWithAssert('#email_input').prop('required')).to.be.true;
-      });
+  describe("38.4 En cas de champs manquant, un message d'erreur apparaît", function () {
+    beforeEach(() => {
+      return visit('/')
+        .then(() => {
+          return RSVP.all([
+            fillIn('.firstname_input', 'Thomas'),
+            fillIn('.lastname_input', 'Wickham'),
+            fillIn('.email_input', 'twi@octo.com')
+          ])
+        });
+    });
+
+    it('missing firstname', function () {
+      return fillIn('.firstname_input', '')
+        .then(() => {
+          expect(find('#firstname')[0].classList.contains('has-error')).to.be.true;
+          expect(findWithAssert('#firstname .help-block').text()).to.contains('Champ requis');
+        });
+    });
+
+    it('missing lastname', function () {
+      return fillIn('.lastname_input', '')
+        .then(() => {
+          expect(find('#lastname')[0].classList.contains('has-error')).to.be.true;
+          expect(findWithAssert('#lastname .help-block').text()).to.contains('Champ requis');
+        });
+    });
+
+    it('missing email', function () {
+      return fillIn('.email_input', '')
+        .then(() => {
+          expect(find('#email')[0].classList.contains('has-error')).to.be.true;
+          expect(findWithAssert('#email .help-block').text()).to.contains('Champ requis');
+        });
+    });
+
+    it('bad email', function () {
+      return fillIn('.email_input', 'bad email')
+        .then(() => {
+          expect(find('#email')[0].classList.contains('has-error')).to.be.true;
+          expect(findWithAssert('#email .help-block').text()).to.contains('Entrez un email correct');
+        });
+
+    })
+
   });
 });
