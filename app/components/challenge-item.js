22eeb8b66b4271c7b34c63a3138a8789943dac8c
Jérémy Buget <jbuget@octo.com>
2016-08-26 11:13:35 +0200

    Remove useless comments

--- app/components/challenge-item.js
+++ app/components/challenge-item.js
@@ -2,18 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  /* inherited */
   tagName: 'article',
   classNames: ['challenge-item'],
   attributeBindings: ['challenge.id:data-challenge-id'],
 
-  /* common */
   mode: 'live',
   challenge: null,
   isLiveMode: Ember.computed.equal('mode', 'live'),
   isCoursePreviewMode: Ember.computed.equal('mode', 'course-preview'),
 
-  /* required properties when mode equals 'course-preview' */
   course: null,
   hasNextChallenge: Ember.computed('challenge', 'course', function () {
     const course = this.get('course');
