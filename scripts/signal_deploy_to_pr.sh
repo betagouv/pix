530a10c02fc939e0973e13bac1cb7eca23e937cd
Thomas Wickham <twickham@octo.com>
2016-08-30 16:11:39 +0200

    test the presence of env vars

--- scripts/signal_deploy_to_pr.sh
+++ scripts/signal_deploy_to_pr.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+[ -z $GITHUB_TOKEN ] && { echo 'FATAL: $GITHUB_TOKEN is absent !'; exit 1}
+[ -z $CIRCLE_PR_NUMBER ] && { echo 'FATAL: $CIRCLE_PR_NUMBER is absent !'; exit 1}
+[ -z $CIRCLE_BRANCH ] && { echo 'FATAL: $CIRCLE_BRANCH is absent !'; exit 1}
+
 curl \
 	-u twickham:$GITHUB_TOKEN \
 	-X POST "https://api.github.com/repos/sgmap/pix-live/issues/${CIRCLE_PR_NUMBER}/comments" \
