a58d0aac07e5cb7eaedb4df6909ee2e9089e210c
Thomas Wickham <twickham@octo.com>
2016-08-30 16:00:10 +0200

    fix circle deploy hook script

---
+++ scripts/signal_deploy_to_pr.sh
@@ -0,0 +1,7 @@
+#! /bin/env bash
+
+curl \
+	-u twickham:$GITHUB_TOKEN \
+	-X POST "https://api.github.com/repos/sgmap/pix-live/issues/${CIRCLE_PR_NUMBER}/comments" \
+	--data "{\"body\":\"Holly joly ! I've deployed this PR to http://${CIRCLE_BRANCH}.pix.beta.gouv.fr. Please check it out !\"}"\
+	--verbose
