003e7f1325d09b8f1559081e635cda358d5d65ce
David <davbohr@gmail.com>
2016-09-23 16:44:20 +0200

    displaying progress with promise

--- app/controllers/assessments/get-challenge.js
+++ app/controllers/assessments/get-challenge.js
@@ -4,6 +4,19 @@ export default Ember.Controller.extend({
 
   assessmentService: Ember.inject.service('assessment'),
 
+  currentChallenge: Ember.computed('model', function() {
+      let currentChallenge = this.get('model').challenge;
+      let promiseNumber = this.get('model').assessment
+        .get('course')
+        .then((course) => course.get('challenges'))
+        .then((challenges) => {
+          return { progress: challenges.indexOf(currentChallenge) + 1 };
+        });
+    return DS.PromiseObject.create({ promise: promiseNumber });
+  }),
+
+
+
   saveAnswerAndNavigate(currentChallenge, assessment, answerValue) {
 
     const answer = this._createAnswer(answerValue, currentChallenge, assessment);
