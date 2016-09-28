1cbe6169d4dfe0a788b8c761abefb7810fe88c73
David <davbohr@gmail.com>
2016-09-28 14:11:37 +0200

    cleaning code

--- app/components/challenge-item.js
+++ app/components/challenge-item.js
@@ -72,7 +72,12 @@ const ChallengeItem = Ember.Component.extend(Scrolling, {
   resetScroll: function() {
     this.unbindScrolling();
     this.bindScrolling();
-    window.scrollTo(0,0);
+    // window.scrollTo(0,0);
+    $(window).scrollTop(0, { text: 'blablabla' });
+    // $('window').trigger('scrollTop', [0, 'blablabla']);
+    // $('window').scrollTop.apply(0, 'eeeBlabla');
+    // var e = jQuery.Event( "scrollTop", { which: 'blablabla' } );
+    // $('window').trigger(e);
     Ember.$('body').removeClass('no-nav');
   },
 
--- app/mixins/scrolling.js
+++ app/mixins/scrolling.js
@@ -6,9 +6,17 @@ export default Ember.Mixin.create({
 
   bindScrolling: function(opts) {
 
-    const onScroll = (e, a) => {
-
-      if ($(window).scrollTop() === 0) {
+    const onScroll = (e) => {
+      // console.log('data.isInTestEnvironment');
+      // console.log(data.isInTestEnvironment);
+      // console.log(e.originalEvent[0].isInTestEnvironment);
+      console.log(e);
+      // if (e.originalEvent[0].isInTestEnvironment) {
+      //   return this.scrolled();
+      // }
+      if (this._isEventComeFromTestEnvironment(e)) {
+        return this.scrolled();
+      } else if (this._isTriggeredByTransition()) {
         return 0;
       } else {
         return this.scrolled();
@@ -16,8 +24,22 @@ export default Ember.Mixin.create({
     };
 
     $(document).bind('touchmove',  onScroll);
-    $(window).bind('scroll', onScroll);
+    $(window).bind('scroll', {data:'e'}, onScroll);
   },
+
+  _isScrollFiredByActualUser: function(e) {
+    return this._isEventComeFromTestEnvironment || this._isTriggeredOnEachTransition;
+  },
+
+  _isEventComeFromTestEnvironment: function(e) {
+    return (e && e.originalEvent && e.originalEvent[0] && e.originalEvent[0].isInTestEnvironment);
+  },
+
+  _isTriggeredByTransition: function() {
+    return $(window).scrollTop() === 0;
+  },
+
+
 
   unbindScrolling: function() {
     $(window).unbind('scroll');
