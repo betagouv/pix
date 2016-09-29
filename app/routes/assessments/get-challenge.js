4b51d9467af965eaffca7faf3c5876009ca9ced5
David <davbohr@gmail.com>
2016-09-29 12:06:19 +0200

    cleaning one controller, transferring responsibilities to route
     Please enter the commit message for your changes. Lines starting
    
    wip, do not work 2
    
    wip, do not work 3
    
    a first working version

--- app/routes/assessments/get-challenge.js
+++ app/routes/assessments/get-challenge.js
@@ -3,11 +3,42 @@ import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
 
+  assessmentService: Ember.inject.service('assessment'),
+
   model(params) {
     const store = this.get('store');
     return RSVP.hash({
       assessment: store.findRecord('assessment', params.assessment_id),
       challenge: store.findRecord('challenge', params.challenge_id)
+    });
+  },
+
+  actions : {
+
+    saveAnswerAndNavigate: function (currentChallenge, assessment, answerValue) {
+      const answer = this._createAnswer(answerValue, currentChallenge, assessment);
+      answer.save().then(() => {
+        this._navigateToNextView(currentChallenge, assessment);
+      });
+    }
+  },
+
+  _createAnswer: function (answerValue, currentChallenge, assessment) {
+
+    return this.get('store').createRecord('answer', {
+      value: answerValue,
+      challenge: currentChallenge,
+      assessment
+    });
+  },
+
+  _navigateToNextView: function (currentChallenge, assessment) {
+
+    this.get('assessmentService').getNextChallenge(currentChallenge, assessment).then((challenge) => {
+      if (challenge) {
+        return this.transitionTo('assessments.get-challenge', { challenge, assessment });
+      }
+      return this.transitionTo('assessments.get-results', { assessment });
     });
   },
 
