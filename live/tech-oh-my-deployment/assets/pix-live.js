7c644c79a093fca884339f60912e7b62e17addaa
Thomas Wickham <mackwic@gmail.com>
2016-10-05 12:43:55 +0000

    Release of tech-oh-my-deployment with env development (via commit hash: 83040397961d82e395314da8d3cf23dff977044b)

--- live/tech-oh-my-deployment/assets/pix-live.js
+++ live/tech-oh-my-deployment/assets/pix-live.js
@@ -8824,7 +8824,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"0.0.0+6edef399"});
+  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"0.0.0+83040397"});
 }
 
 /* jshint ignore:end */
