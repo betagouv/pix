e98d40695f701e7c1178c24e3b4e674e09781615
Thomas Wickham <mackwic@gmail.com>
2016-10-10 15:15:59 +0000

    Release of development with env development (via commit hash: 160c1cc676d3cedfb75dd233efeb9d25835a8a8a)

--- live/development/assets/pix-live.js
+++ live/development/assets/pix-live.js
@@ -27,7 +27,9 @@ define("pix-live/adapters/answer", ["exports", "pix-live/adapters/airtable"], fu
   });
 });
 define('pix-live/adapters/application', ['exports', 'ember-data/adapters/rest'], function (exports, _emberDataAdaptersRest) {
-  exports['default'] = _emberDataAdaptersRest['default'].extend({});
+  exports['default'] = _emberDataAdaptersRest['default'].extend({
+    host: EmberENV.apiHost.current
+  });
 });
 define("pix-live/adapters/assessment", ["exports", "pix-live/adapters/airtable"], function (exports, _pixLiveAdaptersAirtable) {
   exports["default"] = _pixLiveAdaptersAirtable["default"].extend({
@@ -58,6 +60,14 @@ define('pix-live/adapters/course', ['exports', 'pix-live/adapters/airtable'], fu
       var url = this._super.apply(this, arguments);
       return url + '?view=' + encodeURIComponent('PIX view');
     }
+  });
+});
+define('pix-live/adapters/user', ['exports', 'ember-data'], function (exports, _emberData) {
+  exports['default'] = _emberData['default'].RESTAdapter.extend({
+
+    namespace: 'api',
+    host: EmberENV.apiHost.current
+
   });
 });
 define('pix-live/app', ['exports', 'ember', 'pix-live/resolver', 'ember-load-initializers', 'pix-live/config/environment'], function (exports, _ember, _pixLiveResolver, _emberLoadInitializers, _pixLiveConfigEnvironment) {
@@ -866,6 +876,20 @@ define('pix-live/initializers/ember-data', ['exports', 'ember-data/setup-contain
     initialize: _emberDataSetupContainer['default']
   };
 });
+define('pix-live/initializers/enable-sentry', ['exports'], function (exports) {
+  exports.initialize = initialize;
+
+  function initialize() {
+    if (EmberENV.environment) {
+      Raven.config('https://4b60c9f39a844832956f840b9d0d1359@sentry.io/99479').install();
+    }
+  }
+
+  exports['default'] = {
+    name: 'enable-sentry',
+    initialize: initialize
+  };
+});
 define('pix-live/initializers/export-application-global', ['exports', 'ember', 'pix-live/config/environment'], function (exports, _ember, _pixLiveConfigEnvironment) {
   exports.initialize = initialize;
 
@@ -897,6 +921,32 @@ define('pix-live/initializers/export-application-global', ['exports', 'ember', '
   exports['default'] = {
     name: 'export-application-global',
 
+    initialize: initialize
+  };
+});
+define('pix-live/initializers/infer-api-host', ['exports'], function (exports) {
+  exports.inferApiHost = inferApiHost;
+  exports.initialize = initialize;
+
+  function inferApiHost(locationObject) {
+
+    if (/localhost/.test(locationObject.hostname)) return 'http://' + EmberENV.apiHost.localhost;
+
+    if ('pix.beta.gouv.fr' === locationObject.hostname) return 'https://api-prod.' + EmberENV.apiHost.pix;
+
+    if ('development.pix.beta.gouv.fr' === locationObject.hostname) return 'http://api-development.' + EmberENV.apiHost.pix;
+
+    var matches = /^(.*).pix.beta.gouv.fr/.exec(locationObject.hostname);
+    return 'http://' + matches[1] + '.' + EmberENV.apiHost.pix;
+  }
+
+  function initialize() {
+
+    EmberENV.apiHost.current = inferApiHost(window.location);
+  }
+
+  exports['default'] = {
+    name: 'infer-api-host',
     initialize: initialize
   };
 });
@@ -969,13 +1019,7 @@ define("pix-live/instance-initializers/ember-data", ["exports", "ember-data/-pri
 define('pix-live/mirage/config', ['exports'], function (exports) {
   exports.testConfig = testConfig;
 
-  exports['default'] = function () {
-    this.passthrough('https://api.airtable.com/**');
-
-    this.post('https://sentry.io/**', function () {
-      return 200;
-    });
-  };
+  exports['default'] = function () {};
 
   function testConfig() {
 
@@ -1315,6 +1359,16 @@ define('pix-live/models/course', ['exports', 'ember-data'], function (exports, _
     }
   });
 });
+define('pix-live/models/user', ['exports', 'ember-data'], function (exports, _emberData) {
+  var attr = _emberData['default'].attr;
+  exports['default'] = _emberData['default'].Model.extend({
+    firstName: attr('string'),
+    lastName: attr('string'),
+    email: attr('string'),
+    login: attr('string'),
+    password: attr('string')
+  });
+});
 define('pix-live/resolver', ['exports', 'ember-resolver'], function (exports, _emberResolver) {
   exports['default'] = _emberResolver['default'];
 });
@@ -1326,7 +1380,6 @@ define('pix-live/router', ['exports', 'ember', 'pix-live/config/environment'], f
   });
 
   exports['default'] = Router.map(function () {
-
     this.route('index', { path: '/' });
     this.route('home');
     this.route('preferences');
@@ -1339,6 +1392,7 @@ define('pix-live/router', ['exports', 'ember', 'pix-live/config/environment'], f
 
     this.route('assessments.get-challenge', { path: '/assessments/:assessment_id/challenges/:challenge_id' });
     this.route('assessments.get-results', { path: '/assessments/:assessment_id/results' });
+    this.route('secret-yo');
   });
 });
 define('pix-live/routes/assessments/get-challenge', ['exports', 'ember', 'rsvp', 'ember-data'], function (exports, _ember, _rsvp, _emberData) {
@@ -1545,6 +1599,13 @@ define('pix-live/routes/index', ['exports', 'ember'], function (exports, _ember)
 define('pix-live/routes/preferences', ['exports', 'ember'], function (exports, _ember) {
   exports['default'] = _ember['default'].Route.extend({});
 });
+define('pix-live/routes/secret-yo', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Route.extend({
+    model: function model() {
+      return this.store.findAll('user');
+    }
+  });
+});
 define('pix-live/serializers/airtable-serializer', ['exports', 'ember', 'ember-data'], function (exports, _ember, _emberData) {
 
   var inflector = _ember['default'].Inflector.inflector;
@@ -1692,6 +1753,13 @@ define('pix-live/serializers/course', ['exports', 'ember', 'pix-live/serializers
       return result;
     }
 
+  });
+});
+define('pix-live/serializers/user', ['exports', 'ember-data'], function (exports, _emberData) {
+  exports['default'] = _emberData['default'].RESTSerializer.extend({
+    keyForAttribute: function keyForAttribute(attr) {
+      return Ember.String.underscore(attr);
+    }
   });
 });
 define('pix-live/services/ajax', ['exports', 'ember-ajax/services/ajax'], function (exports, _emberAjaxServicesAjax) {
@@ -8214,6 +8282,109 @@ define("pix-live/templates/preferences", ["exports"], function (exports) {
     };
   })());
 });
+define("pix-live/templates/secret-yo", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template((function () {
+    var child0 = (function () {
+      return {
+        meta: {
+          "revision": "Ember@2.8.2",
+          "loc": {
+            "source": null,
+            "start": {
+              "line": 1,
+              "column": 0
+            },
+            "end": {
+              "line": 6,
+              "column": 0
+            }
+          },
+          "moduleName": "pix-live/templates/secret-yo.hbs"
+        },
+        isEmpty: false,
+        arity: 1,
+        cachedFragment: null,
+        hasRendered: false,
+        buildFragment: function buildFragment(dom) {
+          var el0 = dom.createDocumentFragment();
+          var el1 = dom.createTextNode("    ");
+          dom.appendChild(el0, el1);
+          var el1 = dom.createElement("div");
+          dom.setAttribute(el1, "class", "container");
+          var el2 = dom.createTextNode("\n        ");
+          dom.appendChild(el1, el2);
+          var el2 = dom.createElement("h3");
+          var el3 = dom.createComment("");
+          dom.appendChild(el2, el3);
+          var el3 = dom.createTextNode(" ");
+          dom.appendChild(el2, el3);
+          var el3 = dom.createComment("");
+          dom.appendChild(el2, el3);
+          dom.appendChild(el1, el2);
+          var el2 = dom.createTextNode("\n        email: ");
+          dom.appendChild(el1, el2);
+          var el2 = dom.createComment("");
+          dom.appendChild(el1, el2);
+          var el2 = dom.createTextNode("\n    ");
+          dom.appendChild(el1, el2);
+          dom.appendChild(el0, el1);
+          var el1 = dom.createTextNode("\n");
+          dom.appendChild(el0, el1);
+          return el0;
+        },
+        buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+          var element0 = dom.childAt(fragment, [1]);
+          var element1 = dom.childAt(element0, [1]);
+          var morphs = new Array(3);
+          morphs[0] = dom.createMorphAt(element1, 0, 0);
+          morphs[1] = dom.createMorphAt(element1, 2, 2);
+          morphs[2] = dom.createMorphAt(element0, 3, 3);
+          return morphs;
+        },
+        statements: [["content", "user.firstName", ["loc", [null, [3, 12], [3, 30]]], 0, 0, 0, 0], ["content", "user.lastName", ["loc", [null, [3, 31], [3, 48]]], 0, 0, 0, 0], ["content", "user.email", ["loc", [null, [4, 15], [4, 31]]], 0, 0, 0, 0]],
+        locals: ["user"],
+        templates: []
+      };
+    })();
+    return {
+      meta: {
+        "revision": "Ember@2.8.2",
+        "loc": {
+          "source": null,
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 7,
+            "column": 0
+          }
+        },
+        "moduleName": "pix-live/templates/secret-yo.hbs"
+      },
+      isEmpty: false,
+      arity: 0,
+      cachedFragment: null,
+      hasRendered: false,
+      buildFragment: function buildFragment(dom) {
+        var el0 = dom.createDocumentFragment();
+        var el1 = dom.createComment("");
+        dom.appendChild(el0, el1);
+        return el0;
+      },
+      buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+        var morphs = new Array(1);
+        morphs[0] = dom.createMorphAt(fragment, 0, 0, contextualElement);
+        dom.insertBoundary(fragment, 0);
+        dom.insertBoundary(fragment, null);
+        return morphs;
+      },
+      statements: [["block", "each", [["get", "model", ["loc", [null, [1, 8], [1, 13]]], 0, 0, 0, 0]], [], 0, null, ["loc", [null, [1, 0], [6, 9]]]]],
+      locals: [],
+      templates: [child0]
+    };
+  })());
+});
 define('pix-live/tests/mirage/mirage/config.lint-test', ['exports'], function (exports) {
   describe('ESLint - mirage/config.js', function () {
     it('should pass ESLint', function () {
@@ -8410,7 +8581,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"1.0.0+0379ab96"});
+  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"1.0.0+160c1cc6"});
 }
 
 /* jshint ignore:end */
