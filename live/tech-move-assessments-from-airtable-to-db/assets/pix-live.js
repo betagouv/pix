5eeb3ee18421e009f8460ba5286cc3f30aebc74c
Thomas Wickham <mackwic@gmail.com>
2016-10-10 19:53:20 +0000

    Release of tech-move-assessments-from-airtable-to-db with env development (via commit hash: ae98010fdc2cb58bc132608be65e8625e79f6b1a)

--- live/tech-move-assessments-from-airtable-to-db/assets/pix-live.js
+++ live/tech-move-assessments-from-airtable-to-db/assets/pix-live.js
@@ -896,6 +896,18 @@ define('pix-live/initializers/export-application-global', ['exports', 'ember', '
   function initialize() {
     var application = arguments[1] || arguments[0];
     if (_pixLiveConfigEnvironment['default'].exportApplicationGlobal !== false) {
+      var theGlobal;
+      if (typeof window !== 'undefined') {
+        theGlobal = window;
+      } else if (typeof global !== 'undefined') {
+        theGlobal = global;
+      } else if (typeof self !== 'undefined') {
+        theGlobal = self;
+      } else {
+        // no reasonable global, just bail
+        return;
+      }
+
       var value = _pixLiveConfigEnvironment['default'].exportApplicationGlobal;
       var globalName;
 
@@ -905,13 +917,13 @@ define('pix-live/initializers/export-application-global', ['exports', 'ember', '
         globalName = _ember['default'].String.classify(_pixLiveConfigEnvironment['default'].modulePrefix);
       }
 
-      if (!window[globalName]) {
-        window[globalName] = application;
+      if (!theGlobal[globalName]) {
+        theGlobal[globalName] = application;
 
         application.reopen({
           willDestroy: function willDestroy() {
             this._super.apply(this, arguments);
-            delete window[globalName];
+            delete theGlobal[globalName];
           }
         });
       }
@@ -4664,12 +4676,13 @@ define("pix-live/templates/components/challenge-item", ["exports"], function (ex
         },
         buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
           var element5 = dom.childAt(fragment, [3, 1]);
-          var morphs = new Array(2);
+          var morphs = new Array(3);
           morphs[0] = dom.createAttrMorph(element5, 'href');
-          morphs[1] = dom.createMorphAt(element5, 5, 5);
+          morphs[1] = dom.createAttrMorph(element5, 'download');
+          morphs[2] = dom.createMorphAt(element5, 5, 5);
           return morphs;
         },
-        statements: [["attribute", "href", ["concat", [["get", "challenge.attachmentUrl", ["loc", [null, [16, 21], [16, 44]]], 0, 0, 0, 0]], 0, 0, 0, 0, 0], 0, 0, 0, 0], ["content", "challenge.attachmentFilename", ["loc", [null, [19, 12], [19, 44]]], 0, 0, 0, 0]],
+        statements: [["attribute", "href", ["concat", [["get", "challenge.attachmentUrl", ["loc", [null, [16, 21], [16, 44]]], 0, 0, 0, 0]], 0, 0, 0, 0, 0], 0, 0, 0, 0], ["attribute", "download", ["concat", [["get", "challenge.attachmentFilename", ["loc", [null, [16, 91], [16, 119]]], 0, 0, 0, 0]], 0, 0, 0, 0, 0], 0, 0, 0, 0], ["content", "challenge.attachmentFilename", ["loc", [null, [19, 12], [19, 44]]], 0, 0, 0, 0]],
         locals: [],
         templates: []
       };
@@ -8577,7 +8590,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"1.0.0+dabc18d1"});
+  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"1.0.0+ae98010f"});
 }
 
 /* jshint ignore:end */
