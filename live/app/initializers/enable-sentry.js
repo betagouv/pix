0f4e15d9149783cfade6dea4c6686dbcacb3796c
Thomas Wickham <twickham@octo.com>
2016-10-10 13:52:21 +0200

    Sentry: enable with emberENV.environment rather than window.location

--- live/app/initializers/enable-sentry.js
+++ live/app/initializers/enable-sentry.js
@@ -1,5 +1,5 @@
 export function initialize() {
-  if (window.location.hostname === 'pix.beta.gouv.fr') {
+  if (EmberENV.environment) {
     Raven.config('https://4b60c9f39a844832956f840b9d0d1359@sentry.io/99479').install();
   }
 }
