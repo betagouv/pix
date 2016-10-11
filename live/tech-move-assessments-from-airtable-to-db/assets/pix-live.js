f52afa4a804a62c277b636a7fa11de7b516b2095
Thomas Wickham <mackwic@gmail.com>
2016-10-11 22:18:31 +0000

    Release of tech-move-assessments-from-airtable-to-db with env development (via commit hash: e44257144c7369ba8afd67eda267043946ad1c47)

--- live/tech-move-assessments-from-airtable-to-db/assets/pix-live.js
+++ live/tech-move-assessments-from-airtable-to-db/assets/pix-live.js
@@ -8590,7 +8590,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"1.0.0+388d6a4e"});
+  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"1.0.0+e4425714"});
 }
 
 /* jshint ignore:end */
