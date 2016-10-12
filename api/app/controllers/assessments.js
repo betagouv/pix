33dee0f71b4f371207bbe53a69c884e52f420fba
David <davbohr@gmail.com>
2016-10-12 11:59:47 +0200

    post assessment ok

--- api/app/controllers/assessments.js
+++ api/app/controllers/assessments.js
@@ -4,13 +4,10 @@ module.exports = {
 
   save: {
     handler: (request, reply) => {
-      const response = reply({});
-      response.statusCode = 201;
-      return response;
-      // const newAssessment = new Assessment();
-      // newAssessment.save(function () {
-      //   return reply({}).code(201);
-      // });
+      const newAssessment = new Assessment();
+      newAssessment.save().then(function () {
+        return reply({}).code(201);
+      });
     }
   }
 
