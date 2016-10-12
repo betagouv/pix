c2680202c6b6e73cd4efeffca8de00ca87d05a7a
David <davbohr@gmail.com>
2016-10-12 16:19:39 +0200

    test assessment clean code

--- api/app/controllers/assessments.js
+++ api/app/controllers/assessments.js
@@ -4,9 +4,10 @@ module.exports = {
 
   save: {
     handler: (request, reply) => {
-      const newAssessment = new Assessment();
-      newAssessment.set('userId', request.payload.userId);
-      newAssessment.set('courseId', request.payload.courseId);
+      const newAssessment = new Assessment({
+        userId: request.payload.userId,
+        courseId: request.payload.courseId
+      });
       newAssessment.save().then(function (assessment) {
         return reply({assessment}).code(201);
       });
