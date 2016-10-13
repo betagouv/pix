e331c63254ec541351c44dd15fed73884eed204d
Jérémy Buget <jbuget@octo.com>
2016-10-14 01:22:13 +0200

    BSR : remove useless greetings API

--- api/config/routes.js
+++ api/config/routes.js
@@ -1,14 +1,10 @@
 const server = require('../server');
-const Greetings = require('../app/controllers/greetings');
 const Assessments = require('../app/controllers/assessments');
 const Users = require('../app/controllers/users');
 const Courses = require('../app/controllers/courses');
 const Challenges = require('../app/controllers/challenges');
 
 module.exports = [
-
-  { method: 'GET',  path: '/', config: Greetings.world },
-  { method: 'GET',  path: '/{name}', config: Greetings.buddy },
 
   { method: 'GET',  path: '/api/users', config: Users.list },
   { method: 'GET',  path: '/api/users/{id}', config: Users.get },
