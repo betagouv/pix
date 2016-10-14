619c2575310f7bd8a4bef1d9723ae06147af9bdf
Thomas Wickham <mackwic@gmail.com>
2016-10-14 15:06:23 +0000

    Release of 191-setup-archi-v2 with env development (via commit hash: d7912b5d2304df73a5489e0e91c1cb8baeaef2b1)

--- live/191-setup-archi-v2/assets/pix-live.js
+++ live/191-setup-archi-v2/assets/pix-live.js
@@ -8508,7 +8508,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"1.0.0+3dc9520c"});
+  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"1.0.0+d7912b5d"});
 }
 
 /* jshint ignore:end */
