cba43721fd4e09989be1c4f641a0caddf0ab7ea7
Jérémy Buget <jbuget@octo.com>
2016-10-17 13:04:50 +0200

    Fix new course start

--- live/app/routes/courses/create-assessment.js
+++ live/app/routes/courses/create-assessment.js
@@ -5,28 +5,24 @@ export default Ember.Route.extend({
 
   session: Ember.inject.service(),
 
-  _getAssessmentFirstChallenge(assessment) {
-    return assessment.get('course').then()
-  },
-
   model(params) {
 
     const store = this.get('store');
     return store.findRecord('course', params.course_id).then((course) => {
 
       // FIXME : add (route?) tests
-      const userName = `${this.get('session.firstname')} ${this.get('session.lastname')}`;
-      const userEmail = this.get('session.email');
+      const userName = `${this.get('session.user.firstName')} ${this.get('session.user.lastName')}`;
+      const userEmail = this.get('session.user.email');
 
-      const assessment = store
+      return store
         .createRecord('assessment', { course, userId: 1, userName, userEmail })
         .save()
-        .then(() => this._getAssessmentFirstChallenge);
-
-      return RSVP.hash({
-        assessment: assessment.save(),
-        challenge: course.get('challenges.firstObject')
-      });
+        .then((assessment) => {
+          return RSVP.hash({
+            assessment: assessment,
+            challenge: assessment.get('firstChallenge')
+          });
+        });
     });
   },
 
