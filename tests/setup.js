b7b7625a13f7f54b1c830510feed0955b3d9db5a
Jérémy Buget <jbuget@octo.com>
2016-10-17 10:15:37 +0200

    Clean bad files in repository's root

--- tests/setup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Global variables used int tests.
-global.nock = require('nock');
-nock.disableNetConnect();
-
-global.sinon = require('sinon');
-global.chai = require('chai');
-global.expect = global.chai.expect;
