6a80dfde654d83cddf3889a4a23c7fd5225f8868
David <davbohr@gmail.com>
2016-10-18 15:09:10 +0200

    acceptance 27 ok

--- live/mirage/data/challenges/qcu-challenge-with-attachment.js
+++ live/mirage/data/challenges/qcu-challenge-with-attachment.js
@@ -1,9 +1,11 @@
 export default  {
   data: {
     type: 'challenge',
-    id: 'qcu_challenge_without_image_id',
+    id: 'qcu_challenge_with_attachment_id',
     attributes: {
       type: 'QCU',
+      'attachment-url': 'http://example_of_url',
+      'attachment-filename': 'example_of_filename.pdf',
       instruction: "Julie a déposé un document dans un espace de stockage partagé avec Pierre. Elle lui envoie un mail pour l’en informer. Quel est le meilleur message ?",
       proposals: "" +
       "- J’ai déposé le document ici : P: > Equipe > Communication > Textes > intro.odt\n " +
--- live/mirage/routes/get-challenge.js
+++ live/mirage/routes/get-challenge.js
@@ -1,4 +1,5 @@
 import qcuChallengeWithImage from '../data/challenges/qcu-challenge-with-image';
+import qcuChallengeWithAttachment from '../data/challenges/qcu-challenge-with-attachment';
 import qcuChallenge from '../data/challenges/qcu-challenge';
 import qcmChallenge from '../data/challenges/qcm-challenge';
 import qrocmChallenge from '../data/challenges/qrocm-challenge';
@@ -13,6 +14,8 @@ export default function (schema, request) {
       return qcmChallenge;    
     case 'qcu_challenge_with_image_id':
       return qcuChallengeWithImage;
+    case 'qcu_challenge_with_attachment_id':
+      return qcuChallengeWithAttachment;
     case 'challenge_qcu_id':
     default:
       return qcuChallenge;
