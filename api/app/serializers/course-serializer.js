80e850e77e6896ddbdcd555f028c48e777041f01
Jérémy Buget <jbuget@octo.com>
2016-10-18 11:12:26 +0200

    Convert camelCase data into dashed data in API

--- api/app/serializers/course-serializer.js
+++ api/app/serializers/course-serializer.js
@@ -9,9 +9,13 @@ class CourseSerializer extends JSONAPISerializer {
   }
 
   serializeAttributes(model, data) {
-    data.attributes.name = model.name;
-    data.attributes.description = model.description;
-    data.attributes.duration = model.duration;
+    data.attributes["name"] = model.name;
+    data.attributes["description"] = model.description;
+    data.attributes["duration"] = model.duration;
+
+    if (model.imageUrl) {
+      data.attributes['image-url'] = model.imageUrl;
+    }
   }
 
   serializeRelationships(model, data) {
@@ -23,8 +27,8 @@ class CourseSerializer extends JSONAPISerializer {
       };
       for (let challengeId of model.challenges) {
         data.relationships.challenges.data.push({
-          type: 'challenge',
-          id: challengeId
+          "type": 'challenge',
+          "id": challengeId
         });
       }
     }
