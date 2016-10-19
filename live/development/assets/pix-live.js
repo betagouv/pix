5ae590de41b03157207702dc9064861ef4e087e9
Thomas Wickham <mackwic@gmail.com>
2016-10-19 18:54:47 +0000

    Release of development with env development (via commit hash: 0bffe42587dcf7d99d0517029468e12a5d53c5e3)

--- live/development/assets/pix-live.js
+++ live/development/assets/pix-live.js
@@ -8584,7 +8584,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"1.0.0+6ee5d8a6"});
+  require("pix-live/app")["default"].create({"LOG_RESOLVER":false,"LOG_ACTIVE_GENERATION":false,"LOG_TRANSITIONS":false,"LOG_TRANSITIONS_INTERNAL":false,"LOG_VIEW_LOOKUPS":false,"name":"pix-live","version":"1.0.0+0bffe425"});
 }
 
 /* jshint ignore:end */
