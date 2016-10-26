fc6a361c035f59c9af6599490802aae432fee6b7
David <davbohr@gmail.com>
2016-10-26 17:55:05 +0200

    simplification & debug of serializer

--- api/app/controllers/assessments.js
+++ api/app/controllers/assessments.js
@@ -44,35 +44,9 @@ module.exports = {
  get: {
     handler: (request, reply) => {
 
-       new Assessment({ id: request.params.id }).fetch().then((assessment) => {
-        Answer.query('where', 'assessmentId', '=', assessment.id).fetchAll().then((allAnswers) => {
-          
-          let serializedAnswers = [];
-          allAnswers.forEach(function (oneAnswer) {
-            serializedAnswers.push(
-
-                  {
-                    "type":"answers",
-                    "id":oneAnswer.attributes.id
-                  }
-                
-            );   
-          });
-
-          let serializedAssessment = assessmentSerializer.serialize(assessment);
-          if (!serializedAssessment.data.relationships) {
-            serializedAssessment.data.relationships = {};
-          }
-          if (!serializedAssessment.data.relationships.answers) {
-            serializedAssessment.data.relationships.answers = {};
-          }
-          if (!serializedAssessment.data.relationships.answers.data) {
-            serializedAssessment.data.relationships.answers.data = {};
-          }
-          serializedAssessment.data.relationships.answers.data = serializedAnswers;
-          reply(serializedAssessment);
-
-        });
+       new Assessment({ id: request.params.id }).fetch({ withRelated: ['answers'] }).then((assessment) => {
+        let serializedAssessment = assessmentSerializer.serialize(assessment);
+        reply(serializedAssessment);
       });
 
 
