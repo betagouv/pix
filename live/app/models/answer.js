e92f73107a05756e157100ddc76af504b8821f4e
David <davbohr@gmail.com>
2016-10-26 11:43:53 +0200

    code ok, no test yet

--- live/app/models/answer.js
+++ live/app/models/answer.js
@@ -1,11 +1,23 @@
 import DS from 'ember-data';
 
 const { Model, attr, belongsTo } = DS;
+const { computed } = Ember;
 
 export default Model.extend({
 
   value: attr('string'),
+  result: attr('string'),
   assessment: belongsTo('assessment'),
-  challenge: belongsTo('challenge')
+  challenge: belongsTo('challenge'),
+  
+  isPending: computed('result', function () {
+    return this.get('result') === 'pending'
+  }),
+  isOk: computed('result', function () {
+    return this.get('result') === 'ok'
+  }),
+  isNotOk: computed('result', function () {
+    return this.get('result') === 'ko'
+  })
 
 });
