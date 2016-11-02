5ce5eb6a5aeaaec003f596db2eca289bd7208336
Jill-Jênn Vie <vie@jill-jenn.net>
2016-11-02 16:56:59 +0100

    Add mirage stubs for QCM

--- live/tests/acceptance/196-validation-automatique-des-qcm-test.js
+++ live/tests/acceptance/196-validation-automatique-des-qcm-test.js
@@ -23,19 +23,21 @@ describe("Acceptance | 196 - Validation automatique d'un QCM, visualisation du r
   });
 
   before(function() {
-    return visit(`/assessments/completed_assessment_id/results`);
+    return visit(`/assessments/completed_assessment_qcm_id/results`);
   });
 
   before(function () {
     $summary = findWithAssert('.table#summary tbody tr');
   });
 
-  it("197.1. Pour un QCM avec toutes les réponses attendues, le tableau récapitulatif donne une indication que la réponse est correcte", function () {
-    expect($summary.text()).to.contains('réponse correcte');
+  it("196.1. Pour un QCM avec toutes les réponses attendues, le tableau récapitulatif donne une indication que la réponse est correcte", function () {
+    let $cell = findWithAssert('.table#summary tbody tr:nth-child(1) td:nth-child(3)');
+    expect($cell.text()).to.contains('réponse correcte');
   });
 
-  it("195.2. Pour un QCM avec une mauvaise réponse, le tableau récapitulatif donne une indication que la réponse est incorrecte", function () {
-    expect($summary.text()).to.contains('réponse incorrecte');
+  it("196.2. Pour un QCM avec une mauvaise réponse, le tableau récapitulatif donne une indication que la réponse est incorrecte", function () {
+    let $cell = findWithAssert('.table#summary tbody tr:nth-child(2) td:nth-child(3)');
+    expect($cell.text()).to.contains('réponse incorrecte');
   });
 
 });
