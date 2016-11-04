69ffd4d4c0512a7f970f73cca246557dc7dd427c
David <davbohr@gmail.com>
2016-11-04 15:08:02 +0100

    a tested api, ready for auth

---
+++ live/app/authenticators/jwt.js
@@ -0,0 +1,59 @@
+// taken from http://www.thegreatcodeadventure.com/jwt-authentication-with-rails-ember-part-ii-custom-ember-simple-auth/
+import Ember from 'ember';  
+import Base from 'ember-simple-auth/authenticators/base';  
+import config from '../config/environment';
+
+const { RSVP: { Promise }, $: { ajax }, run } = Ember;
+
+export default Base.extend({  
+
+  tokenEndpoint: `${EmberENV.pixApiHost}/api/users/create`,
+
+  restore(data) {
+    return new Promise((resolve, reject) => {
+      if (!Ember.isEmpty(data.token)) {
+        resolve(data);
+      } else {
+        reject();
+      }
+    });
+  },
+
+  authenticate(creds) {
+    const { identification, password } = creds;
+    const data = JSON.stringify({
+      auth: {
+        email: identification,
+        password
+      }
+    });
+    const requestOptions = {
+      url: this.tokenEndpoint,
+      type: 'POST',
+      data,
+      contentType: 'application/json',
+      dataType: 'json'
+    };
+    return new Promise((resolve, reject) => {
+      ajax(requestOptions).then((response) => {
+        const { jwt } = response;
+        // Wrapping aync operation in Ember.run
+        run(() => {
+          resolve({
+            token: jwt
+          });
+        });
+      }, (error) => {
+        // Wrapping aync operation in Ember.run
+        run(() => {
+          reject(error);
+        });
+      });
+    });
+  },
+  
+  invalidate(data) {
+    return Promise.resolve(data);
+  }
+
+});
