86638d79ed4e61fc3ffc6c8f6c69d156be40e04d
David <davbohr@gmail.com>
2016-11-04 11:30:30 +0100

    jilljenn modification integrated

--- live/mirage/routes/get-answer.js
+++ live/mirage/routes/get-answer.js
@@ -2,6 +2,8 @@ import qcuAnswer from '../data/answers/qcu-answer';
 import qcuAnswerWithImage from '../data/answers/qcu-answer-with-image';
 import qcmAnswer from '../data/answers/qcm-answer';
 import qrocmAnswer from '../data/answers/qrocm-answer';
+import qcmAnswerKo from '../data/answers/qcm-answer-ko';
+import qcmAnswerOk from '../data/answers/qcm-answer-ok';
 
 export default function (schema, request) {
 
@@ -15,8 +17,12 @@ export default function (schema, request) {
       return qcuAnswer;
     case qrocmAnswer.data.id:
       return qrocmAnswer;
+    case qcmAnswerKo.data.id:
+      return qcmAnswerKo;
+    case qcmAnswerOk.data.id:
+      return qcmAnswerOk;
     default:
-      throw new Error('The answer you requested in the fake server does not exist');
+      throw new Error('The answer you required in the fake server does not exist');
   }
 
 }
