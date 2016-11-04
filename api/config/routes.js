cca408fde55e381f580a5b018b6d053c6363e32c
David <davbohr@gmail.com>
2016-11-04 10:03:58 +0100

    fixed bug of rebase 2

--- api/config/routes.js
+++ api/config/routes.js
@@ -14,6 +14,7 @@ module.exports = [
   { method: 'POST', path: '/api/users',                               config: Users.save },
 
   { method: 'POST', path: '/api/assessments',                         config: Assessments.save },
+  { method: 'GET',  path: '/api/assessments/{id}/next',               config: Assessments.getNextChallenge },
   { method: 'GET',  path: '/api/assessments/{id}/next/{challengeId}', config: Assessments.getNextChallenge },
   { method: 'GET',  path: '/api/assessments/{id}',                    config: Assessments.get },
 
