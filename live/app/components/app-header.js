c96a1efd9c292aec14ccbbeef6b64c8eca7fb8c3
David <davbohr@gmail.com>
2016-11-07 08:05:12 +0100

    injecting stubbed user-email into template

--- live/app/components/app-header.js
+++ live/app/components/app-header.js
@@ -2,5 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'header',
-  session: Ember.inject.service()
+  session: Ember.inject.service(),
+  sessionAccount: Ember.inject.service('session-account')
+
 });
