e844d78da59879733c0537d62e81a3ec5c0f5d0d
David <davbohr@gmail.com>
2016-11-07 16:21:12 +0100

    login/logout

--- live/app/routes/index.js
+++ live/app/routes/index.js
@@ -6,9 +6,6 @@ export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   session: Ember.inject.service(),
 
   beforeModel: function(){
-    if (this.get('session.isAuthenticated')) {
-      this.get('session').invalidate();
-    }
     let session = this.get('session');
   },
 
