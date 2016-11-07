d5fc0d897d26a67ced19da1213e13b9c549d9690
David <davbohr@gmail.com>
2016-11-07 15:35:10 +0100

    disconnect when come back home

--- live/app/routes/index.js
+++ live/app/routes/index.js
@@ -6,6 +6,9 @@ export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   session: Ember.inject.service(),
 
   beforeModel: function(){
+    if (this.get('session.isAuthenticated')) {
+      this.get('session').invalidate();
+    }
     let session = this.get('session');
   },
 
