4d19dd1f9dc10dc1331bd4ad846bc0ffdf879604
David <davbohr@gmail.com>
2016-11-08 14:18:14 +0100

    fixed live/app/adapters/application.js 2

--- live/app/adapters/application.js
+++ live/app/adapters/application.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data'
 import ENV from 'pix-live/config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin'; 
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
 
   namespace: 'api',
   host: ENV.APP.API_HOST
