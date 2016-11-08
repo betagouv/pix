a5034f3f1958f86afe1a7c3f0696e8cd9f4eed36
David <davbohr@gmail.com>
2016-11-08 14:18:54 +0100

    deleted live/app/initializers/configure-pix-api-host.js

--- api/app/controllers/tokens.js
+++ api/app/controllers/tokens.js
@@ -3,6 +3,7 @@
 const User = require('../models/data/user');
 const bcrypt = require('bcryptjs');
 const Boom = require('boom');
+const _ = require('lodash');
 
 const createToken = require('../services/token-service');
 
@@ -16,38 +17,105 @@ function hashPassword(password, callback) {
   });
 } 
 
+function validatePassword(password) {
+  return _.isString(password) && password.length > 1;
+}
+
+function validateEmail(email) {
+  return _.isString(email) && email.indexOf("@") > 0;
+}
+
+function validateFirstName(firstName) {
+  return _.isString(firstName) && firstName.length > 1;
+}
+
+function validateLastName(lastName) {
+  return _.isString(lastName) && lastName.length > 1;
+}
+
+function validateHasAcceptedCGU(hasAcceptedCGU) {
+  return _.isBoolean(hasAcceptedCGU) && hasAcceptedCGU;
+}
+
+function validatePasswordConfirm(password, passwordConfirm) {
+  return _.isString(password) 
+  && _.isString(passwordConfirm) 
+  && password.length > 0
+  && passwordConfirm.length > 0
+  && passwordConfirm === password
+}
+
+
+function validateCredentials(credentials) {
+  let errors = [];
+  if (!validatePassword(credentials.password)) {
+    errors.push('Le mot de passe doit faire au moins 6 caractères');
+  }
+  // if (!validatePasswordConfirm(credentials.passwordConfirm)) {
+  //   errors.push('Les 2 mots de passe ne correspondent pas');
+  // }
+  // if (!validateHasAcceptedCGU(credentials.hasAcceptedCGU)) {
+  //   errors.push('Vous devez acceptez les CGU');
+  // }
+  if (!validateFirstName(credentials.firstName)) {
+    errors.push('Le prénom est obligatoire');
+  }
+  if (!validateLastName(credentials.lastName)) {
+    errors.push('Le nom de famille est obligatoire');
+  }
+  return errors;
+};
+
 module.exports = {
 
   create: {
     auth: false,
     handler: (request, reply) => {
       const user = new User();
+      
+      const credentials = {
+        email: request.payload.auth.email,
+        firstName: request.payload.auth.firstName,
+        lastName: request.payload.auth.lastName,
+        password: request.payload.auth.password,
+        passwordConfirm: request.payload.auth.passwordConfirm,
+        hasAcceptedCGU: request.payload.auth.hasAcceptedCGU
+      };
 
-      console.log(JSON.stringify(request.payload));
+      let credentialsErrors = validateCredentials(credentials);
 
-      // XXX : random login, should be delete from database
-      user.attributes.login = Math.random().toString(36).substr(2, 5);
-      user.attributes.email = request.payload.auth.email;
-      user.attributes.firstName = request.payload.auth.firstName;
-      user.attributes.lastName = request.payload.auth.lastName;
+      if (credentialsErrors.length > 0) {
+        reply(credentialsErrors).code(400);
+      } else {
 
-      hashPassword(request.payload.auth.password, (err, hash) => {
-        if (err) {
-          reply({ message:'invalid query' }).code(400);
-        }
+        console.log(JSON.stringify(request.payload));
 
-        user.attributes.password = hash;
+        // XXX : random login, should be delete from database
+        user.attributes.login = Math.random().toString(36).substr(2, 5);
+        user.attributes.email = request.payload.auth.email;
+        user.attributes.firstName = request.payload.auth.firstName;
+        user.attributes.lastName = request.payload.auth.lastName;
 
-        user.save()
-        .then((createdUser) => {
-          reply({ jwt: createToken(user) }).code(201);
-        })
-        .catch((error) => {
-          console.log(error);
-          reply({ message:'unable to create user' }).code(400);
+        hashPassword(request.payload.auth.password, (err, hash) => {
+          if (err) {
+            reply({ message:'invalid query' }).code(400);
+          }
+
+          user.attributes.password = hash;
+
+          user.save()
+          .then((createdUser) => {
+            reply({ jwt: createToken(user) }).code(201);
+          })
+          .catch((error) => {
+            reply(['L\'utilisateur existe déjà']).code(422);
+          });
+
         });
+      }
 
-      });
+
+
     }
   },
 
