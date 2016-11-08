113655fef47d1e2385bf1da80998952ec9f03ca9
David <davbohr@gmail.com>
2016-11-08 15:07:20 +0100

    debugging after rebase 1

--- live/app/adapters/application.js
+++ live/app/adapters/application.js
@@ -5,6 +5,6 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
 
   namespace: 'api',
-  host: ENV.APP.API_HOST
+  host: ENV.APP.API_HOST,
   authorizer: 'authorizer:custom'
 });
