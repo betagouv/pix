681b51eef573637e169130cd86383aec81794526
David <davbohr@gmail.com>
2016-11-10 11:00:29 +0100

    bsr

--- live/app/services/delay.js
+++ live/app/services/delay.js
@@ -3,7 +3,6 @@ import RSVP from 'rsvp';
 
 export default Ember.Service.extend({
   ms(ms) {
-    /* istanbul ignore */
     if (EmberENV.useDelay) {
       return new RSVP.Promise((resolve) => {
         setTimeout(resolve, ms)
