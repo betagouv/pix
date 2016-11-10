5a3e6ccd6de6cdde4a2c6bb617053f325818b69d
David <davbohr@gmail.com>
2016-11-10 11:25:10 +0100

    bsr

--- live/tests/integration/components/get-result-test.js
+++ live/tests/integration/components/get-result-test.js
@@ -14,15 +14,6 @@ describeComponent(
   },
   function() {
     it('renders', function() {
-      // Set any properties with this.set('myProperty', 'value');
-      // Handle any actions with this.on('myAction', function(val) { ... });
-      // Template block usage:
-      // this.render(hbs`
-      //   {{#get-result}}
-      //     template content
-      //   {{/get-result}}
-      // `);
-
       this.render(hbs`{{get-result}}`);
       expect(this.$()).to.have.length(1);
     });
