223cf90eb815ca122a3430437414f52520e65b5d
Thomas Wickham <mackwic@gmail.com>
2016-11-13 23:01:04 +0000

    Release of tech-benchmark with env integration (via commit hash: 2d6eb958003d6e3b40df45d0a724068107aac563)

--- live/tech-benchmark/assets/pix-live.js
+++ live/tech-benchmark/assets/pix-live.js
@@ -9533,7 +9533,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+ad361fdd"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+2d6eb958"});
 }
 
 /* jshint ignore:end */
