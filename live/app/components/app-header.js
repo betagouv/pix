4a617fb82e4a99185e44174e9792f0df05e34c36
David <davbohr@gmail.com>
2016-11-14 19:21:15 +0100

    tests are corrected

--- live/app/components/app-header.js
+++ live/app/components/app-header.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'header',
   session: Ember.inject.service(),
-  router: Ember.inject.service(),
+  router: Ember.inject.service()
 });
