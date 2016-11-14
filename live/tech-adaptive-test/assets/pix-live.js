6447e91f2edea00c491ba75ec9ba5dcd3f3d02cb
Thomas Wickham <mackwic@gmail.com>
2016-11-14 10:09:28 +0000

    Release of tech-adaptive-test with env integration (via commit hash: 023f8d13920fffc4372be32396768dc418873419)

--- live/tech-adaptive-test/assets/pix-live.js
+++ live/tech-adaptive-test/assets/pix-live.js
@@ -9535,7 +9535,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+ba5c5382"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+023f8d13"});
 }
 
 /* jshint ignore:end */
