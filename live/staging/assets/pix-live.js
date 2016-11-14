7e6eff4f9e051092d68e520e84f14a96ade34079
Thomas Wickham <mackwic@gmail.com>
2016-11-14 10:37:44 +0000

    Release of staging with env staging (via commit hash: 81c0d19f856c42d0a567a2ebf6bd6e79fcad8951)

--- live/staging/assets/pix-live.js
+++ live/staging/assets/pix-live.js
@@ -9673,7 +9673,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+3dce55f8"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+81c0d19f"});
 }
 
 /* jshint ignore:end */
