6dcfa2ce7dba21ec4fb3c420af2f017ce40560dc
Thomas Wickham <mackwic@gmail.com>
2016-11-15 12:14:50 +0000

    Release of 264-design-challenge-buttons with env integration (via commit hash: 144b37709e693d336e9340ec2212d3913df4d173)

--- live/264-design-challenge-buttons/assets/pix-live.js
+++ live/264-design-challenge-buttons/assets/pix-live.js
@@ -10613,7 +10613,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+cc58ff1d"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+144b3770"});
 }
 
 /* jshint ignore:end */
