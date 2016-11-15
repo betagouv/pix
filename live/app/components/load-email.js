3544a39ed3908cb5b7d190375fd68ce053dae5a7
David <davbohr@gmail.com>
2016-11-15 12:03:40 +0100

    wip

--- live/app/components/load-email.js
+++ live/app/components/load-email.js
@@ -1,26 +1,50 @@
 import Ember from 'ember';
 
+// http://stackoverflow.com/a/32686261/2595513
+function validateEmail(email) 
+{
+  let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
 export default Ember.Component.extend({  
   didInsertElement: function() {
     Ember.run.scheduleOnce('afterRender', this, function() {
-     
-      // borrowed from http://codepen.io/dead_seagull/pen/QjPOjq
-      var states = ["error","success"],
-      curState = 0;
-      $(".dsButtonAnim").on('click', function() {
-        curState ? curState = 0 : curState = 1;
-        var that = $(this);
-        if (that.hasClass("success")) {
-          that.removeClass("success");
-        } else if (that.hasClass("error")) {
-          that.removeClass("error");
-        } else {
-          that.addClass("loading");
-          window.setTimeout(function() {
-            that.removeClass("loading").addClass( states[ curState ] );
-          }, 4000);
-        }
+
+      let btn = $('.load-email')[0];
+      let $contactForm = $('#contact-form');
+
+      $contactForm.submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+          url: 'https://formspree.io/1024pix@gmail.com',
+          method: 'POST',
+          data: $(this).serialize(),
+          dataType: 'json',
+          beforeSend: function() {
+            console.log('before send');
+          },
+          success: function(data) {
+            console.log('success');
+            btn.classList.add('load-email-is-active');
+          },
+          error: function(err) {
+            console.log('error');
+            btn.classList.add('load-email-is-error');
+
+          }
+        });
       });
+
+
+      // $('.load-email').on('click', function () {
+      //   let btn = $('.load-email')[0];
+      //   btn.classList.add('load-email-is-active');
+      // });
+
+      // setTimeout(function () {
+      //   btn.classList.remove('load-email-is-active');
+      // }, 2500)
 
     });
   }
