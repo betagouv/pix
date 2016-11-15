9b63fde99298018b70d819e85a3ae4bbc44107e7
Thomas Wickham <mackwic@gmail.com>
2016-11-15 15:01:58 +0000

    Release of 240-add-analytics with env integration (via commit hash: c42eccb8d5a2c13631439fbb1c21951b3ca247b5)

--- live/240-add-analytics/assets/pix-live.js
+++ live/240-add-analytics/assets/pix-live.js
@@ -10777,7 +10777,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+ed758cdf"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+c42eccb8"});
 }
 
 /* jshint ignore:end */
