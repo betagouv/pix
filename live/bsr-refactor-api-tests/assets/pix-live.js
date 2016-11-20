5dc1665341200756077817c55157fccc9e67ddd3
Thomas Wickham <mackwic@gmail.com>
2016-11-20 23:02:49 +0000

    Release of bsr-refactor-api-tests with env integration (via commit hash: e6029261aa768137d193a81aab2b597d93ee30e8)

--- live/bsr-refactor-api-tests/assets/pix-live.js
+++ live/bsr-refactor-api-tests/assets/pix-live.js
@@ -10715,7 +10715,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+8cb86870"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+e6029261"});
 }
 
 /* jshint ignore:end */
