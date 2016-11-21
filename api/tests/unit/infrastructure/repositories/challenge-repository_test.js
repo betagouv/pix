68de74e1918e773ec363e6e9a142ee28c7979606
Jérémy Buget <jbuget@octo.com>
2016-11-21 09:50:48 +0100

    Add unit tests for challenge-repository

--- api/tests/unit/infrastructure/repositories/challenge-repository_test.js
+++ api/tests/unit/infrastructure/repositories/challenge-repository_test.js
@@ -9,12 +9,12 @@ describe('Unit | Repository | ChallengeRepository', function () {
   let stub;
 
   beforeEach(function () {
+    cache.flushAll();
     stub = sinon.stub(Airtable, 'base');
   });
 
   afterEach(function () {
     stub.restore();
-    cache.flushAll();
   });
 
   /*
@@ -24,6 +24,7 @@ describe('Unit | Repository | ChallengeRepository', function () {
   describe('#get(id)', function () {
 
     describe('when the challenge has been previously fetched and cached', function () {
+
       it('should return the challenge directly retrieved from the cache', function () {
         // given
         const challengeId = 'challengeId';
@@ -40,6 +41,31 @@ describe('Unit | Repository | ChallengeRepository', function () {
 
       it('should not make call to Airtable', function () {
         expect(stub.called).to.be.false;
+      });
+
+    });
+
+    describe('when the cache throw an error', function () {
+
+      let mockedCache;
+      const cacheErrorMessage = 'Cache error';
+
+      before(function () {
+        mockedCache = sinon.stub(cache, 'get', (key, callback) => {
+          callback(new Error(cacheErrorMessage));
+        });
+      });
+
+      after(function () {
+        cache.get.restore();
+      });
+
+      it('should reject with thrown error', function () {
+        // when
+        const result = ChallengeRepository.get('challenge_id');
+
+        // then
+        return expect(result).to.eventually.be.rejectedWith(cacheErrorMessage);
       });
 
     });
@@ -117,6 +143,31 @@ describe('Unit | Repository | ChallengeRepository', function () {
 
     });
 
+    describe('when the cache throw an error', function () {
+
+      let mockedCache;
+      const cacheErrorMessage = 'Cache error';
+
+      before(function () {
+        mockedCache = sinon.stub(cache, 'get', (key, callback) => {
+          callback(new Error(cacheErrorMessage));
+        });
+      });
+
+      after(function () {
+        cache.get.restore();
+      });
+
+      it('should reject with thrown error', function () {
+        // when
+        const result = ChallengeRepository.list();
+
+        // then
+        return expect(result).to.eventually.be.rejectedWith(cacheErrorMessage);
+      });
+
+    });
+
     describe('when the challenges have not been previously cached', function () {
 
       const record_1 = { "id": "challenge_1" };
@@ -160,6 +211,82 @@ describe('Unit | Repository | ChallengeRepository', function () {
           });
         });
       });
+    });
+
+  });
+
+  /*
+   * #refresh(id)
+   */
+
+  describe('#refresh(id)', function () {
+
+    let record = {
+      "id": "challenge_id",
+      "fields": {
+        "Consigne": "Citez jusqu'à 3 moteurs de recherche généralistes.",
+        "Propositions": "${moteur 1}\n${moteur 2}\n${moteur 3}",
+        "Type d'épreuve": "QROCM",
+        "Bonnes réponses": "${moteur 1} ou ${moteur 2} ou ${moteur 3} = \nGoogle\nBing\nQwant\nDuckduckgo\nYahoo\nYahoo Search\nLycos\nAltavista\nHotbot"
+      }
+    };
+
+    beforeEach(function () {
+      stub.returns({
+        find(id, callback) {
+          if (record.id !== id) callback(new Error());
+          return callback(null, record);
+        }
+      });
+    });
+
+    it('should return the challenge fetched from Airtable', function () {
+      // given
+      const challenge = new Challenge(record);
+
+      // when
+      const result = ChallengeRepository.refresh(challenge.id);
+
+      // then
+      return expect(result).to.eventually.deep.equal(challenge);
+    });
+
+    it('should store the challenge in the cache', function () {
+      // given
+      const challengeId = 'challenge_id';
+
+      // when
+      ChallengeRepository.refresh(challengeId);
+
+      // then
+      cache.get(`challenge-repository_get_${challengeId}`, (err, cachedValue) => {
+        expect(cachedValue).to.exist;
+      });
+    });
+
+    describe('when the cache throw an error', function () {
+
+      let mockedCache;
+      const cacheErrorMessage = 'Cache error';
+
+      before(function () {
+        mockedCache = sinon.stub(cache, 'del', (key, callback) => {
+          callback(new Error(cacheErrorMessage));
+        });
+      });
+
+      after(function () {
+        cache.del.restore();
+      });
+
+      it('should reject with thrown error', function () {
+        // when
+        const result = ChallengeRepository.refresh('challenge_id');
+
+        // then
+        return expect(result).to.eventually.be.rejectedWith(cacheErrorMessage);
+      });
+
     });
 
   });
