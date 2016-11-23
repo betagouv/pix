0d7102a4be7433c586521044c82cdfb040511da9
Thomas Wickham <mackwic@gmail.com>
2016-11-23 13:40:45 +0000

    Release of 272-rwd with env integration (via commit hash: 8505faa2e7f84cee214460f4da84bfc53e50e8c6)

--- live/272-rwd/assets/pix-live.js
+++ live/272-rwd/assets/pix-live.js
@@ -10406,7 +10406,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+5520b30a"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"1.0.0+8505faa2"});
 }
 
 /* jshint ignore:end */
