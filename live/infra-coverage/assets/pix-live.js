be35d340af301698eef0dda78a27ab351f1c6b26
Jeremy Buget <jbuget@gmail.com>
2016-11-24 23:25:20 +0000

    Release of infra-coverage with env integration (via commit hash: 611b69ea10ecfaf208f1c5ce494fc69624910210)

--- live/infra-coverage/assets/pix-live.js
+++ live/infra-coverage/assets/pix-live.js
@@ -10410,7 +10410,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"2.0.0-SNAPSHOT+3c37d1a3"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"2.0.0-SNAPSHOT+611b69ea"});
 }
 
 /* jshint ignore:end */
