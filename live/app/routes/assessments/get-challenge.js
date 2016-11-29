a55461884d23b59deeead026ac3d686d7f64610b
David <davbohr@gmail.com>
2016-11-29 13:35:20 +0100

    bug not rendering

--- live/app/routes/assessments/get-challenge.js
+++ live/app/routes/assessments/get-challenge.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import RSVP from 'rsvp';
 import DS from 'ember-data';
-import getChallengeType from '../../utils/get-challenge-type';
 
 export default Ember.Route.extend({
 
@@ -10,12 +9,37 @@ export default Ember.Route.extend({
   model(params) {
     const store = this.get('store');
     const assessmentPromise = store.findRecord('assessment', params.assessment_id);
-    const challengePromise = store.findRecord('challenge', params.challenge_id);
+    const challengePromise  = store.findRecord('challenge', params.challenge_id);
+    const answerPromise     = store.queryRecord('answer', { 
+      assessment: params.assessment_id, 
+      challenge:  params.challenge_id });
 
-    return RSVP.hash({
-      assessment: assessmentPromise,
-      challenge: challengePromise
-    });
+    const spotsPromises = [
+      assessmentPromise,
+      challengePromise,
+      answerPromise
+    ];
+    
+
+    return Ember.RSVP.allSettled(spotsPromises).then((spotPromisesResults)=> {
+      if (!spotPromisesResults.isAny('state', 'rejected')) {
+        console.log('allsettled!')
+          // Yay ! all promised resolved
+          return RSVP.hash({
+            assessment: spotsPromises[0],
+            challenge: spotsPromises[1],
+            answer: spotsPromises[2]
+          });
+      } else {
+        console.log('erroneus');
+        // answerPromise is allowed to fail (404 not found). Resolve other promises
+        return RSVP.hash({
+          assessment: spotsPromises[0],
+          challenge: spotsPromises[1]
+        });
+      } 
+      });
+
   },
 
   actions : {
@@ -26,6 +50,7 @@ export default Ember.Route.extend({
         this._navigateToNextView(currentChallenge, assessment);
       });
     }
+
   },
 
   _createAnswer: function (answerValue, currentChallenge, assessment) {
@@ -50,15 +75,13 @@ export default Ember.Route.extend({
   setupController: function(controller, model) {
     this._super(controller, model);
 
+    // model.reload();
+
     const progressToSet = model.assessment
-      .get('course')
-      .then((course) => course.getProgress(model.challenge));
+    .get('course')
+    .then((course) => course.getProgress(model.challenge));
 
     controller.set('progress', DS.PromiseObject.create({ promise: progressToSet }));
-
-    const challengeType =  getChallengeType(model.challenge.get('type'));
-    controller.set('challengeItemType', 'challenge-item-' + challengeType);
-
   },
 
   serialize: function (model) {
