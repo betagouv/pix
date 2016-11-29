0e734b896d6d7ac9091908769f2d51247382dd72
David <davbohr@gmail.com>
2016-11-29 13:35:20 +0100

    bug not rendering

---
+++ live/app/components/qcm-proposals.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    answersDidChange: Ember.on('init', Ember.observer('answers',     function() {
+    // some side effect of salutation changing
+    console.log('answers changed to ' + JSON.stringify(this.get('answers')));
+  })),
+});
--- live/mirage/data/answers/ref-qcu-answer.js
+++ live/mirage/data/answers/ref-qcu-answer.js
@@ -14,6 +14,12 @@ export default {
           type: 'challenges',
           id: challenge.data.id
         }
+      },
+      assessment: {
+        data: {
+          type: 'assessments',
+          id: 'ref_assessment_id'
+        }
       }
     }
   }
