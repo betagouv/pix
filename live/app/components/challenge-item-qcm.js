a2b18852fe0c3d74d4efc207cb00952d33f754e7
David <davbohr@gmail.com>
2016-11-29 22:19:07 +0100

    starting to debug tests

--- live/app/components/challenge-item-qcm.js
+++ live/app/components/challenge-item-qcm.js
@@ -1,5 +1,3 @@
-import Ember from 'ember';
-import _ from 'lodash/lodash';
 import ChallengeItemGeneric from './challenge-item-generic';
 
 const ChallengeItemQcm = ChallengeItemGeneric.extend({
--- live/app/models/challenge.js
+++ live/app/models/challenge.js
@@ -1,8 +1,8 @@
-import DS from 'ember-data';
+import DS                     from 'ember-data';
 
-import ProposalsAsArrayMixin from './challenge/proposals-as-array-mixin';
+import ProposalsAsArrayMixin  from './challenge/proposals-as-array-mixin';
 import ProposalsAsBlocksMixin from './challenge/proposals-as-blocks-mixin';
-import InstructionAsObject from './challenge/instruction-as-object-mixin';
+import InstructionAsObject    from './challenge/instruction-as-object-mixin';
 
 const { Model, attr } = DS;
 
