81f9629d48e012c5beb15aaecbc557e32ffcbdde
Jérémy Buget <jbuget@octo.com>
2016-12-01 19:51:22 +0100

    Refactor challenge-item-* components

--- live/app/components/challenge-item-qcm.js
+++ live/app/components/challenge-item-qcm.js
@@ -3,7 +3,7 @@ import ChallengeItemGeneric from './challenge-item-generic';
 const ChallengeItemQcm = ChallengeItemGeneric.extend({
 
   _hasError: function () {
-    return !(this._getAnswerValue().length >= 1);
+    return this._getAnswerValue().length < 1;
   },
 
   // XXX : data is extracted from DOM of child component, breaking child encapsulation.
