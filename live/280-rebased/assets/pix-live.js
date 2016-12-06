3ab8929a755118a98425d06bc7661c8db7bb4fd6
Jeremy Buget <jbuget@gmail.com>
2016-12-06 10:26:05 +0000

    Release of 280-rebased with env integration (via commit hash: a8898a77aaa479551de5a7722874b6298b4ba83d)

--- live/280-rebased/assets/pix-live.js
+++ live/280-rebased/assets/pix-live.js
@@ -2813,7 +2813,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"3.0.0+da84d90d"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"3.0.0+a8898a77"});
 }
 
 /* jshint ignore:end */
