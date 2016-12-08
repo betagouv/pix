e74cfabde17dadd9b3c9528bf3e98d95763eacd4
David <davbohr@gmail.com>
2016-12-08 14:22:02 +0100

    qrocm-dep, backend, done

--- api/lib/domain/services/solution-service-qrocm-depsco.js
+++ api/lib/domain/services/solution-service-qrocm-depsco.js
@@ -1,7 +1,55 @@
+const utils = require('./solution-service-utils');
+const yaml = require('js-yaml');
+const _ = require('lodash');
+
 module.exports = {
 
   match (yamlAnswer, yamlSolution) {
-    return 'ko';
+    try {
+      let result = 'ko';
+
+      const answerMap = yaml.load(yamlAnswer);
+      // answerMap is
+      //{ num1: ' google.fr', num2: 'yahoo aNswer ' }
+
+      const solution = yaml.load(yamlSolution);
+      // solution is
+      // { Google: [ 'Google', 'google.fr', 'Google Search' ], Yahoo: [ 'Yahoo', 'Yahoo Answer' ] }
+
+      const possibleAnswers = {};
+      _.each(solution, (answerList, solutionKey) => {
+        _.each(answerList, (answer) => {
+          possibleAnswers[answer] = solutionKey;
+        });
+      });
+      // possibleAnswers is
+      // { Google: 'Google','google.fr': 'Google','Google Search': 'Google',Yahoo: 'Yahoo','Yahoo Answer': 'Yahoo' }
+
+      let scoredKeys = [];
+      _.each(answerMap, (answer) => {
+        _.each(possibleAnswers, (solutionKey, possibleAnswer) => {
+          if(utils.fuzzyMatchingWithAnswers(answer, [possibleAnswer])) {
+            scoredKeys.push(solutionKey);
+          }
+        });
+      });
+      //scoredKeys is
+      // [ 'Google', 'Yahoo' ]
+
+      // remove duplicates
+      scoredKeys = _.uniq(scoredKeys);
+
+      const numberOfUserAnswers = Object.keys(answerMap).length;
+      const numberOfUniqueCorrectAnswers = scoredKeys.length;
+
+
+      if (numberOfUniqueCorrectAnswers === numberOfUserAnswers) {
+        result = 'ok';
+      }
+      return result;
+    } catch (e) { // Parse exceptions like script injection could happen. They are detected here.
+      return 'ko';
+    }
   }
 
 };
--- api/tests/unit/domain/services/solution-service_test.js
+++ api/tests/unit/domain/services/solution-service_test.js
@@ -165,10 +165,35 @@ describe('Unit | Service | SolutionService', function () {
 
     describe('if solution type is QROCM-depsco', function () {
 
-      it('should return "ko" when answer does not match any solution variants', function () {
-        const answer = buildAnswer('answer: unmatching answer');
-        const solution = buildSolution('QROCM-depsco', 'answer:\n- unmatched solution variant');
+      it('should return "ko" for badly formatted solution', function () {
+        const answer = buildAnswer('num1: Google\nnum2: Yahoo');
+        const solution = buildSolution('QROCM-depsco', 'solution like a QCU');
         expect(service.match(answer, solution)).to.equal('ko');
+      });
+
+      it('should return "ko" when answer is incorrect', function () {
+        const answer = buildAnswer('num1: Foo\nnum2: Bar');
+        const solution = buildSolution('QROCM-depsco', 'Google:\n- Google\n- google.fr\n- Google Search\nYahoo:\n- Yahoo\n- Yahoo Answer');
+        expect(service.match(answer, solution)).to.equal('ko');
+      });
+
+      it('should return "ko" when user duplicated a correct answer', function () {
+        const answer = buildAnswer('num1: google.fr\nnum2: google.fr');
+        const solution = buildSolution('QROCM-depsco', 'Google:\n- Google\n- google.fr\n- Google Search\nYahoo:\n- Yahoo\n- Yahoo Answer');
+        expect(service.match(answer, solution)).to.equal('ko');
+      });
+
+      const maximalScoreCases = [
+        { answer: 'num1: " google.fr"\nnum2: "Yahoo anSwer "',
+          solution: 'Google:\n- Google\n- google.fr\n- Google Search\nYahoo:\n- Yahoo\n- Yahoo Answer' },
+      ];
+
+      maximalScoreCases.forEach(function (testCase) {
+        it('should return "ok" when answer is "' + testCase.answer + '" and solution is "' + escape(testCase.solution) + '"', function () {
+          const answer = buildAnswer(testCase.answer);
+          const solution = buildSolution('QROCM-depsco', testCase.solution);
+          expect(service.match(answer, solution)).to.equal('ok');
+        });
       });
 
     });
