0939f48d7283af7c4beeab352f4fad42fc85b680
Jeremy Buget <jbuget@gmail.com>
2016-12-08 14:49:26 +0000

    Release of 301-qrocm-dep with env integration (via commit hash: f65c72383adc8a5b1aa124810230ccf50644ba7e)

--- live/301-qrocm-dep/assets/pix-live.js
+++ live/301-qrocm-dep/assets/pix-live.js
@@ -2818,7 +2818,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"2.1.1+d3d3b238"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"2.1.1+f65c7238"});
 }
 
 /* jshint ignore:end */
