f65c72383adc8a5b1aa124810230ccf50644ba7e
David <davbohr@gmail.com>
2016-12-08 15:43:47 +0100

    bsr

--- api/lib/domain/services/solution-service-qrocm-dep.js
+++ api/lib/domain/services/solution-service-qrocm-dep.js
@@ -18,54 +18,58 @@ function _isValidSolution(solution) {
 module.exports = {
 
   match (yamlAnswer, yamlSolution) {
-    try {
-      let result = 'ko';
+    let result = 'ko';
+    let answerMap = null;
+    let solution = null;
 
-      const answerMap = yaml.load(yamlAnswer);
+    try {
+      answerMap = yaml.safeLoad(yamlAnswer);
       // answerMap is
       //{ num1: ' google.fr', num2: 'yahoo aNswer ' }
 
-      const solution = yaml.load(yamlSolution);
+      solution = yaml.safeLoad(yamlSolution);
       // solution is
       // { Google: [ 'Google', 'google.fr', 'Google Search' ], Yahoo: [ 'Yahoo', 'Yahoo Answer' ] }
 
-      if (!_isValidSolution(solution)) {
-        return result;
-      }
-
-      const possibleAnswers = {};
-      _.each(solution, (answerList, solutionKey) => {
-        _.each(answerList, (answer) => {
-          possibleAnswers[answer] = solutionKey;
-        });
-      });
-      // possibleAnswers is
-      // { Google: 'Google','google.fr': 'Google','Google Search': 'Google',Yahoo: 'Yahoo','Yahoo Answer': 'Yahoo' }
-
-      let scoredKeys = [];
-      _.each(answerMap, (answer) => {
-        _.each(possibleAnswers, (solutionKey, possibleAnswer) => {
-          if(utils.fuzzyMatchingWithAnswers(answer, [possibleAnswer])) {
-            scoredKeys.push(solutionKey);
-          }
-        });
-      });
-      //scoredKeys is
-      // [ 'Google', 'Yahoo' ]
-
-      // remove duplicates
-      scoredKeys = _.uniq(scoredKeys);
-
-      const numberOfUserAnswers = Object.keys(answerMap).length;
-      const numberOfUniqueCorrectAnswers = scoredKeys.length;
-
-      if (numberOfUniqueCorrectAnswers === numberOfUserAnswers) {
-        result = 'ok';
-      }
-      return result;
     } catch (e) { // Parse exceptions like script injection could happen. They are detected here.
       return 'ko';
     }
+
+    if (!_isValidSolution(solution)) {
+      return 'ko';
+    }
+
+    const possibleAnswers = {};
+    _.each(solution, (answerList, solutionKey) => {
+      _.each(answerList, (answer) => {
+        possibleAnswers[answer] = solutionKey;
+      });
+    });
+      // possibleAnswers is
+      // { Google: 'Google','google.fr': 'Google','Google Search': 'Google',Yahoo: 'Yahoo','Yahoo Answer': 'Yahoo' }
+
+    let scoredKeys = [];
+    _.each(answerMap, (answer) => {
+      _.each(possibleAnswers, (solutionKey, possibleAnswer) => {
+        if(utils.fuzzyMatchingWithAnswers(answer, [possibleAnswer])) {
+          scoredKeys.push(solutionKey);
+        }
+      });
+    });
+    //scoredKeys is
+    // [ 'Google', 'Yahoo' ]
+
+    // remove duplicates
+    scoredKeys = _.uniq(scoredKeys);
+
+    const numberOfUserAnswers = Object.keys(answerMap).length;
+    const numberOfUniqueCorrectAnswers = scoredKeys.length;
+
+    if (numberOfUniqueCorrectAnswers === numberOfUserAnswers) {
+      result = 'ok';
+    }
+    return result;
+
   }
 
 };
