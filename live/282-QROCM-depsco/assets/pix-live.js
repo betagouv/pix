a09f23504537c7c7e1e62bf83ed24fa58ba6e7ed
Jeremy Buget <jbuget@gmail.com>
2016-12-08 14:22:49 +0000

    Release of 282-QROCM-depsco with env integration (via commit hash: d3d3b238913071ac7e6b4172e1709963f72ce1c4)

--- live/282-QROCM-depsco/assets/pix-live.js
+++ live/282-QROCM-depsco/assets/pix-live.js
@@ -2818,7 +2818,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"2.1.1+383c1956"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"2.1.1+d3d3b238"});
 }
 
 /* jshint ignore:end */
