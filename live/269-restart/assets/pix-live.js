03982bfe9cdfa148ddcd5f24af9f90c8395e6b60
Jeremy Buget <jbuget@gmail.com>
2016-12-14 15:30:23 +0000

    Release of 269-restart with env integration (via commit hash: a1d0e6b4b0a4fb509c63775ae947ede0bfe52bbe)

--- live/269-restart/assets/pix-live.js
+++ live/269-restart/assets/pix-live.js
@@ -2899,7 +2899,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"2.1.1+c71faff4"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"2.1.1+a1d0e6b4"});
 }
 
 /* jshint ignore:end */
