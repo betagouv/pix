3ece8f7aba6c7e5ecbf3489b591f03ced1bb3f52
David <davbohr@gmail.com>
2016-12-14 15:26:45 +0100

    Add first adaptive course test

--- api/tests/acceptance/application/assessment-controller_test.js
+++ api/tests/acceptance/application/assessment-controller_test.js
@@ -15,7 +15,24 @@ describe('Acceptance | API | Assessments', function () {
             'id': 'non_adaptive_course_id',
             'fields': {
               // a bunch of fields
+              'Adaptatif ?': false,
               '\u00c9preuves': [
+                'second_challenge',
+                'first_challenge',
+              ],
+            },
+          }
+        );
+        nock('https://api.airtable.com')
+          .get('/v0/test-base/Tests/adaptive_course_id')
+          .times(4)
+          .reply(200, {
+            'id': 'adaptive_course_id',
+            'fields': {
+              // a bunch of fields
+              'Adaptatif ?': true,
+              '\u00c9preuves': [
+                'third_challenge',
                 'second_challenge',
                 'first_challenge',
               ],
@@ -36,6 +53,15 @@ describe('Acceptance | API | Assessments', function () {
           .get('/v0/test-base/Epreuves/second_challenge')
           .reply(200, {
             'id': 'second_challenge',
+            'fields': {
+              // a bunch of fields
+            },
+          }
+        );
+        nock('https://api.airtable.com')
+          .get('/v0/test-base/Epreuves/third_challenge')
+          .reply(200, {
+            'id': 'third_challenge',
             'fields': {
               // a bunch of fields
             },
@@ -284,6 +310,64 @@ describe('Acceptance | API | Assessments', function () {
         done();
       });
     });
+
+  });
+
+  describe('(adaptive) GET /api/assessments/:assessment_id/next', function () {
+
+    let inserted_assessment_id = null;
+
+    const inserted_assessment = {
+      userName: 'John Doe',
+      userEmail: 'john.doe@mailmail.com',
+      courseId: 'adaptive_course_id'
+    };
+
+    beforeEach(function (done) {
+      knex('assessments').delete().then(() => {
+        knex('assessments').insert([inserted_assessment]).then((rows) => {
+          inserted_assessment_id = rows[0];
+          done();
+        });
+      });
+    });
+
+    afterEach(function (done) {
+      knex('assessments').delete().then(() => {done();});
+    });
+
+    it('should return 200 HTTP status code', function (done) {
+      const challengeData = { method: 'GET', url: '/api/assessments/' + inserted_assessment_id + '/next' };
+      server.injectThen(challengeData).then((response) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    /* it('should return application/json', function (done) {
+      const challengeData = { method: 'GET', url: '/api/assessments/' + inserted_assessment_id + '/next' };
+      server.injectThen(challengeData).then((response) => {
+        const contentType = response.headers['content-type'];
+        expect(contentType).to.contain('application/json');
+        done();
+      });
+    });
+
+    it('should return the first challenge if no challenge specified', function (done) {
+      const challengeData = { method: 'GET', url: '/api/assessments/' + inserted_assessment_id + '/next' };
+      server.injectThen(challengeData).then((response) => {
+        expect(response.result.data.id).to.equal('first_challenge');
+        done();
+      });
+    });
+
+    it('should return the next challenge otherwise', function (done) {
+      const challengeData = { method: 'GET', url: '/api/assessments/' + inserted_assessment_id + '/next/first_challenge' };
+      server.injectThen(challengeData).then((response) => {
+        expect(response.result.data.id).to.equal('second_challenge');
+        done();
+      });
+    }); */
 
   });
 
