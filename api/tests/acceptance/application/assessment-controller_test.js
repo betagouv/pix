d02b5d47b5c767e578576844a4e0f5e38f54cdd8
David <davbohr@gmail.com>
2016-12-14 14:36:05 +0100

    refactoring of first test ok

--- api/tests/acceptance/application/assessment-controller_test.js
+++ api/tests/acceptance/application/assessment-controller_test.js
@@ -1,3 +1,4 @@
+/* global describe, before, after, beforeEach, afterEach, knex, nock, it, expect */
 const server = require('../../../server');
 const Assessment = require('../../../lib/domain/models/data/assessment');
 
@@ -11,12 +12,12 @@ describe('Acceptance | API | Assessments', function () {
           .get('/v0/test-base/Tests/assessment_id')
           .times(4)
           .reply(200, {
-            "id": "assessment_id",
-            "fields": {
+            'id': 'assessment_id',
+            'fields': {
               // a bunch of fields
-              "\u00c9preuves": [
-                "second_challenge",
-                "first_challenge",
+              '\u00c9preuves': [
+                'second_challenge',
+                'first_challenge',
               ],
             },
           }
@@ -25,8 +26,8 @@ describe('Acceptance | API | Assessments', function () {
           .get('/v0/test-base/Epreuves/first_challenge')
           .times(3)
           .reply(200, {
-            "id": "first_challenge",
-            "fields": {
+            'id': 'first_challenge',
+            'fields': {
               // a bunch of fields
             },
           }
@@ -34,8 +35,8 @@ describe('Acceptance | API | Assessments', function () {
         nock('https://api.airtable.com')
           .get('/v0/test-base/Epreuves/second_challenge')
           .reply(200, {
-            "id": "second_challenge",
-            "fields": {
+            'id': 'second_challenge',
+            'fields': {
               // a bunch of fields
             },
           }
@@ -51,13 +52,34 @@ describe('Acceptance | API | Assessments', function () {
 
   describe('GET /api/assessments/:id', function () {
 
-    it("should return 200 HTTP status code", function (done) {
+    let inserted_assessment_id = null;
+
+    const inserted_assessment = {
+      userName: 'John Doe',
+      userEmail: 'john.doe@mailmail.com',
+      courseId:'anyFromAirTable'
+    };
+
+    beforeEach(function (done) {
+      knex('assessments').delete().then(() => {
+        knex('assessments').insert([inserted_assessment]).then((id) => {
+          inserted_assessment_id = id;
+          done();
+        });
+      });
+    });
+
+    afterEach(function (done) {
+      knex('assessments').delete().then(() => {done();});
+    });
+
+    it('should return 200 HTTP status code', function (done) {
 
       knex.select('id')
       .from('assessments')
       .limit(1)
       .then(function(rows) {
-        server.injectThen({ method: "GET", url: `/api/assessments/${rows[0].id}` }).then((response) => {
+        server.injectThen({ method: 'GET', url: `/api/assessments/${rows[0].id}` }).then((response) => {
           expect(response.statusCode).to.equal(200);
           done();
         });
@@ -66,37 +88,38 @@ describe('Acceptance | API | Assessments', function () {
     });
 
 
-   it("should return application/json", function (done) {
+    it('should return application/json', function (done) {
 
-     knex.select('id')
+      knex.select('id')
       .from('assessments')
       .limit(1)
       .then(function(rows) {
-        server.injectThen({ method: "GET", url: `/api/assessments/${rows[0].id}` }).then((response) => {
+        server.injectThen({ method: 'GET', url: `/api/assessments/${rows[0].id}` }).then((response) => {
           const contentType = response.headers['content-type'];
           expect(contentType).to.contain('application/json');
           done();
         });
       });
 
-   });
+    });
 
 
-   it("should return the expected assessment", function (done) {
+    it('should return the expected assessment', function (done) {
 
-     knex.select('id')
+      knex.select('id')
       .from('assessments')
       .limit(1)
       .then(function(rows) {
-        server.injectThen({ method: "GET", url: `/api/assessments/${rows[0].id}` }).then((response) => {
-          const expectedAssessment = {"type":"assessments","id":rows[0].id,"attributes":{"user-name":"Jon Snow","user-email":"jsnow@winterfell.got"},"relationships":{"course":{"data":{"type":"courses","id":"anyFromAirTable"}},"answers":{"data":[]}}};
+        server.injectThen({ method: 'GET', url: `/api/assessments/${rows[0].id}` }).then((response) => {
+          const expectedAssessment = {'type':'assessments','id':rows[0].id,'attributes':{'user-name':'John Doe','user-email':'john.doe@mailmail.com'},'relationships':{'course':{'data':{'type':'courses','id':'anyFromAirTable'}},'answers':{'data':[]}}};
           const assessment = response.result.data;
+          console.log('assessment', JSON.stringify(assessment));
           expect(assessment).to.deep.equal(expectedAssessment);
           done();
         });
       });
 
-   });
+    });
 
 
   });
@@ -104,12 +127,12 @@ describe('Acceptance | API | Assessments', function () {
   describe('POST /api/assessments', function () {
 
     const options = {
-      method: "POST", url: "/api/assessments", payload: {
+      method: 'POST', url: '/api/assessments', payload: {
         data: {
-          type: "assessment",
+          type: 'assessment',
           attributes: {
-            "user-name": 'Jon Snow',
-            "user-email": 'jsnow@winterfell.got'
+            'user-name': 'Jon Snow',
+            'user-email': 'jsnow@winterfell.got'
           },
           relationships: {
             course: {
@@ -123,14 +146,14 @@ describe('Acceptance | API | Assessments', function () {
       }
     };
 
-    it("should return 201 HTTP status code", function (done) {
+    it('should return 201 HTTP status code', function (done) {
       server.injectThen(options).then((response) => {
         expect(response.statusCode).to.equal(201);
         done();
       });
     });
 
-    it("should return application/json", function (done) {
+    it('should return application/json', function (done) {
       server.injectThen(options).then((response) => {
         const contentType = response.headers['content-type'];
         expect(contentType).to.contain('application/json');
@@ -138,7 +161,7 @@ describe('Acceptance | API | Assessments', function () {
       });
     });
 
-    it("should add a new assessment into the database", function (done) {
+    it('should add a new assessment into the database', function (done) {
       // given
       Assessment.count().then(function (beforeAssessmentsNumber) {
         // when
@@ -152,7 +175,7 @@ describe('Acceptance | API | Assessments', function () {
       });
     });
 
-    it("should persist the given course ID and user ID", function (done) {
+    it('should persist the given course ID and user ID', function (done) {
 
       // when
       server.injectThen(options).then((response) => {
@@ -161,15 +184,15 @@ describe('Acceptance | API | Assessments', function () {
         .fetch()
         .then(function (model) {
           expect(model.get('courseId')).to.equal(options.payload.data.relationships.course.data.id);
-          expect(model.get('userName')).to.equal(options.payload.data.attributes["user-name"]);
-          expect(model.get('userEmail')).to.equal(options.payload.data.attributes["user-email"]);
+          expect(model.get('userName')).to.equal(options.payload.data.attributes['user-name']);
+          expect(model.get('userEmail')).to.equal(options.payload.data.attributes['user-email']);
           done();
         });
 
       });
     });
 
-    it("should return persisted assessement", function (done) {
+    it('should return persisted assessement', function (done) {
 
       // when
       server.injectThen(options).then((response) => {
@@ -177,9 +200,9 @@ describe('Acceptance | API | Assessments', function () {
 
         // then
         expect(assessment.id).to.exist;
-        expect(assessment.attributes["user-id"]).to.equal(options.payload.data.attributes["user-id"]);
-        expect(assessment.attributes["user-name"]).to.equal(options.payload.data.attributes["user-name"]);
-        expect(assessment.attributes["user-email"]).to.equal(options.payload.data.attributes["user-email"]);
+        expect(assessment.attributes['user-id']).to.equal(options.payload.data.attributes['user-id']);
+        expect(assessment.attributes['user-name']).to.equal(options.payload.data.attributes['user-name']);
+        expect(assessment.attributes['user-email']).to.equal(options.payload.data.attributes['user-email']);
         expect(assessment.relationships.course.data.id).to.equal(options.payload.data.relationships.course.data.id);
 
         done();
@@ -188,15 +211,15 @@ describe('Acceptance | API | Assessments', function () {
 
   });
 
-  describe('GET /api/assessments/:assessment_id/next', function () {
+  describe('(non-adaptative) GET /api/assessments/:assessment_id/next', function () {
 
     const assessmentData = {
-      method: "POST", url: "/api/assessments", payload: {
+      method: 'POST', url: '/api/assessments', payload: {
         data: {
-          type: "assessment",
+          type: 'assessment',
           attributes: {
-            "user-name": 'Jon Snow',
-            "user-email": 'jsnow@winterfell.got'
+            'user-name': 'Jon Snow',
+            'user-email': 'jsnow@winterfell.got'
           },
           relationships: {
             course: {
@@ -210,9 +233,9 @@ describe('Acceptance | API | Assessments', function () {
       }
     };
 
-    it("should return 200 HTTP status code", function (done) {
+    it('should return 200 HTTP status code', function (done) {
       server.injectThen(assessmentData).then((response) => {
-        const challengeData = { method: "GET", url: "/api/assessments/" + response.result.data.id + "/next" };
+        const challengeData = { method: 'GET', url: '/api/assessments/' + response.result.data.id + '/next' };
         server.injectThen(challengeData).then((response) => {
           expect(response.statusCode).to.equal(200);
           done();
@@ -220,9 +243,9 @@ describe('Acceptance | API | Assessments', function () {
       });
     });
 
-    it("should return application/json", function (done) {
+    it('should return application/json', function (done) {
       server.injectThen(assessmentData).then((response) => {
-        const challengeData = { method: "GET", url: "/api/assessments/" + response.result.data.id + "/next" };
+        const challengeData = { method: 'GET', url: '/api/assessments/' + response.result.data.id + '/next' };
         server.injectThen(challengeData).then((response) => {
           const contentType = response.headers['content-type'];
           expect(contentType).to.contain('application/json');
@@ -231,9 +254,9 @@ describe('Acceptance | API | Assessments', function () {
       });
     });
 
-    it("should return the first challenge if no challenge specified", function (done) {
+    it('should return the first challenge if no challenge specified', function (done) {
       server.injectThen(assessmentData).then((response) => {
-        const challengeData = { method: "GET", url: "/api/assessments/" + response.result.data.id + "/next" };
+        const challengeData = { method: 'GET', url: '/api/assessments/' + response.result.data.id + '/next' };
         server.injectThen(challengeData).then((response) => {
           expect(response.result.data.id).to.equal('first_challenge');
           done();
@@ -241,9 +264,9 @@ describe('Acceptance | API | Assessments', function () {
       });
     });
 
-    it("should return the next challenge otherwise", function (done) {
+    it('should return the next challenge otherwise', function (done) {
       server.injectThen(assessmentData).then((response) => {
-        const challengeData = { method: "GET", url: "/api/assessments/" + response.result.data.id + "/next/first_challenge" };
+        const challengeData = { method: 'GET', url: '/api/assessments/' + response.result.data.id + '/next/first_challenge' };
         server.injectThen(challengeData).then((response) => {
           expect(response.result.data.id).to.equal('second_challenge');
           done();
