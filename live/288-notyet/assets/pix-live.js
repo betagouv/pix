d60b6dd54e1134257aa8a9ad11a29900b8c5a207
Jeremy Buget <jbuget@gmail.com>
2016-12-15 14:32:52 +0000

    Release of 288-notyet with env integration (via commit hash: 4e5d2635aab21c56adf6097c9161f45c8df25aac)

--- live/288-notyet/assets/pix-live.js
+++ live/288-notyet/assets/pix-live.js
@@ -2818,7 +2818,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"2.1.1+1359437b"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"2.1.1+4e5d2635"});
 }
 
 /* jshint ignore:end */
