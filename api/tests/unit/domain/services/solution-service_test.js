4ec333ac50cef1abd1a5b02d7da1ecbf9fc0bb96
David <davbohr@gmail.com>
2016-12-20 17:30:44 +0100

    Add test

--- api/tests/unit/domain/services/solution-service_test.js
+++ api/tests/unit/domain/services/solution-service_test.js
@@ -6,10 +6,11 @@ const Solution = require('../../../../lib/domain/models/referential/solution');
 
 describe('Unit | Service | SolutionService', function () {
 
-  function buildSolution(type, value) {
+  function buildSolution(type, value, scoring) {
     const solution = new Solution({ id: 'solution_id' });
     solution.type = type;
     solution.value = value;
+    solution.scoring = scoring;
     return solution;
   }
 
@@ -195,6 +196,41 @@ describe('Unit | Service | SolutionService', function () {
           expect(service.match(answer, solution)).to.equal('ok');
         });
       });
+
+    });
+
+    describe('if solution type is QROCM-dep with scoring', function () {
+
+      it('should return "ko" for badly formatted solution', function () {
+        const answer = buildAnswer('num1: Google\nnum2: Yahoo');
+        const solution = buildSolution('QROCM-dep', 'solution like a QCU', '1: @acquix');
+        expect(service.match(answer, solution)).to.equal('ko');
+      });
+
+      // it('should return "ko" when answer is incorrect', function () {
+      //   const answer = buildAnswer('num1: Foo\nnum2: Bar');
+      //   const solution = buildSolution('QROCM-dep', 'Google:\n- Google\n- google.fr\n- Google Search\nYahoo:\n- Yahoo\n- Yahoo Answer');
+      //   expect(service.match(answer, solution)).to.equal('ko');
+      // });
+
+      // it('should return "ko" when user duplicated a correct answer', function () {
+      //   const answer = buildAnswer('num1: google.fr\nnum2: google.fr');
+      //   const solution = buildSolution('QROCM-dep', 'Google:\n- Google\n- google.fr\n- Google Search\nYahoo:\n- Yahoo\n- Yahoo Answer');
+      //   expect(service.match(answer, solution)).to.equal('ko');
+      // });
+
+      // const maximalScoreCases = [
+      //   { answer: 'num1: " google.fr"\nnum2: "Yahoo anSwer "',
+      //     solution: 'Google:\n- Google\n- google.fr\n- Google Search\nYahoo:\n- Yahoo\n- Yahoo Answer' },
+      // ];
+
+      // maximalScoreCases.forEach(function (testCase) {
+      //   it('should return "ok" when answer is "' + testCase.answer + '" and solution is "' + escape(testCase.solution) + '"', function () {
+      //     const answer = buildAnswer(testCase.answer);
+      //     const solution = buildSolution('QROCM-dep', testCase.solution);
+      //     expect(service.match(answer, solution)).to.equal('ok');
+      //   });
+      // });
 
     });
 
