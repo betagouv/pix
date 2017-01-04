85fc3fa58d37b9b0fbe8f0c943ea2823fab22307
David <davbohr@gmail.com>
2017-01-04 11:42:21 +0100

    improvments

--- live/app/utils/labeled-checkboxes.js
+++ live/app/utils/labeled-checkboxes.js
@@ -1,33 +1,37 @@
 import _ from 'pix-live/utils/lodash-custom';
 
-// Example with 4 proposals
-// Proposals could be ['prop 1','prop 2','prop 3','prop 4']
-// Answers given are only the first(s) : [false, true], the missing values are false
-// Output expected : [['prop 1', false], ['prop 2', true], ['prop 3', false], ['prop 4', false]]
 
-function calculate(proposals, answers) {
-  // Example
-  // proposals = ['prop 1','prop 2','prop 3','prop 4']
-  // answers = [false, true]
-  const sizeDifference = proposals.length - answers.length;
-
-  return  _.chain(answers)                                      // [false, true]
-            .concat(_.times(sizeDifference, _.constant(false))) // [false, true, false, false]
-            .zip(proposals)                                     // [[false, 'prop 1'], [true, 'prop 2'], [false, 'prop 3'], [false, 'prop 4']]
-            .map(_.reverse)                                     // [['prop 1', false], ['prop 2', true], ['prop 3', false], ['prop 4', false]]
-            .value();
-}
-
-export default function labeledCheckboxes (proposals, answers) {
-
-  const DEFAULT_RETURN_VALUE = [];
+/*
+ * Example :
+ * => Input :
+ *     proposals :  ['is sky red ?' , 'is sun red ?' , 'is grass red ?' , 'is cloud red ?']
+ * => Input :
+ *     userAnswers :  [false, true]
+ *
+ * WARNING : only first(s) userAnswers are given,
+ *           all others have implicitly the boolean value "false"
+ *
+ * => Output :
+ *    [['is sky red ?', false],
+ *     ['is sun red ?', true],
+ *     ['is grass red ?', false],
+ *     ['are clouds red ?' false]]
+ */
+export default function labeledCheckboxes (proposals, userAnswers) {
 
   // check pre-conditions
-  if (_(proposals).isEmpty())            return DEFAULT_RETURN_VALUE;
-  if (_(proposals).isNotArrayOfString()) return DEFAULT_RETURN_VALUE;
-  if (_(answers).isNotArrayOfBoolean())  return DEFAULT_RETURN_VALUE;
+  if (_(proposals).isNotArrayOfString())            return [];
+  if (_(proposals).isEmpty())                       return [];
+  if (_(userAnswers).isNotArrayOfBoolean())         return [];
+  if (_(userAnswers).size() > _(proposals).size())  return [];
 
-  return calculate(proposals, answers);
+  const sizeDifference    = _(proposals).size() - _(userAnswers).size(); // 2
+  const arrayOfFalse = _.times(sizeDifference, _.constant(false));       // [false, false]
 
+  return  _.chain(userAnswers)      // [false, true]
+            .concat(arrayOfFalse)   // [false, true, false, false]
+            .zip(proposals)         // [[false, 'prop 1'], [true, 'prop 2'], [false, 'prop 3'], [false, 'prop 4']]
+            .map(_.reverse)         // [['prop 1', false], ['prop 2', true], ['prop 3', false], ['prop 4', false]]
+            .value();
 
 }
