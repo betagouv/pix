85541341cca2e8c9ca0bcafbf6ba49d186065f69
David <davbohr@gmail.com>
2017-01-04 15:14:40 +0100

    debugged, 2

--- live/app/models/challenge.js
+++ live/app/models/challenge.js
@@ -9,7 +9,7 @@ const ChallengeModel = Model.extend(InstructionAsObject, ProposalsAsArrayMixin, 
 
   instruction: attr('string'),
   proposals: attr('string'),
-  hasInternetAndTools: attr('string'),
+  hasInternetAndTools: attr('boolean'),
   illustrationUrl: attr('string'),
   type: attr('string'),
   attachmentUrl: attr('string'),
