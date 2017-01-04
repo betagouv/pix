cf12ae28476be7e4e3454be121a02269150a277a
David <davbohr@gmail.com>
2017-01-04 17:56:33 +0100

    start front dev

---
+++ live/app/components/timeout-jauge.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  // remainingMinutes: Ember.computed('remainingTime', function() {
+  //   return this.get('baz.foo') + ' ' + this.get('baz.bar');
+  // });
+});
