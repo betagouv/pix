4f4beec9d10fd5a6d5815b480196cb65ee82bfc5
Jeremy Buget <jbuget@gmail.com>
2017-01-09 14:55:45 +0000

    Release of 180-rebased with env integration (via commit hash: 553fdb930868cbe22b84b8b6e3d57c0d0b5f0802)

--- live/180-rebased/assets/pix-live.js
+++ live/180-rebased/assets/pix-live.js
@@ -3205,7 +3205,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"3.0.0+9b9f4bc6"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"3.0.0+553fdb93"});
 }
 
 /* jshint ignore:end */
