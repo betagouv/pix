d8b13c42d2664319f598b862884a130b0a36b78c
David <davbohr@gmail.com>
2017-01-10 16:25:37 +0100

    ability to test mobile, all tests added

--- live/tests/acceptance/a4-demarrer-un-test-test.js
+++ live/tests/acceptance/a4-demarrer-un-test-test.js
@@ -20,7 +20,7 @@ describe('Acceptance | a4 - Démarrer un test |', function () {
     destroyApp(application);
   });
 
-  before(function () {
+  beforeEach(function () {
     return visit('/');
   });
 
@@ -29,13 +29,13 @@ describe('Acceptance | a4 - Démarrer un test |', function () {
     expect($startLink.text()).to.contains('Démarrer le test');
   });
 
-  // it('a4.2 Quand je démarre un test, je suis redirigé vers la première épreuve du test', function() {
-  //   const $startLink = findWithAssert('div[data-id="ref_course_id"] .start-button')[0];
-  //   return click($startLink).then(function() {
-  //     findWithAssert('#assessment-challenge');
-  //     expect(currentURL()).to.contains('/assessments/ref_assessment_id/challenges/ref_qcm_challenge_id');
-  //   });
-  // });
+  it('a4.2 Quand je démarre un test, je suis redirigé vers la première épreuve du test', function() {
+    const $startLink = findWithAssert('.start-button');
+    return click($startLink).then(function() {
+      findWithAssert('#assessment-challenge');
+      expect(currentURL()).to.contains('/assessments/ref_assessment_id/challenges/ref_qcm_challenge_id');
+    });
+  });
 
   it('a4.3 Quand je démarre un test sur mobile, une modale m\'averti que l\'expérience ne sera pas optimale', function(done) {
     const $startLink = findWithAssert('.start-button');
@@ -49,13 +49,13 @@ describe('Acceptance | a4 - Démarrer un test |', function () {
     });
     click($startLink);
     andThen(() => {
-      // XXX : ickiest hack : wait 200ms for bootstrap transition to complete
+      // XXX : ickiest hack : wait 300ms for bootstrap transition to complete
       setTimeout(function() {
         expect($jsModalMobile.css('display')).to.equals('block');
         expect(currentURL()).to.equals('/');
         $('button[data-dismiss]').click();
         done();
-      }, 400);
+      }, 300);
     });
   });
 
