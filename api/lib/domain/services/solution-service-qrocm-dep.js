7889746ae40ec2ae3318c429460375a79da965ab
Brandone Martins <brandone.martins@gmail.com>
2017-01-18 18:52:19 +0100

    refacto fonction match pour qroc-m dep et ajout de test

--- api/lib/domain/services/solution-service-qrocm-dep.js
+++ api/lib/domain/services/solution-service-qrocm-dep.js
@@ -1,64 +1,99 @@
 /*eslint no-console: ["error", { allow: ["warn", "error"] }] */
-const utils = require('./solution-service-utils');
 const jsYaml = require('js-yaml');
 const _ = require('../../utils/lodash-utils');
-const util = require('util');
 
+_applyTreatments = (objects) => {
+  const result = {};
+  _.each(objects, (value, key) => {
+    result[key] = value.toString().trim().toLowerCase();
+  });
+  return result;
+};
 
-_rightNumberOfAnswers = function (answers, nbExpectedAnswers) {
+_getSolutionKeys = (solutions) => {
+  return Object.keys(solutions);
+};
+
+_removeMatchedSolutionIfExist = (matchingSolutionKey, solutions) => {
+  if (matchingSolutionKey) {
+    solutions = _.omit(solutions, matchingSolutionKey);
+  }
+  return solutions;
+};
+
+_rightNumberOfAnswers = (answers, nbExpectedAnswers) => {
   return Object.keys(answers).length == nbExpectedAnswers;
-}
+};
+
+_hasBadAnswers = (validations) => {
+  let badAnswers = _.filter(validations, (item) => item === false);
+  return !_.isEmpty(badAnswers);
+};
+
+_compareAnswersAndSolutions = (answers, solutions) => {
+  const validations = {};
+  _.each(answers, (answer) => {
+    validations[answer] = false;
+    const solutionKeys = _getSolutionKeys(solutions);
+    let matchingSolutionKey = null;
+    _.each(solutionKeys, (solutionKey) => {
+      if (validations[answer] == false) {
+        const solutionVariants = solutions[solutionKey];
+        if (!_.isEmpty(answer) && solutionVariants.includes(answer)) {
+          validations[answer] = true;
+          matchingSolutionKey = solutionKey;
+        }
+      }
+    });
+    solutions = _removeMatchedSolutionIfExist(matchingSolutionKey, solutions);
+  });
+  return validations;
+};
+
+_calculateResult = (scoring, validations, answers) => {
+  let result = 'ok';
+
+  if (_.isEmpty(scoring)) {
+    if (_hasBadAnswers(validations)) {
+      result = 'ko';
+    }
+  } else {
+    if (!_hasBadAnswers(validations)) {
+      result = 'ok';
+    } else {
+      const nbGoodAnswers = _.filter(validations, (item) => item == true).length;
+      const minGrade = _.min(Object.keys(scoring));
+      const maxGrade = _.max(Object.keys(scoring));
+
+      if (nbGoodAnswers >= maxGrade) {
+        result = 'ok';
+      } else if (nbGoodAnswers >= minGrade) {
+        result = 'partially';
+      } else {
+        result = 'ko';
+      }
+    }
+  }
+  return result;
+};
 
 module.exports = {
 
-  match (yamlAnswer, yamlSolution, yamlScoring, nbExpectedAnswers) {
+  match (yamlAnswer, yamlSolution, yamlScoring) {
+    // Convert Yaml to JS objects
+    let answers = jsYaml.safeLoad(yamlAnswer);
+    let solutions = jsYaml.safeLoad(yamlSolution);
+    const scoring = jsYaml.safeLoad(yamlScoring);
 
-    const answers = jsYaml.safeLoad(yamlAnswer);
-    const solutions = jsYaml.safeLoad(yamlSolution);
+    // Treatments
+    answers = _applyTreatments(answers);
+    solutions = _applyTreatments(solutions);
 
-    if (! _rightNumberOfAnswers(answers, nbExpectedAnswers)) {
-      return "ko";
-    }
+    // Comparisons
+    const validations = _compareAnswersAndSolutions(answers, solutions);
 
-    const validations = {};
-
-    _.each(answers, (answer) => {
-      validations[answer] = false;
-      const solutionKeys = Object.keys(solutions);
-      if (solutionKeys.includes(answer)) {
-        validations[answer] = true;
-      }
-      else {
-        _.each(solutionKeys, (solutionKey) => {
-          if (validations[answer] == false) {
-            const solutionVariants = solutions[solutionKey];
-            if (solutionVariants.includes(answer)) {
-              validations[answer] = true;
-            }
-          }
-        })
-      }
-    })
-
-
-    /*_.each(validations, (validation) => {
-      console.log('***************************' + validation);
-      if (!validation){
-        //return "ko";
-        console.log('here');
-      }
-    });*/
-
-    let res = _.find(validations,function(item){
-        return item === false;
-    });
-
-    if (typeof res !== 'undefined')
-      return "ko";
-
-
-
-    return "ok";
+    // Restitution
+    return _calculateResult(scoring, validations, answers);
   }
 
 
--- api/tests/unit/domain/services/solution-service-qrocm-dep_test.js
+++ api/tests/unit/domain/services/solution-service-qrocm-dep_test.js
@@ -4,47 +4,141 @@ describe.only('Unit | Service | SolutionServiceQrocmDep', function () {
 
   describe('#match', function () {
 
-    describe('when expected solutions are strings', function () {
+    describe('when challenge has no scoring defined', function () {
 
-      const solution = `Google:\n- google\n- google.fr\n- google.com\n- google search\nYahoo:\n- yahoo\n- yahoo search\n- yahoo.fr\n- yahoo.com\nAltavista:\n- altavista\n- altavista.fr\n- altavista.com\nBing:\n- bing\n- bing.fr\n- bing.com`;
-      const scoring = null;
+      describe('when expected solutions are strings', function () {
 
-      it('should return "ok" if given answer matches solution', function () {
-        // given
-        const goodAnswer = `moteur 1: yahoo\nmoteur 2: google\nmoteur 3: bing`;
-        // when
-        const result = service.match(goodAnswer, solution, scoring, 3);
-        // then
-        expect(result).to.equal('ok');
-      });
-
-      [
-        `moteur 1: yahoo\nmoteur 2: google\nmoteur 3: mauvaise_reponse`,
-        `moteur 1: yahoo\nmoteur 2: google`
-      ].forEach(function (badAnswer) {
-        it(`should return "ko" if the given answer doesn't match the solution (answer = ${badAnswer})`, function () {
-          const result = service.match(badAnswer, solution, scoring, 3);
-          expect(result).to.equal('ko');
-        });
-      });
-
-      describe.skip('when expected solutions are numbers', function () {
+        const solution = `Google:\n- google\n- google.fr\n- google.com\n- google search\nYahoo:\n- yahoo\n- yahoo search\n- yahoo.fr\n- yahoo.com\nAltavista:\n- altavista\n- altavista.fr\n- altavista.com\nBing:\n- bing\n- bing.fr\n- bing.com`;
+        const scoring = null;
 
         it('should return "ok" if given answer matches solution', function () {
           // given
-          const yamlAnswer = `num1: '4'\nnum2: '2'\nnum3: '1'\nnum4: '3'`;
-          const yamlSolution = `num1:\n  - 4\nnum2:\n  - 2\nnum3:\n  - 1\nnum4:\n  - 3`;
-          const yamlScoring = null;
-
+          const goodAnswer = `moteur 1: yahoo\nmoteur 2: google\nmoteur 3: bing`;
           // when
-          const result = service.match(yamlAnswer, yamlSolution, yamlScoring);
-
+          const result = service.match(goodAnswer, solution, scoring);
           // then
           expect(result).to.equal('ok');
         });
+
+        [
+          `moteur 1: yahoo\nmoteur 2: google\nmoteur 3: mauvaise_reponse`,
+          `moteur 1: yahoo\nmoteur 2: google\nmoteur 3: ''`
+        ].forEach(function (badAnswer) {
+          it(`should return "ko" if the given answer doesn't match the solution (answer = ${badAnswer})`, function () {
+            const result = service.match(badAnswer, solution, scoring);
+            expect(result).to.equal('ko');
+          });
+        });
       });
+
+      describe('when expected solutions are numbers', function () {
+
+        const solution = `solA:\n- 1\n- 2\n- 3\nsolB:\n- 4\n- 5\n- 6`;
+        const scoring = null;
+
+        [
+          `solA: '2'\nsolB: '4'`,
+          `solA: '4'\nsolB: '2'`
+        ].forEach(function (goodAnswer) {
+          it(`should return "ok" if given answer matches solution (answer = ${goodAnswer})`, function () {
+            const result = service.match(goodAnswer, solution, scoring);
+            expect(result).to.equal('ok');
+          });
+        });
+
+
+        [
+          `solA: '2'\nsolB: '888'`,
+          `solA: '888'\nsolB: '2'`,
+          `solA: '1'\nsolB: '2'`,
+          `solA: '4'\nsolB: '5'`,
+          `solA: '1'\nsolB: ''`,
+        ].forEach(function (badAnswer) {
+          it(`should return "ko" if given answer doesn't matches solution (answer = ${badAnswer})`, function () {
+            const result = service.match(badAnswer, solution, scoring);
+            expect(result).to.equal('ko');
+          });
+        });
+
+      });
+
     });
+
+    describe('when challenge has scoring', function () {
+
+      describe('when expected solutions are strings', function () {
+
+        const solution = `Google:\n- google\n- google.fr\n- google.com\n- google search\nYahoo:\n- yahoo\n- yahoo search\n- yahoo.fr\n- yahoo.com\nAltavista:\n- altavista\n- altavista.fr\n- altavista.com\nBing:\n- bing\n- bing.fr\n- bing.com`;
+        const scoring = `1: "@rechinfo1"\n2: "@rechinfo2"\n3: "@rechinfo3"`;
+
+        [
+          `moteur 1: 'yahoo'\nmoteur 2: 'google'\nmoteur 3: 'bing'`,
+          `moteur 1: 'YAHOO'\nmoteur 2: 'Google'\nmoteur 3: 'binG'`,
+          `moteur 1: 'yahoo  '\nmoteur 2: ' google'\nmoteur 3: '  bing   '`
+        ].forEach(function (answer) {
+          it(`should return "ok" if given answer matches solution (answer = ${answer})`, function () {
+            const result = service.match(answer, solution, scoring);
+            expect(result).to.equal('ok');
+          });
+        });
+
+
+        [
+          `moteur 1: 'yahoo'\nmoteur 2: 'google'\nmoteur 3: 'mauvaise_reponse'`,
+          `moteur 1: 'yahoo'\nmoteur 2: 'google'\nmoteur 3: ''`
+        ].forEach(function (answer) {
+          it(`should return "partially" if the given answer doesn't totally match the solution (answer = ${answer})`, function () {
+            const result = service.match(answer, solution, scoring);
+            expect(result).to.equal('partially');
+          });
+        });
+
+        [
+          `moteur 1: 'facebook'\nmoteur 2: ''\nmoteur 3: 'mauvaise_reponse'`,
+          `moteur 1: 'facebook'\nmoteur 2: 'twitter'\nmoteur 3: ''`
+        ].forEach(function (answer) {
+          it(`should return "ko" if the given answer doesn't match the solution (answer = ${answer})`, function () {
+            const result = service.match(answer, solution, scoring);
+            expect(result).to.equal('ko');
+          });
+        });
+
+
+      });
+
+      describe('when expected solutions are numbers', function () {
+
+        const solution = `solA:\n- 1\n- 2\n- 3\nsolB:\n- 4\n- 5\n- 6`;
+        const scoring = null;
+
+        [
+          `solA: '2'\nsolB: '4'`,
+          `solA: '4'\nsolB: '2'`
+        ].forEach(function (goodAnswer) {
+          it(`should return "ok" if given answer matches solution (answer = ${goodAnswer})`, function () {
+            const result = service.match(goodAnswer, solution, scoring);
+            expect(result).to.equal('ok');
+          });
+        });
+
+        [
+          `solA: '2'\nsolB: '888'`,
+          `solA: '888'\nsolB: '2'`,
+          `solA: '1'\nsolB: '2'`,
+          `solA: '4'\nsolB: '5'`,
+          `solA: '1'\nsolB: ''`,
+        ].forEach(function (badAnswer) {
+          it(`should return "ko" if given answer doesn't matches solution (answer = ${badAnswer})`, function () {
+            const result = service.match(badAnswer, solution, scoring);
+            expect(result).to.equal('ko');
+          });
+        });
+
+      });
+
+    });
+
   });
-  });
+});
 
 
