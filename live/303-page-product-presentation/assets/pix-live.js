c744542ae4400d761a2e4a0fa84f9711c0133c93
Jeremy Buget <jbuget@gmail.com>
2017-01-18 14:55:54 +0000

    Release of 303-page-product-presentation with env integration (via commit hash: 19262c2bb4fccfd46e559f14cea4927a0e5f34f7)

--- live/303-page-product-presentation/assets/pix-live.js
+++ live/303-page-product-presentation/assets/pix-live.js
@@ -3581,7 +3581,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"3.1.1+134a630b"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"3.1.1+19262c2b"});
 }
 
 /* jshint ignore:end */
