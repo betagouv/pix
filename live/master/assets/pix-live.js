746697a1118fb49a2a2ac490f76492546b3e2fe7
Jeremy Buget <jbuget@gmail.com>
2017-01-19 17:32:52 +0000

    Release of master with env integration (via commit hash: eef995b3b2e4c86806633c26fee23276b8f11497)

--- live/master/assets/pix-live.js
+++ live/master/assets/pix-live.js
@@ -3581,7 +3581,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"4.0.0+81e6e32e"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"4.0.0+eef995b3"});
 }
 
 /* jshint ignore:end */
