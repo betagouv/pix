674a3ddd8958a1d486dc15de7d20cb7fc9cdebeb
Jeremy Buget <jbuget@gmail.com>
2017-01-20 10:42:50 +0000

    Release of infra-improve-pix-cli with env integration (via commit hash: 313f099bd424d07f6b748fc819960a2699166730)

--- live/infra-improve-pix-cli/assets/pix-live.js
+++ live/infra-improve-pix-cli/assets/pix-live.js
@@ -3581,7 +3581,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"4.0.0+18521515"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"4.0.0+313f099b"});
 }
 
 /* jshint ignore:end */
