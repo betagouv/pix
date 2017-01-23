5e17ba5a49b7e105b953795d4c905294020f986e
Jeremy Buget <jbuget@gmail.com>
2017-01-23 14:40:15 +0000

    Release of cleanup-add-tests with env integration (via commit hash: 06cdc1980d37b27d1faec6415c2d8ce9ee7142f9)

--- live/cleanup-add-tests/assets/pix-live.js
+++ live/cleanup-add-tests/assets/pix-live.js
@@ -3517,7 +3517,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"3.1.1+0b4e90cd"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"3.1.1+06cdc198"});
 }
 
 /* jshint ignore:end */
