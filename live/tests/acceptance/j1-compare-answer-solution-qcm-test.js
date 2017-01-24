0366b4d82961d120c0ca861636ff332e2c478e15
Brandone Martins <brandone.martins@gmail.com>
2017-01-24 16:51:17 +0100

    test acceptance en place

--- live/tests/acceptance/j1-compare-answer-solution-qcm-test.js
+++ live/tests/acceptance/j1-compare-answer-solution-qcm-test.js
@@ -14,10 +14,14 @@ describe('Acceptance | j1 - Comparer réponses et solutions pour un QCM |', func
     destroyApp(application);
   });
 
-  describe('a', function () {
+  describe(`Affiche sur la ligne de l'épreuve le mot REPONSE pour un QCM sur l'écran des résultats`, function () {
 
-    it('j1.1 ', function () {
-      expect(true).to.equal(true);
+    it(`j1.1 il l'affiche pour un QCM mais pas pour les autres types d'epreuves` , function () {
+      visit('/assessments/ref_assessment_id/results');
+      andThen(() => {
+        expect($('.assessment-results-list-item:eq(0) .js-answer').text()).to.contain('RÉPONSE'); //QCM
+        expect($('.assessment-results-list-item:eq(1) .js-answer').text()).not.to.contain('RÉPONSE'); //QCU
+      });
     });
 
   });
