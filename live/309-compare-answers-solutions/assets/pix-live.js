8742fd530ddfb3b771aabe122797711c0d600124
Jeremy Buget <jbuget@gmail.com>
2017-01-25 10:35:56 +0000

    Release of 309-compare-answers-solutions with env integration (via commit hash: 65bf3e98549516af9b83a51751ffa7734a075691)

--- live/309-compare-answers-solutions/assets/pix-live.js
+++ live/309-compare-answers-solutions/assets/pix-live.js
@@ -3606,7 +3606,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"4.0.0+194cbfdd"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"4.0.0+65bf3e98"});
 }
 
 /* jshint ignore:end */
