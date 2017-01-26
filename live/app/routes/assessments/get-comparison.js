c20d7fb3541fedb03fe0ac2a70e71a85583e36b7
Brandone Martins <brandone.martins@gmail.com>
2017-01-26 12:57:27 +0100

    wip

--- live/app/routes/assessments/get-comparison.js
+++ live/app/routes/assessments/get-comparison.js
@@ -1,5 +1,52 @@
 import Ember from 'ember';
 import ModalRouteMixin from 'ember-routable-modal/mixins/route';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend(ModalRouteMixin, {
+
+  _urlForSolution: function (adapter, assessmentId, answerId) {
+    return adapter.buildURL('assessment', assessmentId) + '/solutions/' + answerId;
+  },
+
+  model(params) {
+    const adapter = this.get('store').adapterFor('application');
+    const store = this.get('store');
+
+    const assessmentId = params.assessment_id;
+    const answerId = params.answer_id;
+
+    return store.findRecord('answer', answerId).then((answer) => {
+      return store.findRecord('challenge', answer.get('challenge.id')).then((challenge) => {
+        return adapter.ajax(this._urlForSolution(adapter, assessmentId, answerId), 'GET')
+          .then(solution => {
+            return RSVP.hash({
+              answer,
+              challenge,
+              solution
+            });
+          });
+      });
+    });
+
+
+  },
+
+  // model(params) {
+
+  //   const store = this.get('store');
+
+  //   return store.findRecord('course', params.course_id).then((course) => {
+
+  //     // No auth yet, therefore userName and userEmail are null.
+  //     return store
+  //     .createRecord('assessment', { course, userName:null, userEmail:null })
+  //     .save()
+  //     .then((assessment) => {
+  //       return RSVP.hash({
+  //         assessment
+  //       });
+  //     });
+  //   });
+  // },
+
 });
