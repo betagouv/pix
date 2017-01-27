1f71ae19a2d4c6f2ddcc7af1a8039623d359237e
Jeremy Buget <jbuget@gmail.com>
2017-01-27 11:32:48 +0000

    Release of correction-bug-CROQM-ind with env integration (via commit hash: fa8d6665656ef587d3ed340c09b21aaf9552b888)

--- live/correction-bug-CROQM-ind/assets/pix-live.js
+++ live/correction-bug-CROQM-ind/assets/pix-live.js
@@ -3517,7 +3517,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"3.1.1+087cabe6"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"3.1.1+fa8d6665"});
 }
 
 /* jshint ignore:end */
