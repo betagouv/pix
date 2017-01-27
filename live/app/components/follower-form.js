2df00363d6e3dab5fb625d078e9fdaa78b5d8221
Florian <fenoh@octo.com>
2017-01-27 11:31:57 +0100

    Add saveFollower method

--- live/app/components/follower-form.js
+++ live/app/components/follower-form.js
@@ -5,14 +5,29 @@ export default Ember.Component.extend({
   hasError: false,
   email: '',
 
-  _hasValidEmail(context){
-    return context.get('emailValidator').emailIsValid(context.get('email').trim());
-  },
 
   errorMessage:  Ember.computed('hasError', function(){
     return (this.get('hasError'))? 'Veuillez rentrer une adresse mail valide SVP !' : '';
   }),
 
+  _hasValidEmail(context){
+    return context.get('emailValidator').emailIsValid(context.get('email').trim());
+  },
+
+  _saveFollower(email,context){
+    $.ajax({
+      url: 'http://localhost:3000/api/followers',
+      method: 'POST',
+      data: {email: email},
+      dataType: 'json',
+      success: function (res) {
+        context.set('hasError',false);
+      },
+      error: function (err) {
+        context.set('hasError',true);
+      }
+    });
+  },
   actions: {
     updateEmail(){
       this.set('email',event.target.value);
@@ -23,7 +38,7 @@ export default Ember.Component.extend({
         this.set('hasError', true);
         return false;
       }
-
+      return this._saveFollower(this.get('email').trim(),this);
     }
   }
 });
