1fb07a598751ecb25f351e42070920c559e88644
Florian <fenoh@octo.com>
2017-01-30 10:38:13 +0100

    Add method to check if email is already saved

--- api/lib/application/followers/follower-controller.js
+++ api/lib/application/followers/follower-controller.js
@@ -3,7 +3,17 @@ const Follower = require('../../domain/models/data/follower');
 
 module.exports = {
 
+  isExistingEMail(email){
+    new Follower({ email: remail })
+      .fetch()
+      .then((follower) => true)
+      .catch((err) => false);
+
+  },
+
   save(request, reply) {
+    if(this.isExistingEMail(request.payload)) return reply(Boom.conflict(err));
+
     new Follower(request.payload)
       .save()
       .then((follower) => reply(follower).code(201))
--- api/tests/unit/application/followers/follower-controller_test.js
+++ api/tests/unit/application/followers/follower-controller_test.js
@@ -8,14 +8,14 @@ describe('Unit | Controller | FollowerController', function () {
 
   before(function () {
     server = this.server = new Hapi.Server();
-    server.connection({ port: null });
-    server.register({ register: require('../../../../lib/application/followers') });
+    server.connection({port: null});
+    server.register({register: require('../../../../lib/application/followers')});
   });
 
   describe('#Save', function () {
 
     let stub;
-    const follower = new Follower({ "email": "test@follower.pix" });
+    const follower = new Follower({"email": "test@follower.pix"});
 
     before(function () {
       stub = sinon.stub(Follower.prototype, 'save');
@@ -30,26 +30,38 @@ describe('Unit | Controller | FollowerController', function () {
       stub.resolves(follower);
 
       // when
-      server.inject({ method: 'POST', url: '/api/follower-form-test.js', payload : JSON.stringify(follower) }, (res) => {
-
-        // then
-        expect(res.result).to.deep.equal(follower);
-        expect(res.statusCode).to.equal(201);
-        done();
-      });
+      server.inject({method: 'POST', url: '/api/follower-form-test.js', payload: JSON.stringify(follower)},
+        (res) => {
+          // then
+          expect(res.result).to.deep.equal(follower);
+          expect(res.statusCode).to.equal(201);
+          done();
+        });
     });
 
-    it('should return an error 500 when the fetch fails', function(done) {
+    it('should return false when follower is saved because email is already saved', function (done) {
       // given
-      stub.rejects(new Error('Fetch error'));
+      stub.rejects(new Error('Save fails'));
 
       // when
-      server.inject({ method: 'POST', url: '/api/follower-form-test.js', payload : JSON.stringify(follower) }, (res) => {
+      server.inject({method: 'POST', url: '/api/follower-form-test.js', payload: JSON.stringify(follower)},
+        (res) => {
+          // then
+          expect(res.statusCode).to.equal(200);
+          done();
+        });
+    });
 
-        // then
-        expect(res.statusCode).to.equal(500);
-        done();
-      });
+    it('should return an error 500 when the Save fails', function (done) {
+      // given
+      stub.rejects(new Error('Save error'));
+      // when
+      server.inject({method: 'POST', url: '/api/follower-form-test.js', payload: JSON.stringify(follower)},
+        (res) => {
+          // then
+          expect(res.statusCode).to.equal(500);
+          done();
+        });
     });
   });
 
