371d3e9d4b505d7b835cbe74497775f57d20285f
David <davbohr@gmail.com>
2017-01-31 15:49:41 +0100

    merged clean code and added modal access test

--- live/tests/acceptance/j1-compare-answer-solution-qcm-test.js
+++ live/tests/acceptance/j1-compare-answer-solution-qcm-test.js
@@ -1,8 +1,13 @@
+import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
 
 describe('Acceptance | j1 - Comparer réponses et solutions pour un QCM |', function () {
+
+  const RESULT_URL = '/assessments/ref_assessment_id/results';
+  const COMPARISON_MODAL_URL = '/assessments/ref_assessment_id/results/compare/ref_answer_qcm_id/1';
+
 
   let application;
 
@@ -14,16 +19,34 @@ describe('Acceptance | j1 - Comparer réponses et solutions pour un QCM |', func
     destroyApp(application);
   });
 
-  describe('Affiche sur la ligne de l\'épreuve le mot REPONSE pour un QCM sur l\'écran des résultats', function () {
-
-    it('j1.1 il l\'affiche pour un QCM mais pas pour les autres types d\'épreuves' , function () {
-      visit('/assessments/ref_assessment_id/results');
-      andThen(() => {
-        expect($('.assessment-results-list-item:eq(0) .js-correct-answer').text()).to.contain('RÉPONSE'); //QCM
-        expect($('.assessment-results-list-item:eq(1) .js-correct-answer').text()).not.to.contain('RÉPONSE'); //QCU
-      });
+  describe('j1.1 Affiche sur la ligne de l\'épreuve le mot REPONSE pour un QCM sur l\'écran des résultats', function () {
+    it('j1.1.1 il l\'affiche pour un QCM mais pas pour les autres types d\'épreuves' , async function () {
+      await visit(RESULT_URL);
+      expect($('.assessment-results-list-item:eq(0) .js-correct-answer').text()).to.contain('RÉPONSE'); //QCM
+      expect($('.assessment-results-list-item:eq(1) .js-correct-answer').text()).not.to.contain('RÉPONSE'); //QCU
+      expect($('.assessment-results-list-item:eq(2) .js-correct-answer').text()).not.to.contain('RÉPONSE'); //QRU
+      expect($('.assessment-results-list-item:eq(3) .js-correct-answer').text()).not.to.contain('RÉPONSE'); //QROC
+      expect($('.assessment-results-list-item:eq(4) .js-correct-answer').text()).not.to.contain('RÉPONSE'); //QROCM
     });
+  });
 
+  describe('j1.2 Accès à la modale', function () {
+    it('j1.2.2 On peut accèder directement à la modale via URL et fermer la modale' , async function () {
+      await visit(COMPARISON_MODAL_URL);
+      expect($('.comparison-window')).to.have.lengthOf(1);
+      // XXX test env needs the modal to be closed manually
+      await click('.close-button-container');
+      expect($('.comparison-window')).to.have.lengthOf(0);
+    });
+    it('j1.2.1 Si on clique sur REPONSE la modale s\'ouvre' , async function () {
+      await visit(RESULT_URL);
+      expect($('.comparison-window')).to.have.lengthOf(0);
+      await click('.assessment-results-result-correction-button');
+      expect($('.comparison-window')).to.have.lengthOf(1);
+      // XXX test env needs the modal to be closed manually
+      await click('.close-button-container');
+      expect($('.comparison-window')).to.have.lengthOf(0);
+    });
   });
 
 });
