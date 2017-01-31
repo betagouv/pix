c773418a3f679e4247d088e75776e6bb157636ca
Florian <fenoh@octo.com>
2017-01-31 12:11:35 +0100

    Refactoring follower controller with promises

--- api/lib/application/followers/follower-controller.js
+++ api/lib/application/followers/follower-controller.js
@@ -2,29 +2,37 @@ const Boom = require('boom');
 const Follower = require('../../domain/models/data/follower');
 const EmailValidator = require('../../domain/services/email-validator');
 
-module.exports = {
+function _assertFollowerNotExist(follower){
+  return new Promise((resolve, reject) => {
+    if (follower) {
+      reject(Boom.conflict('Follower already exist'));
+    }
+    resolve();
+  });
+}
 
+function _saveFollower(email){
+  return new Promise((resolve, reject) => {
+    new Follower(email)
+      .save()
+      .then((follower) => resolve(follower))
+      .catch((err) => reject(Boom.badImplementation()));
+  });
+}
+
+module.exports = {
   save(request, reply) {
     const email = request.payload.email.trim();
     if (!EmailValidator.emailIsValid(email)) {
       return reply(Boom.badRequest('Bad format of email provided'));
     }
-
     Follower
       .where({email})
       .fetch()
-      .then((follower) => {
-        if (follower) {
-          return reply(Boom.conflict('Follower already exist'));
-        }
-
-        new Follower(request.payload)
-          .save()
-          .then((follower) => reply(follower).code(201))
-          .catch((err) => reply(Boom.badImplementation(err)));
-      })
-      .catch((err) => reply(Boom.badImplementation(err)));
+      .then(_assertFollowerNotExist)
+      .then(_ => _saveFollower(email))
+      .then(follower => reply(follower).code(201))
+      .catch((err) => reply(err));
   }
-
 };
 
