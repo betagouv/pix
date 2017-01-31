dcbf295ac63fcfc050b548a31fc5c5f128d436d1
David <davbohr@gmail.com>
2017-01-31 14:42:46 +0100

    correcting adapter bug

--- live/app/adapters/solution.js
+++ live/app/adapters/solution.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 
 export default ApplicationAdapter.extend({
   queryRecord(modelName, clazz, query) {
-    return Ember.$.getJSON( this.host + '/' + this.namespace + '/assessments/' + query.assessmentId +  '/solutions/' + query.answerId);
+    let prefix = '/';
+    if (this.host !== '/') {
+      prefix = this.host + ('/');
+    }
+    return Ember.$.getJSON( prefix + this.namespace + '/assessments/' + query.assessmentId +  '/solutions/' + query.answerId);
   }
 });
