4b2d2d0430198757011dce419bf5dca67e682025
Jeremy Buget <jbuget@gmail.com>
2017-02-01 11:19:37 +0000

    Release of debug-circleci with env integration (via commit hash: 47c5922e1bb6a222d755d34961a2ee9f817e1000)

--- live/debug-circleci/assets/pix-live.js
+++ live/debug-circleci/assets/pix-live.js
@@ -3612,7 +3612,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"4.0.0+b7559b7a"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"4.0.0+47c5922e"});
 }
 
 /* jshint ignore:end */
