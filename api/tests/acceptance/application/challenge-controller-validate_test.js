c1e343bb66a27d450d1d2343ddafe1bb278db02e
David <davbohr@gmail.com>
2017-02-02 16:02:19 +0100

    add acceptance test

---
+++ api/tests/acceptance/application/challenge-controller-validate_test.js
@@ -0,0 +1,25 @@
+/* global describe, after, it, expect */
+const server = require('../../../server');
+
+describe.only('Acceptance | API | ChallengeController', function () {
+
+  after(function (done) {
+    server.stop(done);
+  });
+
+  // validate again all answers of the challenge
+  describe('PUT /api/challenges/:challenge_id/validate', function () {
+
+    const options = { method: 'PUT', url: '/api/challenges/challenge_1234/validate' };
+
+    it('should return 200 HTTP status code', function (done) {
+      server.injectThen(options).then((response) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+  });
+
+
+});
