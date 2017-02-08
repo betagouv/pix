233adc017c3c109c880db4cc5cd86e2aa1cf58a7
Jeremy Buget <jbuget@gmail.com>
2017-02-08 09:48:36 +0000

    Release of 332-revalidation with env integration (via commit hash: bebbdd837ffa5e04aba2efd263dd742e125d42c7)

--- live/332-revalidation/assets/pix-live.js
+++ live/332-revalidation/assets/pix-live.js
@@ -3832,7 +3832,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+bc670319"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+bebbdd83"});
 }
 
 /* jshint ignore:end */
