0c222849b023777b077bbe398a04a32e2eb4e05b
David <davbohr@gmail.com>
2017-02-08 14:37:22 +0100

    first commit

--- live/mirage/routes/get-challenge.js
+++ live/mirage/routes/get-challenge.js
@@ -6,9 +6,6 @@ import refQcuChallengeFull from '../data/challenges/ref-qcu-challenge';
 import refQruChallengeFull from '../data/challenges/ref-qru-challenge';
 import refQrocChallengeFull from '../data/challenges/ref-qroc-challenge';
 import refQrocmChallengeFull from '../data/challenges/ref-qrocm-challenge';
-import noFileChallenge from '../data/challenges/no-file-challenge';
-import oneFileChallenge from '../data/challenges/one-file-challenge';
-import multipleFilesChallenge from '../data/challenges/multiple-files-challenge';
 
 export default function (schema, request) {
 
@@ -18,10 +15,7 @@ export default function (schema, request) {
     refQcuChallengeFull,
     refQruChallengeFull,
     refQrocChallengeFull,
-    refQrocmChallengeFull,
-    noFileChallenge,
-    oneFileChallenge,
-    multipleFilesChallenge
+    refQrocmChallengeFull
   ];
 
   const challenges = _.map(allChallenges, function (oneChallenge) {
