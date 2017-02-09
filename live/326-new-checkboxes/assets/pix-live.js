8c1211f6d1068e392c6c2b960a2db83655216e84
Jeremy Buget <jbuget@gmail.com>
2017-02-09 15:31:09 +0000

    Release of 326-new-checkboxes with env integration (via commit hash: e92191f7a924b38f318203358af0011220fb5ba7)

--- live/326-new-checkboxes/assets/pix-live.js
+++ live/326-new-checkboxes/assets/pix-live.js
@@ -3832,7 +3832,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+4f202bdb"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+e92191f7"});
 }
 
 /* jshint ignore:end */
