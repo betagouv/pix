4f28dbb79740d1b216afb7f63a498ed42275e32a
Jeremy Buget <jbuget@gmail.com>
2017-02-09 12:59:19 +0000

    Release of 171-signaler-une-epreuve with env integration (via commit hash: 14bb26c612d26a69e176c3f7c22f7a04368d33dd)

--- live/171-signaler-une-epreuve/assets/pix-live.js
+++ live/171-signaler-une-epreuve/assets/pix-live.js
@@ -3956,7 +3956,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+e3f78299"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+14bb26c6"});
 }
 
 /* jshint ignore:end */
