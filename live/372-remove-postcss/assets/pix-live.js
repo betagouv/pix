26eddb6342f527042769f017deef1d8e93d3b805
Jeremy Buget <jbuget@gmail.com>
2017-02-17 16:56:11 +0000

    Release of 372-remove-postcss with env integration (via commit hash: 68c2f28b516a831845059bb513c6415ff0dffbb0)

--- live/372-remove-postcss/assets/pix-live.js
+++ live/372-remove-postcss/assets/pix-live.js
@@ -3788,7 +3788,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+a4e1fa68"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+68c2f28b"});
 }
 
 /* jshint ignore:end */
