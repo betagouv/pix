68f28e3c426a4265680040a4ada840f4157be956
Jeremy Buget <jbuget@gmail.com>
2017-02-18 15:19:49 +0000

    Release of 372-remove-postcss with env integration (via commit hash: 05713b296e242dc00f6f5934cbecd937fbb32c3d)

--- live/372-remove-postcss/assets/tests.js
+++ live/372-remove-postcss/assets/tests.js
@@ -2026,6 +2026,15 @@ define('pix-live/tests/acceptance/j1-compare-answer-solution-qcm-test.lint-test'
     });
   });
 });
+define('pix-live/tests/adapters/answer.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  describe('ESLint - adapters/answer.js', function () {
+    it('should pass ESLint', function () {
+      // precompiled test passed
+    });
+  });
+});
 define('pix-live/tests/adapters/application.lint-test', ['exports'], function (exports) {
   'use strict';
 
