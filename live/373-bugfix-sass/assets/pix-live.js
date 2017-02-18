ca3c8e475d2b9a041103eab8a9955e68f6c20cca
Jeremy Buget <jbuget@gmail.com>
2017-02-18 13:49:52 +0000

    Release of 373-bugfix-sass with env integration (via commit hash: 13d9b3a6f16884733ce5e21750cf3a3bcb31022b)

--- live/373-bugfix-sass/assets/pix-live.js
+++ live/373-bugfix-sass/assets/pix-live.js
@@ -3788,7 +3788,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+f0e54720"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+13d9b3a6"});
 }
 
 /* jshint ignore:end */
