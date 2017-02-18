6ffdc414cef85f0a27e63525323f39389f28f510
Jeremy Buget <jbuget@gmail.com>
2017-02-18 13:28:18 +0000

    Release of bugfix-sass with env integration (via commit hash: e0d7ef2661003918e26f4128eb31fa6cc3b7da81)

--- live/bugfix-sass/assets/pix-live.js
+++ live/bugfix-sass/assets/pix-live.js
@@ -3788,7 +3788,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+4f371aaf"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+e0d7ef26"});
 }
 
 /* jshint ignore:end */
