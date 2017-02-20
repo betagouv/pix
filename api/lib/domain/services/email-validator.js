484922b46229c247492bd1e21d1d977b090dff72
Florian <fenoh@octo.com>
2017-02-20 14:35:57 +0100

    Add regex stackoverflow link

--- api/lib/domain/services/email-validator.js
+++ api/lib/domain/services/email-validator.js
@@ -4,7 +4,7 @@ module.exports = {
     if (!email) {
       return false;
     }
-
+    //XXX: Cf - http://stackoverflow.com/a/46181/5430854
     const pattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
     return pattern.test(email.trim());
