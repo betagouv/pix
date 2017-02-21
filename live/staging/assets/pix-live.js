3237d47a51a37b2f3bad3d232a2fdf16e0465cd8
Jeremy Buget <jbuget@gmail.com>
2017-02-21 15:22:14 +0000

    Release of staging with env staging (via commit hash: 26eb0b42255606771a465ed699f664821a22f33e)

--- live/staging/assets/pix-live.js
+++ live/staging/assets/pix-live.js
@@ -3971,7 +3971,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","name":"pix-live","version":"5.0.0+f237aa07"});
+  require("pix-live/app")["default"].create({"API_HOST":"","name":"pix-live","version":"5.0.0+26eb0b42"});
 }
 
 /* jshint ignore:end */
