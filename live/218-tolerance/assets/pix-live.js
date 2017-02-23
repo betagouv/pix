4a872622614d1e85d7c86dd5facd0e2056614f5b
Jeremy Buget <jbuget@gmail.com>
2017-02-23 11:18:00 +0000

    Release of 218-tolerance with env integration (via commit hash: 5752ca94085192e9cbf2b84985f06321b816306f)

--- live/218-tolerance/assets/pix-live.js
+++ live/218-tolerance/assets/pix-live.js
@@ -3971,7 +3971,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","name":"pix-live","version":"1.5.0+7f4f4152"});
+  require("pix-live/app")["default"].create({"API_HOST":"","name":"pix-live","version":"1.5.0+5752ca94"});
 }
 
 /* jshint ignore:end */
