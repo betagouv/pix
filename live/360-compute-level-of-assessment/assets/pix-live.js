13f5fc033d660960755fcd073366b5e4ca79f320
Jeremy Buget <jbuget@gmail.com>
2017-02-27 18:09:41 +0000

    Release of 360-compute-level-of-assessment with env integration (via commit hash: 9b5ed6f6ab687da0d1828e58a0a8cfeb53a4d502)

--- live/360-compute-level-of-assessment/assets/pix-live.js
+++ live/360-compute-level-of-assessment/assets/pix-live.js
@@ -3832,7 +3832,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+b0c95e27"});
+  require("pix-live/app")["default"].create({"API_HOST":"/","name":"pix-live","version":"5.0.0+9b5ed6f6"});
 }
 
 /* jshint ignore:end */
