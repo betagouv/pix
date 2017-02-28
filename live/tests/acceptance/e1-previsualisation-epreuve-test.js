5c8cd6a17d52763d2b656b6caf8910d11a75073e
David <davbohr@gmail.com>
2017-02-28 15:39:36 +0100

    front test ok

--- live/tests/acceptance/e1-previsualisation-epreuve-test.js
+++ live/tests/acceptance/e1-previsualisation-epreuve-test.js
@@ -2,7 +2,7 @@ import { describe, it, beforeEach, afterEach } from 'mocha';
 import { expect } from 'chai';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
-import { resetPostRequest, bodyOfLastPostRequest, urlOfLastPostRequest } from '../helpers/shared-state';
+import { resetTestingState } from '../helpers/shared-state';
 import _ from 'pix-live/utils/lodash-custom';
 
 describe('Acceptance | e1 - Prévisualiser une épreuve | ', function () {
@@ -17,26 +17,53 @@ describe('Acceptance | e1 - Prévisualiser une épreuve | ', function () {
     destroyApp(application);
   });
 
-  describe('e1 - Prévisualiser une épreuve |', function () {
+  describe('e1 - Prévisualiser une épreuve | ', function () {
 
     beforeEach(function () {
+      // localStorage.clear();
+      resetTestingState();
       visit('/');
     });
 
     it('e1.1 Il y a une demande de création d\'un assessment avec un course vide', async function () {
-      resetPostRequest();
+      // Given
+      let postOnAssessment = localStorage.getItem('POST_ON_URL_/assessments');
+      expect(postOnAssessment).not.to.exist;
+
+      // When
       await visit('/challenges/ref_qcu_challenge_id/preview');
-      expect(urlOfLastPostRequest()).to.equal('/api/assessments');
-      expect(_.get(bodyOfLastPostRequest(), 'data.relationships.course.data.type')).to.equal('courses');
-      expect(_.get(bodyOfLastPostRequest(), 'data.type')).to.equal('assessments');
-      const idFirstChars = _.get(bodyOfLastPostRequest(), 'data.relationships.course.data.id').substring(0,4);
+
+      // Then
+      postOnAssessment = localStorage.getItem('POST_ON_URL_/assessments');
+      expect(postOnAssessment).to.exist;
+      const postOnAssessmentObj = JSON.parse(postOnAssessment);
+      expect(typeof postOnAssessment).to.equal('string');
+      expect(typeof postOnAssessmentObj).to.equal('object');
+      expect(_.get(postOnAssessmentObj, 'data.type')).to.equal('assessments');
+      const idFirstChars = _.get(postOnAssessmentObj, 'data.relationships.course.data.id').substring(0,4);
+      expect(_.get(postOnAssessmentObj, 'data.relationships.course.data.type')).to.deep.equal('courses');
       expect(idFirstChars).to.equal('null');
     });
 
     it('e1.2 On affiche l\'assessment retourné par le serveur', async function () {
+
+      // Given
       await visit('/challenges/ref_qcu_challenge_id/preview');
       expect(currentURL()).to.equal('/assessments/ref_assessment_id/challenges/ref_qcu_challenge_id');
       expect(findWithAssert('#assessment-challenge'));
+    });
+
+    it('e1.3 Il y a une demande de rafraichissement du cache des solutions', async function () {
+      // Given
+      let postOnAssessment = localStorage.getItem('POST_ON_URL_/challenges/ref_qcu_challenge_id/solution');
+      expect(postOnAssessment).not.to.exist;
+
+      // When
+      await visit('/challenges/ref_qcu_challenge_id/preview');
+
+      // Then
+      postOnAssessment = localStorage.getItem('POST_ON_URL_/challenges/ref_qcu_challenge_id/solution');
+      expect(postOnAssessment).to.exist;
     });
 
   });
