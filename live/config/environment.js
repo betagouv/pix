b323c05207185b9c90f1b86376dbc4c1064ec892
Jérémy Buget <jbuget@gmail.com>
2017-03-01 17:42:12 +0100

    Remove non-standard CSS nesting

--- live/config/environment.js
+++ live/config/environment.js
@@ -45,7 +45,7 @@ module.exports = function (environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     // Redefined in custom initializer 'initializers/configure-pix-api-host.js'
-    ENV.APP.API_HOST= 'http://localhost:3000';
+    ENV.APP.API_HOST = 'http://localhost:3000';
   }
 
   if (environment === 'test') {
@@ -60,7 +60,7 @@ module.exports = function (environment) {
 
     ENV.APP.rootElement = '#ember-testing';
     ENV.googleFonts = [];
-    ENV.APP.API_HOST= 'http://localhost:3000';
+    ENV.APP.API_HOST = 'http://localhost:3000';
   }
 
   if (environment === 'integration') {
