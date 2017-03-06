6d4178648419c511527c4b5fd595da32cf4764a2
David <davbohr@gmail.com>
2017-03-06 10:41:57 +0100

    forEach for solution-service-function-timedOut_test.js

--- api/tests/unit/domain/services/solution-service-function-timedOut_test.js
+++ api/tests/unit/domain/services/solution-service-function-timedOut_test.js
@@ -5,33 +5,25 @@ const service = require('../../../../lib/domain/services/solution-service');
 describe('Unit | Service | SolutionService', function () {
 
   describe('#_timedOut', function () {
-    it('should return "timedout" if result is partially correct and timeout is negative', function () {
-      expect(service._timedOut('partially', -5)).to.equal('timedout');
+
+    const allCases = [
+      {when: 'partially correct & timeout negative', preresult: 'partially' , timeout: -5, output: 'timedout'},
+      {when: 'correct & timeout negative', preresult: 'ok' , timeout: -5, output: 'timedout'},
+      {when: 'partially correct & timeout 0', preresult: 'partially' , timeout: 0, output: 'partially'},
+      {when: 'correct & timeout 0', preresult: 'ok' , timeout: 0, output: 'ok'},
+      {when: 'partially correct & timeout positive', preresult: 'partially' , timeout: 5, output: 'partially'},
+      {when: 'user abandoned and timeout < 0', preresult: 'aband' , timeout: -5, output: 'aband'},
+      {when: 'user abandoned and timeout 0', preresult: 'aband' , timeout: 0, output: 'aband'},
+      {when: 'user abandoned and timeout > 0', preresult: 'aband' , timeout: +5, output: 'aband'},
+
+    ];
+
+    allCases.forEach(function (caze) {
+      it(caze.when + ', should return ' + caze.output + ' when preresult is "' + caze.preresult + '" and timeout is "' + caze.timeout + '"', function () {
+        expect(service._timedOut(caze.preresult, caze.timeout)).to.equal(caze.output);
+      });
     });
-    it('should return "timedout" if result is "ok" and timeout is negative', function () {
-      expect(service._timedOut('ok', -5)).to.equal('timedout');
-    });
-    it('should return "partially" if result is partially correct and timeout is 0', function () {
-      expect(service._timedOut('partially', 0)).to.equal('partially');
-    });
-    it('should return "ok" if result is "ok" and timeout is 0', function () {
-      expect(service._timedOut('ok', 0)).to.equal('ok');
-    });
-    it('should return "partially" if result is partially correct and timeout is positive', function () {
-      expect(service._timedOut('partially', 11)).to.equal('partially');
-    });
-    it('should return "ok" if result is "ok" and timeout is 0', function () {
-      expect(service._timedOut('ok', 11)).to.equal('ok');
-    });
-    it('should return "aband" if result is "aband" and timeout is negative', function () {
-      expect(service._timedOut('aband', -5)).to.equal('aband');
-    });
-    it('should return "aband" if result is "aband" and timeout is 0', function () {
-      expect(service._timedOut('aband', 0)).to.equal('aband');
-    });
-    it('should return "aband" if result is "aband" and timeout is positive', function () {
-      expect(service._timedOut('aband', 11)).to.equal('aband');
-    });
+
   });
 
 });
