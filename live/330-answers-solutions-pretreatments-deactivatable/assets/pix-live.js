1e0577c80a9c4f135a1603f6e807e53dc690a3c8
Jeremy Buget <jbuget@gmail.com>
2017-03-06 09:25:58 +0000

    Release of 330-answers-solutions-pretreatments-deactivatable with env integration (via commit hash: 439783971f671e6799be8e852bddcc5dad8b8967)

--- live/330-answers-solutions-pretreatments-deactivatable/assets/pix-live.js
+++ live/330-answers-solutions-pretreatments-deactivatable/assets/pix-live.js
@@ -4428,7 +4428,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","name":"pix-live","version":"1.5.0+9d658e65"});
+  require("pix-live/app")["default"].create({"API_HOST":"","name":"pix-live","version":"1.5.0+43978397"});
 }
 
 /* jshint ignore:end */
