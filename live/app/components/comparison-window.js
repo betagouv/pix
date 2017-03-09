8bfdc0dd2ab420a86fc8cbb3cee331cc8731bfeb
Florian <fenoh@octo.com>
2017-03-09 12:04:56 +0100

    extract qcm-comparison in component

--- live/app/components/comparison-window.js
+++ live/app/components/comparison-window.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import labeledCheckboxes from 'pix-live/utils/labeled-checkboxes';
 const contentReference = {
   ok: {
     title: 'Vous avez la bonne réponse !',
@@ -58,16 +57,6 @@ const ComparisonWindow = Ember.Component.extend({
   isAssessmentChallengeTypeQrocm: Ember.computed.equal('challenge.type', 'QROCM'),
   isAssessmentChallengeTypeQrocmInd: Ember.computed.equal('challenge.type', 'QROCM-IND'),
   isAssessmentChallengeTypeQrocmDep: Ember.computed.equal('challenge.type', 'QROCM-DEP'),
-
-
-
-  solutionArray: Ember.computed('solution', function () {
-    return this.get('solution').get('_valueAsArrayOfBoolean');
-  }),
-
-  labeledCheckboxes: Ember.computed('answer', function () {
-    return labeledCheckboxes(this.get('challenge').get('_proposalsAsArray'), this.get('answer').get('_valueAsArrayOfBoolean'));
-  }),
 
   resultItemContent: Ember.computed('answer.result', function () {
     if (!this.get('answer.result')) return;
