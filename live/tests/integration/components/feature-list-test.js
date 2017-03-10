a83e08c022c1c885a5466e862b36ec09d78c7793
Jérémy Buget <jbuget@octo.com>
2017-03-10 19:16:17 +0100

    Add tests for feature-list

--- live/tests/integration/components/feature-list-test.js
+++ live/tests/integration/components/feature-list-test.js
@@ -3,15 +3,24 @@ import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describe('Integration | Component | feature list', function() {
+describe('Integration | Component | feature list', function () {
 
   setupComponentTest('feature-list', {
     integration: true
   });
 
-  it('renders', function() {
+  it('renders', function () {
     this.render(hbs`{{feature-list}}`);
     expect(this.$()).to.have.length(1);
+  });
+
+  it('should always render 5 feature-items', function () {
+    // when
+    this.render(hbs`{{feature-list}}`);
+
+    // then
+    expect(this.$('.feature-list__li')).to.have.lengthOf(5);
+    expect(this.$('.feature-item')).to.have.lengthOf(5);
   });
 
 });
