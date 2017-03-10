a45e0089bd7b0cdc70f3ea92008e4b1c6c1544b3
Brandone Martins <brandone.martins@gmail.com>
2017-03-10 15:06:31 +0100

    integration tests done

--- live/tests/integration/components/qrocm-ind-solution-panel-test.js
+++ live/tests/integration/components/qrocm-ind-solution-panel-test.js
@@ -1,33 +1,65 @@
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
+import { describe, it, beforeEach } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
-describe('Integration | Component | qrocm solution panel', function() {
-  setupComponentTest('qrocm-solution-panel', {
+const FIRST_CORRECTION_BLOCK = '.correction-qrocm:nth-child(1)';
+const SECOND_CORRECTION_BLOCK = '.correction-qrocm:nth-child(2)';
+const THIRD_CORRECTION_BLOCK = '.correction-qrocm:nth-child(3)';
+const SOLUTION_BLOCK = '.correction-qrocm__solution';
+const LABEL = '.correction-qrocm__label';
+const INPUT = '.correction-qrocm__answer-input';
+const SOLUTION_TEXT = '.correction-qrocm__solution-text';
+
+
+const RIGHT_ANSWER_GREEN = 'rgb(19, 201, 160)';
+const NO_ANSWER_GREY = 'rgb(51, 51, 51)';
+
+
+describe.only('Integration | Component | qrocm solution panel', function () {
+
+  setupComponentTest('qrocm-ind-solution-panel', {
     integration: true
   });
 
-  it('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#qrocm-solution-panel}}
-    //     template content
-    //   {{/qrocm-solution-panel}}
-    // `);
+  const assessment = Ember.Object.create({ id: 'assessment_id' });
+  const challenge = Ember.Object.create({ id: 'challenge_id', proposals: 'answer1 : ${key1}\nCarte mémoire (SD) : ${key2}\nblabla : ${key3}' });
+  const answer = Ember.Object.create({
+    id: 'answer_id',
+    value: 'key1: \'rightAnswer1\' key2: \'wrongAnswer2\' key3: \'\'',
+    assessment,
+    challenge
+  });
+  const solution = Ember.Object.create({ value: 'key1:\n- rightAnswer1\nkey2:\n- rightAnswer20\n- rightAnswer21\nkey3 :\n- rightAnswer3' });
 
-    this.render(hbs`{{qrocm-solution-panel}}`);
+  beforeEach(function () {
+    this.set('answer', answer);
+    this.set('solution', solution);
+    this.set('challenge', challenge);
+  });
+
+  //Est-ce-que l'on test son render sans aucun parametre => a toi de voir oui pour etre sur
+
+  it('renders', function () {
+    this.render(hbs`{{qrocm-ind-solution-panel answer=answer solution=solution challenge=challenge}}`);
     expect(this.$()).to.have.length(1);
   });
 
   it('should disabled all inputs', function () {
     // given
-    this.render(hbs`{{qroc-solution-panel}}`);
+    this.render(hbs`{{qrocm-ind-solution-panel answer solution challenge}}`);
     const input = this.$('input');
     // then
     expect(input).to.be.disabled;
+  });
+
+  it('should contains three labels', function () {
+    // given
+    this.render(hbs`{{qrocm-ind-solution-panel answer solution challenge}}`);
+    const labels = this.$(LABEL);
+    // then
+    expect(labels).to.have.length(3);
   });
 
   describe('comparison of a qrocm-ind with a right answer, a wrong answer and one empty answer', function () {
@@ -36,20 +68,30 @@ describe('Integration | Component | qrocm solution panel', function() {
 
       it('should display the right answer in green bold', function () {
         // given
-
-        // when
+        this.render(hbs`{{qrocm-ind-solution-panel challenge=challenge answer=answer solution=solution}}`);
+        const answerBlock = this.$(FIRST_CORRECTION_BLOCK);
+        const answerLabel = this.$(FIRST_CORRECTION_BLOCK + ' ' + LABEL);
+        const answerInput = this.$(FIRST_CORRECTION_BLOCK + ' ' + INPUT);
 
         // then
+        expect(answerBlock).to.have.length(1);
+        expect(answerLabel).to.have.length(1);
+        expect(answerInput).to.have.length(1);
 
+        expect(answerLabel.css('color')).to.be.equal(NO_ANSWER_GREY);
+
+        expect(answerInput.css('color')).to.be.equal(RIGHT_ANSWER_GREEN);
+        expect(answerInput.css('font-weight')).to.be.equal('bold');
+        expect(answerInput.css('text-decoration')).to.be.equal('none');
       });
 
       it('should not display the solution', function () {
         // given
-
-        // when
+        this.render(hbs`{{qrocm-ind-solution-panel challenge=challenge answer=answer solution=solution}}`);
+        const solutionBlock = this.$(FIRST_CORRECTION_BLOCK + ' ' + SOLUTION_BLOCK);
 
         // then
-
+        expect(solutionBlock).to.have.length(0);
       });
     });
 
@@ -57,20 +99,34 @@ describe('Integration | Component | qrocm solution panel', function() {
 
       it('should display the wrong answer in the second div line-throughed bold', function () {
         // given
-
-        // when
+        this.render(hbs`{{qrocm-ind-solution-panel challenge=challenge answer=answer solution=solution}}`);
+        const answerBlock = this.$(SECOND_CORRECTION_BLOCK);
+        const answerLabel = this.$(SECOND_CORRECTION_BLOCK + ' ' + LABEL);
+        const answerInput = this.$(SECOND_CORRECTION_BLOCK + ' ' + INPUT);
 
         // then
+        expect(answerBlock).to.have.length(1);
+        expect(answerLabel).to.have.length(1);
+        expect(answerInput).to.have.length(1);
 
+        expect(answerInput.css('color')).to.be.equal(NO_ANSWER_GREY);
+        expect(answerInput.css('font-weight')).to.be.equal('400');
+        expect(answerInput.css('text-decoration')).to.be.equal('line-through');
       });
 
       it('should display one solution in bold green below the input', function () {
         // given
-
-        // when
+        this.render(hbs`{{qrocm-ind-solution-panel challenge=challenge answer=answer solution=solution}}`);
+        const solutionBlock = this.$(SECOND_CORRECTION_BLOCK + ' ' + SOLUTION_BLOCK);
+        const solutionText = this.$(SECOND_CORRECTION_BLOCK + ' ' + SOLUTION_BLOCK + ' ' + SOLUTION_TEXT);
 
         // then
+        expect(solutionBlock).to.have.length(1);
+        expect(solutionText).to.have.length(1);
 
+        expect(solutionText.css('color')).to.be.equal(RIGHT_ANSWER_GREEN);
+        expect(solutionText.css('font-weight')).to.be.equal('bold');
+        expect(solutionText.css('text-decoration')).to.be.equal('none');
       });
     });
 
@@ -78,22 +134,35 @@ describe('Integration | Component | qrocm solution panel', function() {
 
       it('should display the empty answer in the third div with "pas de réponse" in italic', function () {
         // given
-
-        // when
+        this.render(hbs`{{qrocm-ind-solution-panel challenge=challenge answer=answer solution=solution}}`);
+        const answerBlock = this.$(THIRD_CORRECTION_BLOCK);
+        const answerLabel = this.$(THIRD_CORRECTION_BLOCK + ' ' + LABEL);
+        const answerInput = this.$(THIRD_CORRECTION_BLOCK + ' ' + INPUT);
 
         // then
+        expect(answerBlock).to.have.length(1);
+        expect(answerLabel).to.have.length(1);
+        expect(answerInput).to.have.length(1);
 
+        expect(answerInput.css('color')).to.be.equal(NO_ANSWER_GREY);
+        expect(answerInput.css('font-weight')).to.be.equal('400');
+        expect(answerInput.css('text-decoration')).to.be.equal('none');
       });
 
       it('should display one solution in bold green below the input', function () {
         // given
-
-        // when
+        this.render(hbs`{{qrocm-ind-solution-panel challenge=challenge answer=answer solution=solution}}`);
+        const solutionBlock = this.$(THIRD_CORRECTION_BLOCK + ' ' + SOLUTION_BLOCK);
+        const solutionText  = this.$(THIRD_CORRECTION_BLOCK + ' ' + SOLUTION_BLOCK + ' ' + SOLUTION_TEXT);
 
         // then
+        expect(solutionBlock).to.have.length(1);
+        expect(solutionText).to.have.length(1);
 
+        expect(solutionText.css('color')).to.be.equal(RIGHT_ANSWER_GREEN);
+        expect(solutionText.css('font-weight')).to.be.equal('bold');
+        expect(solutionText.css('text-decoration')).to.be.equal('none');
       });
     });
-
   });
 });
