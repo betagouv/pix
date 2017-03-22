39c90ad5c11b4747e9bce6e188b17b25ecf36d17
Florian <fenoh@octo.com>
2017-03-22 14:43:37 +0100

    Refacto eq helper and add tests

--- live/app/helpers/eq.js
+++ live/app/helpers/eq.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
-// borrowed from https://emberigniter.com/how-to-equals-conditional-comparison-handlebars/
-const eq = (params) => params[0] === params[1];
+export function eq(params/*, hash*/) {
+  let isEqual = false;
+  if (params && params[1]) {
+    isEqual = (params[0] === params[1]) ? true : false;
+  }
+  return isEqual;
+}
+
 export default Ember.Helper.helper(eq);
