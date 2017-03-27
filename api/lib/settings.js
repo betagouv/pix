18f283655f6e9cf01a98ffe7c2fa42a4b04ac532
Florian <fenoh@octo.com>
2017-03-27 12:12:27 +0200

    Add mailjet keys

--- api/lib/settings.js
+++ api/lib/settings.js
@@ -2,32 +2,43 @@ const path = require('path');
 
 module.exports = (function () {
 
-  const config = {
+    const config = {
 
-    rootPath: path.normalize(__dirname + '/..'),
+        rootPath: path.normalize(__dirname + '/..'),
 
-    port: parseInt(process.env.PORT, 10) || 3000,
+        port: parseInt(process.env.PORT, 10) || 3000,
 
-    environment: process.env.NODE_ENV || 'development',
+        environment: process.env.NODE_ENV || 'development',
 
-    hapi: {
-      options: {}
-    },
+        hapi: {
+            options: {}
+        },
 
-    airtable: {
-      apiKey: 'keyEgu8JYhXaOhjbd',
-      base: 'appHAIFk9u1qqglhX'
-    }
-  };
+        airtable: {
+            apiKey: 'keyEgu8JYhXaOhjbd',
+            base: 'appHAIFk9u1qqglhX'
+        },
 
-  if (process.env.NODE_ENV === 'test') {
-    config.port = null;
-    config.airtable = {
-      apiKey: 'test-api-key',
-      base: 'test-base'
+        mailjet: {
+            apiKey: '7efe76a3fd5a487b7bf20a3688750666',
+            apiSecret: 'c932bbba9429c842c318f790cc82b0e0'
+        }
     };
-  }
 
-  return config;
+    if (process.env.NODE_ENV === 'test') {
+        config.port = null;
+
+        config.airtable = {
+            apiKey: 'test-api-key',
+            base: 'test-base'
+        };
+
+        mailjet: {
+            apiKey: 'test-api-ket',
+            apiSecret: 'test-api-secret'
+        };
+    }
+
+    return config;
 
 })();
