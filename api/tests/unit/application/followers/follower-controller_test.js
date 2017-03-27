26630f97b2983e8cbea56fd7ed146cb0f692f3dd
Florian <fenoh@octo.com>
2017-03-27 19:01:04 +0200

    Add mailjet test to unit test to follower

--- api/tests/unit/application/followers/follower-controller_test.js
+++ api/tests/unit/application/followers/follower-controller_test.js
@@ -4,10 +4,12 @@ const Hapi = require('hapi');
 const EmailValidator = require('../../../../lib/domain/services/email-validator');
 const Follower = require('../../../../lib/domain/models/data/follower');
 const followerSerializer = require('../../../../lib/infrastructure/serializers/jsonapi/follower-serializer');
+const Mailjet = require('../../../../lib/infrastructure/mailjet')
 
 describe('Unit | Controller | FollowerController', function () {
 
   let server;
+
 
   before(function () {
     server = this.server = new Hapi.Server();
@@ -21,13 +23,17 @@ describe('Unit | Controller | FollowerController', function () {
       const follower = {"email": "testeur@follower.pix"};
       const emailValidatorStub = sinon.stub(EmailValidator,'emailIsValid').returns(false);
       sinon.stub(followerSerializer, 'deserialize', _ => new Follower(follower));
+
+      const spyMailjet = sinon.spy(Mailjet, 'sendWelcomeEmail');
       // when
       server.inject({method: 'POST', url: '/api/followers', payload: {"email": 'INVALID_EMAIL'}},
         (res) => {
           // then
           expect(res.statusCode).to.equal(400);
+          expect(spyMailjet.calledOnce).to.be.false;
           emailValidatorStub.restore();
           followerSerializer.deserialize.restore();
+          spyMailjet.restore();
           done();
         });
     });
