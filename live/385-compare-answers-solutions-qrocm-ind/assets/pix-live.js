c061ec749dbfbdea7d3daad0852851d8aae74d99
Jeremy Buget <jbuget@gmail.com>
2017-03-30 10:45:49 +0000

    Release of 385-compare-answers-solutions-qrocm-ind with env integration (via commit hash: 5c5919ba613b4ccbe97811f4691da8b7b25cb594)

--- live/385-compare-answers-solutions-qrocm-ind/assets/pix-live.js
+++ live/385-compare-answers-solutions-qrocm-ind/assets/pix-live.js
@@ -4894,6 +4894,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","name":"pix-live","version":"1.5.2+14fc4ddb"});
+  require("pix-live/app")["default"].create({"API_HOST":"","name":"pix-live","version":"1.5.2+5c5919ba"});
 }
 //# sourceMappingURL=pix-live.map
