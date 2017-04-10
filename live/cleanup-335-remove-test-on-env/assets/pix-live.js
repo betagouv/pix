ab52195046ed0802cdee2b0277c12973ea78c340
Jeremy Buget <jbuget@gmail.com>
2017-04-10 16:37:19 +0000

    Release of cleanup-335-remove-test-on-env with env integration (via commit hash: 30252d367288d0c77b194e83b35732025886ffa9)

--- live/cleanup-335-remove-test-on-env/assets/pix-live.js
+++ live/cleanup-335-remove-test-on-env/assets/pix-live.js
@@ -4833,6 +4833,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.6.0+a65d6ebf"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.6.0+30252d36"});
 }
 //# sourceMappingURL=pix-live.map
