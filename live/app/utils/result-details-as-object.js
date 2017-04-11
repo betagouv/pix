30c3f28da1b4e8fc96c4dc9180968efd4e508d34
Brandone Martins <brandone.martins@gmail.com>
2017-04-11 15:50:24 +0200

    fix when resultdetailsYaml is null

--- live/app/utils/result-details-as-object.js
+++ live/app/utils/result-details-as-object.js
@@ -1,6 +1,9 @@
 /* global jsyaml */
 
 export default function resultDetailsAsObject(yamlResultDetails) {
-  const resultDetailsAsObject = jsyaml.safeLoad(yamlResultDetails);
+  let resultDetailsAsObject = {};
+  if (yamlResultDetails != 'null\n'){
+    resultDetailsAsObject = jsyaml.safeLoad(yamlResultDetails);
+  }
   return resultDetailsAsObject;
 }
