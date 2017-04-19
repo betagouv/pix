046531a88784be3282476e0862f221d1bc244cdb
Jeremy Buget <jbuget@gmail.com>
2017-04-19 13:57:05 +0000

    Release of master with env integration (via commit hash: c0c7c5467886b9dce1f977d636a686d4a41a7f06)

--- live/master/assets/pix-live.js
+++ live/master/assets/pix-live.js
@@ -4953,6 +4953,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.6.1+47739b55"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.6.2+c0c7c546"});
 }
 //# sourceMappingURL=pix-live.map
