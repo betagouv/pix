19155403e87232ccaaebe53995dab2be96c669a9
Jeremy Buget <jbuget@gmail.com>
2017-04-21 14:54:42 +0000

    Release of bug-assessment-controller-with-scoring-preview with env integration (via commit hash: 9d6d576759b2ee9ce0ff6d6194ade9aaa51afe7f)

--- live/bug-assessment-controller-with-scoring-preview/assets/pix-live.js
+++ live/bug-assessment-controller-with-scoring-preview/assets/pix-live.js
@@ -5096,6 +5096,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.7.0+b4b02787"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.7.0+9d6d5767"});
 }
 //# sourceMappingURL=pix-live.map
