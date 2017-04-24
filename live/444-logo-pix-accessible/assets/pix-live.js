b755da70985eab83e59372fedf200d7a3d37d3f0
Jeremy Buget <jbuget@gmail.com>
2017-04-24 09:17:43 +0000

    Release of 444-logo-pix-accessible with env integration (via commit hash: 0e243dc5acfd2584ab3a14ed0e4b6895920ac20b)

--- live/444-logo-pix-accessible/assets/pix-live.js
+++ live/444-logo-pix-accessible/assets/pix-live.js
@@ -4953,6 +4953,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.6.1+c951af5a"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.6.1+0e243dc5"});
 }
 //# sourceMappingURL=pix-live.map
