fd1ad8057cd4323b4c669be1e4f63f366a123c00
Jeremy Buget <jbuget@gmail.com>
2017-04-24 14:00:51 +0000

    Release of staging with env staging (via commit hash: a9c07184f9ed8dfa88bf776a624d97b2301e1ac0)

--- live/staging/assets/pix-live.js
+++ live/staging/assets/pix-live.js
@@ -5120,6 +5120,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.7.0+62bbd1b3"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.7.0+a9c07184"});
 }
 //# sourceMappingURL=pix-live.map
