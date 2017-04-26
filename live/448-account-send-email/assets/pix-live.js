68cb10cd61d4933088e805dedeffdb1370c13f9f
Jeremy Buget <jbuget@gmail.com>
2017-04-26 16:23:36 +0000

    Release of 448-account-send-email with env integration (via commit hash: def74e025c0fc2173abc5f1fe948d675401f7dae)

--- live/448-account-send-email/assets/pix-live.js
+++ live/448-account-send-email/assets/pix-live.js
@@ -5120,6 +5120,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.8.0+ca993d7d"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.8.0+def74e02"});
 }
 //# sourceMappingURL=pix-live.map
