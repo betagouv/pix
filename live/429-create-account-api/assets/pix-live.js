407bc82bb653f01c9ba6fa8caee51785813a6296
Jeremy Buget <jbuget@gmail.com>
2017-04-26 15:57:16 +0000

    Release of 429-create-account-api with env integration (via commit hash: 2422a800bf6bff8b88edeaa0be4a16f7ce422372)

--- live/429-create-account-api/assets/pix-live.js
+++ live/429-create-account-api/assets/pix-live.js
@@ -5120,6 +5120,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.8.0+84dd440b"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.8.0+2422a800"});
 }
 //# sourceMappingURL=pix-live.map
