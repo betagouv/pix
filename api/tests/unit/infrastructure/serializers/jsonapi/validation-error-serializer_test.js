4dd7822d14afcbd948508f78ad4a0e3e2bc5a312
hypernikao <fenoh@octo.com>
2017-05-04 15:40:26 +0200

    convert to kebab-case fieldName in validation-error-serializer

--- api/tests/unit/infrastructure/serializers/jsonapi/validation-error-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/validation-error-serializer_test.js
@@ -54,6 +54,79 @@ describe('Unit | Serializer | JSONAPI | validation-error-serializer', () => {
     });
 
 
+    it('should format a validation error into a JSON:API spec with kebab-case on source pointer', () => {
+      // Given
+      const validationErrors = {
+        data: {
+          firstName: ['Error message']
+        }
+      };
+      const expectedFormattedJSON = {
+        "errors": [
+          {
+            "status": "400",
+            "title": "Invalid Attribute",
+            "detail": "Error message",
+            "source": {"pointer": "/data/attributes/first-name"},
+            "meta": {
+              "field": "firstName"
+            }
+          }
+        ]
+      };
+
+      // When
+      const formattedJSON = serializer.serialize(validationErrors);
+
+      // Then
+      expect(formattedJSON).to.deep.equal(expectedFormattedJSON);
+    });
+
+    it('should return several messages for one field if they exist', () => {
+      // Given
+      const validationErrors = {
+        data: {
+          field: ['Error message #1', 'Error message #2', 'Error message #3']
+        }
+      };
+      const expectedFormattedJSON = {
+        "errors": [
+          {
+            "status": "400",
+            "title": "Invalid Attribute",
+            "detail": "Error message #1",
+            "source": {"pointer": "/data/attributes/field"},
+            "meta": {
+              "field": "field"
+            }
+          }, {
+            "status": "400",
+            "title": "Invalid Attribute",
+            "detail": "Error message #2",
+            "source": {"pointer": "/data/attributes/field"},
+            "meta": {
+              "field": "field"
+            }
+          }, {
+            "status": "400",
+            "title": "Invalid Attribute",
+            "detail": "Error message #3",
+            "source": {"pointer": "/data/attributes/field"},
+            "meta": {
+              "field": "field"
+            }
+          }
+        ]
+      };
+
+      // When
+      const formattedJSON = serializer.serialize(validationErrors);
+
+      // Then
+      expect(formattedJSON).to.deep.equal(expectedFormattedJSON);
+    });
+
+
     it('should format a validation error into a JSON spec when multiple errors', () => {
       // Given
       const invalidObject = new DummyObject({
