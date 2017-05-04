7fe637ddae0c137c4805bcfa45c7ed31096dfcbe
Jeremy Buget <jbuget@gmail.com>
2017-05-04 13:52:44 +0000

    Release of 472-scoring-panel-won-trophy-parralel with env integration (via commit hash: 8a8ebb58e7683bd0285bb3de444bf9464f728252)

--- live/472-scoring-panel-won-trophy-parralel/assets/pix-live.js
+++ live/472-scoring-panel-won-trophy-parralel/assets/pix-live.js
@@ -5159,6 +5159,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.8.1+df9ebcd4"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.8.1+8a8ebb58"});
 }
 //# sourceMappingURL=pix-live.map
