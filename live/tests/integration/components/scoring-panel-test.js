5ecbe70f3a8cd2fb60360cf581aa7fdb7dedac3a
Brandone Martins <brandone.martins@gmail.com>
2017-05-10 16:03:22 +0200

    create computed property + add unit test and integration test + add medaille to image

--- live/tests/integration/components/scoring-panel-test.js
+++ live/tests/integration/components/scoring-panel-test.js
@@ -11,7 +11,8 @@ describe('Integration | Component | scoring panel', function() {
   });
 
   const assessmentWithTrophy = Ember.Object.create({estimatedLevel : 1, course : {isAdaptive : true}});
-  const assessmentWithNoTrophy = Ember.Object.create({estimatedLevel : 0, course : {isAdaptive : true}});
+  const assessmentWithNoTrophyAndSomePix = Ember.Object.create({estimatedLevel : 0, pixScore : 20, course : {isAdaptive : true}});
+  const assessmentWithNoTrophyAndNoPix = Ember.Object.create({estimatedLevel : 0, pixScore : 0, course : {isAdaptive : true}});
 
   it('renders', function() {
     this.render(hbs`{{scoring-panel}}`);
@@ -21,7 +22,7 @@ describe('Integration | Component | scoring panel', function() {
   describe('view without trophy', function () {
 
     beforeEach(function () {
-      this.set('assessment', assessmentWithNoTrophy);
+      this.set('assessment', assessmentWithNoTrophyAndNoPix);
       this.render(hbs`{{scoring-panel assessment=assessment}}`);
     });
 
@@ -59,6 +60,30 @@ describe('Integration | Component | scoring panel', function() {
       // then
       expect(this.$('.scoring-panel__index-link')).to.have.lengthOf(1);
       expect(this.$('.scoring-panel__index-link-back').text()).to.be.equal('REVENIR À L\'ACCUEIL');
+    });
+  });
+
+  describe('view with no trophy but some pix', function () {
+
+    beforeEach(function () {
+      this.set('assessment', assessmentWithNoTrophyAndSomePix);
+      this.render(hbs`{{scoring-panel assessment=assessment}}`);
+    });
+
+    it('should display the won medal', function () {
+      // then
+      expect(this.$('.scoring-panel__medal-div')).to.have.lengthOf(1);
+      expect(this.$('.scoring-panel__medal-pix-score')).to.have.lengthOf(1);
+      expect(this.$('.scoring-panel__medal-pix-text')).to.have.lengthOf(1);
+      expect(this.$('.scoring-panel__medal-bêta')).to.have.lengthOf(1);
+    });
+
+    it('should display the congratulations', function () {
+      // then
+      expect(this.$('.scoring-panel__congrats-course-name')).to.have.lengthOf(1);
+      expect(this.$('.scoring-panel__congrats-felicitations')).to.have.lengthOf(1);
+      expect(this.$('.scoring-panel__congrats-scoring')).to.have.lengthOf(1);
+      expect(this.$('.scoring-panel__congrats-beta')).to.have.lengthOf(1);
     });
   });
 });
