3eca0c9a1d2afd79151649a9fa3f99263722a03e
Jeremy Buget <jbuget@gmail.com>
2017-05-10 14:08:19 +0000

    Release of 194-creation-compte-front with env integration (via commit hash: 252d863bf7a3895e0279e4de6c94aa733b109622)

--- live/194-creation-compte-front/assets/pix-live.js
+++ live/194-creation-compte-front/assets/pix-live.js
@@ -5437,6 +5437,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"http://localhost:3000","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.9.0+f4a98031"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.9.0+252d863b"});
 }
 //# sourceMappingURL=pix-live.map
