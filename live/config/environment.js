6643360f942c4597e8336ba371a7bc66138423c3
hypernikao <fenoh@octo.com>
2017-05-10 17:27:45 +0200

    trying set environment

--- live/config/environment.js
+++ live/config/environment.js
@@ -48,7 +48,9 @@ module.exports = function (environment) {
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_TRANSITIONS = false;
     ENV.APP.LOG_TRANSITIONS_INTERNAL = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    // Redefined in custom initializer 'initializers/configure-pix-api-host.js'
     ENV.APP.API_HOST = 'http://localhost:3000';
 
 
