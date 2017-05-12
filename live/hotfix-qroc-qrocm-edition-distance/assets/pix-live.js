1e361ec3fadf427fc00cdc05267f181cb260c3a1
Jeremy Buget <jbuget@gmail.com>
2017-05-12 08:28:57 +0000

    Release of hotfix-qroc-qrocm-edition-distance with env integration (via commit hash: 70d4184cc6c8ad72225d9a032c5079e575374d5d)

--- live/hotfix-qroc-qrocm-edition-distance/assets/pix-live.js
+++ live/hotfix-qroc-qrocm-edition-distance/assets/pix-live.js
@@ -5153,6 +5153,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.9.0+f6036b16"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.9.0+70d4184c"});
 }
 //# sourceMappingURL=pix-live.map
