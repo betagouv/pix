b839edb3a0e66db2dd4950e3cf06ffe3b4a8ec7e
Brandone Martins <brandone.martins@gmail.com>
2017-05-12 15:58:49 +0200

    put percentage on pixScore and beta positionment to fit on any size in medal-item

--- live/app/components/scoring-panel.js
+++ live/app/components/scoring-panel.js
@@ -6,9 +6,9 @@ export default Ember.Component.extend({
 
   assessment: null,
 
-  //hasATrophy: Ember.computed.gt('assessment.estimatedLevel', 0),
-  hasATrophy: true,
-  //hasSomePix: Ember.computed.gt('assessment.pixScore', 0)
-  hasSomePix: true
+  hasATrophy: Ember.computed.gt('assessment.estimatedLevel', 0),
+  //hasATrophy: false,
+  hasSomePix: Ember.computed.gt('assessment.pixScore', 0)
+  //hasSomePix: true
 
 });
