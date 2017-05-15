ec3fe47fd83184843e682a7a522e0cb7918a7c43
Jeremy Buget <jbuget@gmail.com>
2017-05-15 16:33:14 +0000

    Release of exit-on-script-error with env integration (via commit hash: d3121c8ba31a9cb3648c1a52f0d5eaf9c7f42acd)

--- live/exit-on-script-error/assets/pix-live.js
+++ live/exit-on-script-error/assets/pix-live.js
@@ -5153,6 +5153,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.11.0+ac581820"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"name":"pix-live","version":"1.11.0+d3121c8b"});
 }
 //# sourceMappingURL=pix-live.map
