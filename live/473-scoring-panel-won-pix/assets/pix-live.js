36b530c9bd26273838f364d6b3d863c8dcff14ba
Jeremy Buget <jbuget@gmail.com>
2017-05-16 16:09:22 +0000

    Release of 473-scoring-panel-won-pix with env integration (via commit hash: 3f45942fcb81d0cbf7709f86fd5829f4ddc70740)

--- live/473-scoring-panel-won-pix/assets/pix-live.js
+++ live/473-scoring-panel-won-pix/assets/pix-live.js
@@ -5469,6 +5469,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.0+2e63a1a8"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.0+3f45942f"});
 }
 //# sourceMappingURL=pix-live.map
