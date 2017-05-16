9c357da5f5fdb69741f912e5e952ca40083b9a71
Jeremy Buget <jbuget@gmail.com>
2017-05-16 22:56:17 +0000

    Release of infra-upgrade-dependencies with env integration (via commit hash: 44634e995f6219b385ef669277b785871451974b)

--- live/infra-upgrade-dependencies/assets/pix-live.js
+++ live/infra-upgrade-dependencies/assets/pix-live.js
@@ -5329,6 +5329,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.0+0c7fedf0"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.0+44634e99"});
 }
 //# sourceMappingURL=pix-live.map
