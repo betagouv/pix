283040c5b7e09d35b679ba1c52254b791a445f53
Jeremy Buget <jbuget@gmail.com>
2017-05-18 22:35:48 +0000

    Release of cleanup-upgrade-dependencies with env integration (via commit hash: 05be9c4033daef34437a65916a56b98ebd59058a)

--- live/cleanup-upgrade-dependencies/assets/pix-live.js
+++ live/cleanup-upgrade-dependencies/assets/pix-live.js
@@ -6866,6 +6866,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.0+af504477"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.0+05be9c40"});
 }
 //# sourceMappingURL=pix-live.map
