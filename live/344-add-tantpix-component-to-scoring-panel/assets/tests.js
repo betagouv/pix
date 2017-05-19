879bff2f8a30be3ddb3c5b5c848d087ba621895a
Jeremy Buget <jbuget@gmail.com>
2017-05-19 16:05:51 +0000

    Release of 344-add-tantpix-component-to-scoring-panel with env integration (via commit hash: 6782fb1fd06175890bb8b774dfe73c89717417eb)

--- live/344-add-tantpix-component-to-scoring-panel/assets/tests.js
+++ live/344-add-tantpix-component-to-scoring-panel/assets/tests.js
@@ -4716,10 +4716,10 @@ define('pix-live/tests/integration/components/scoring-panel-tantpix-test', ['exp
 
         (0, _mocha.it)('should return a smiley illustration which satisfy minimals accessibilities conditions', function () {
           var smiley = this.$(HEADING_ILLUSTRATION_CLASS);
-          (0, _chai.expect)(smiley.attr('src')).to.includes('images/smiley.png');
-          (0, _chai.expect)(smiley.attr('srcset')).to.includes('images/smiley@2x.png');
-          (0, _chai.expect)(smiley.attr('srcset')).to.includes('images/smiley@3x.png');
-          (0, _chai.expect)(smiley.attr('alt')).to.includes('smiley tant pix');
+          (0, _chai.expect)(smiley.attr('src')).to.includes('/images/smiley.png');
+          (0, _chai.expect)(smiley.attr('srcset')).to.includes('/images/smiley@2x.png');
+          (0, _chai.expect)(smiley.attr('srcset')).to.includes('/images/smiley@3x.png');
+          (0, _chai.expect)(smiley.attr('alt')).to.includes('smiley');
         });
       });
     });
