455c3940b1ebd69052ddeab992b7e88cd87a7916
Jeremy Buget <jbuget@gmail.com>
2017-05-22 16:54:16 +0000

    Release of 412-recaptcha-when-user-subscription with env integration (via commit hash: 57b3cf6cd5ba5e1e30311ffbd7b8ffb9616d050b)

--- live/412-recaptcha-when-user-subscription/assets/pix-live.js
+++ live/412-recaptcha-when-user-subscription/assets/pix-live.js
@@ -6875,6 +6875,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.0+326faaec"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.0+57b3cf6c"});
 }
 //# sourceMappingURL=pix-live.map
