55aa2fe4c77798bade584db79877a4bd2a361058
hypernikao <fenoh@octo.com>
2017-05-23 18:43:59 +0200

    Add captcha-validator-service with unit test

--- api/lib/domain/services/recaptcha-validator.js
+++ api/lib/domain/services/recaptcha-validator.js
@@ -1,16 +1,32 @@
 const request = require('request');
 const googleReCaptcha = require('../../settings').googleReCaptcha;
+
 module.exports = {
 
-  verify(response) {
+  isNullOrEmptyResponse: function(response) {
+    return !response || response.trim().length < 1;
+  },
 
-    if(!response || response.trim().length < 1) {
+  getGoogleVerification: function(response) {
+    return request.post({
+      url: 'https://www.google.com/recaptcha/api/siteverify',
+      form: {
+        secret: googleReCaptcha.secret,
+        response: response
+      }
+    }, function(err, httpResponse) {
+      if(err) {
+        return err;
+      }
+      return httpResponse;
+    });
+
+  },
+
+  verify(response) {
+    if(this.isNullOrEmptyResponse(response)) {
       return false;
     }
-
-    return request.post('https://www.google.com/recaptcha/api/siteverify', {
-      secret: googleReCaptcha.secret,
-      response: response
-    });
+    return this.getGoogleVerification(response);
   }
 };
