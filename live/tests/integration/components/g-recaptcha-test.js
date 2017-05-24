ca8ac879193e79b1e74edc84c2e7f556b3f9d68d
Brandone Martins <brandone.martins@gmail.com>
2017-05-24 18:26:06 +0200

    with log

--- live/tests/integration/components/g-recaptcha-test.js
+++ live/tests/integration/components/g-recaptcha-test.js
@@ -3,31 +3,22 @@ import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describe('Integration | Component | g recaptcha', function() {
+describe.only('Integration | Component | g recaptcha', function() {
   setupComponentTest('g-recaptcha', {
     integration: true
   });
 
   it('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#g-recaptcha}}
-    //     template content
-    //   {{/g-recaptcha}}
-    // `);
-
     this.render(hbs`{{g-recaptcha}}`);
     expect(this.$()).to.have.length(1);
   });
 
   it('should render the google recaptcha widget', function() {
     // given
-
-    // when
-
-    // then import script et div avec id:g-recaptcha
+    this.render(hbs`{{g-recaptcha}}`);
+    // then
+    expect(this.$('#g-recaptcha')).to.have.lengthOf(1);
+    expect(this.$('script').attr('src')).to.equal('https://www.google.com/recaptcha/api.js?render=explicit');
 
   });
 
