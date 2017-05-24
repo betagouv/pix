02bd64dd4e73ef224f4ab4ad84ea49cf954f75ec
hypernikao <fenoh@octo.com>
2017-05-24 11:08:13 +0200

    Add test to serialize captachaResponse Error

--- api/tests/unit/infrastructure/serializers/jsonapi/validation-error-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/validation-error-serializer_test.js
@@ -3,7 +3,7 @@ const serializer = require('../../../../../lib/infrastructure/serializers/jsonap
 
 const Bookshelf = require('../../../../../lib/infrastructure/bookshelf');
 
-describe('Unit | Serializer | JSONAPI | validation-error-serializer', () => {
+describe.only('Unit | Serializer | JSONAPI | validation-error-serializer', () => {
 
   describe('#serialize', () => {
 
@@ -166,5 +166,34 @@ describe('Unit | Serializer | JSONAPI | validation-error-serializer', () => {
       });
     });
 
+    it('should format a validation error into Json Spec when invalid captcha response', function() {
+      // given
+      const invalidCaptchaResponse = {
+        data: {
+          captchaResponse: ['Le captcha est invalide.']
+        }
+      };
+
+      const expectJsonFormat = {
+        'errors': [
+          {
+            'detail': 'Le captcha est invalide.',
+            'meta': {
+              'field': 'captchaResponse'
+            },
+            'source': {
+              'pointer': '/data/attributes/captcha-response'
+            },
+            'status': '400',
+            'title': 'Invalid Attribute'
+          }
+        ]
+      }
+      // when
+      const formattedJSON = serializer.serialize(invalidCaptchaResponse);
+
+      // then
+      expect(formattedJSON).to.deep.equal(expectJsonFormat);
+    });
   });
 });
