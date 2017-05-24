78708f6752b8613ffddc191ab152eedf80d61442
Adrien SAUNIER <asaunier@octo.com>
2017-05-24 10:29:16 +0200

    Replacing un-necessary mixin by an util on QCM

--- live/tests/acceptance/b2-epreuve-qcm-test.js
+++ live/tests/acceptance/b2-epreuve-qcm-test.js
@@ -1,4 +1,4 @@
-import { describe, it, before, after } from 'mocha';
+import { describe, it, beforeEach, afterEach } from 'mocha';
 import { expect } from 'chai';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
@@ -14,23 +14,27 @@ describe('Acceptance | b2 - Afficher un QCM | ', function() {
 
   let application;
 
-  before(function() {
+  beforeEach(function() {
     application = startApp();
     visitTimedChallenge();
   });
 
-  after(function() {
+  afterEach(function() {
     destroyApp(application);
   });
 
   it('b2.1 It should render challenge instruction', function() {
     const $challengeInstruction = $('.challenge-statement__instruction');
     const instructionText = 'Un QCM propose plusieurs choix, l\'utilisateur peut en choisir plusieurs';
+
+    // Then
     expect($challengeInstruction.text().trim()).to.equal(instructionText);
   });
 
   it('b2.2 Le contenu de type [foo](bar) doit être converti sous forme de lien', function() {
     const $links = findWithAssert('.challenge-statement__instruction a');
+
+    // Then
     expect($links.length).to.equal(1);
     expect($links.text()).to.equal('plusieurs');
     expect($links.attr('href')).to.equal('http://link.plusieurs.url');
@@ -46,7 +50,7 @@ describe('Acceptance | b2 - Afficher un QCM | ', function() {
     expect($proposals).to.have.lengthOf(4);
   });
 
-  it('b2.5 By default, already checked checkboxes are checked', function() {
+  it('b2.5 It should mark checkboxes that have been checked', function() {
     expect($('input:checkbox:checked')).to.have.lengthOf(2);
   });
 
@@ -62,37 +66,33 @@ describe('Acceptance | b2 - Afficher un QCM | ', function() {
   });
 
   it('b2.8 Error alert box should be displayed if user validate without checking a checkbox', function() {
+    // Given
     const $validateLink = $('.challenge-actions__action-validate');
     expect($('input:checkbox:checked')).to.have.lengthOf(2);
+
+    //
     $('input:checkbox').prop('checked', false);
     expect($('input:checkbox:checked')).to.have.lengthOf(0);
+
+    // When
     click($validateLink);
+
+    // Then
     andThen(() => {
       expect($('.alert')).to.have.lengthOf(1);
       expect($('.alert').text().trim()).to.equal('Pour valider, sélectionner au moins une réponse. Sinon, passer.');
     });
   });
 
-  it('b2.9 If an user check a checkbox, it is checked', function() {
-    expect($('input:checkbox:checked')).to.have.lengthOf(0);
-    $('.input-checkbox-proposal:eq(1)').click();
-    andThen(() => {
-      expect($('input:checkbox:checked')).to.have.lengthOf(1);
-    });
-  });
+  it('b2.9 If an user validate the challenge, the api is request to save the answer of the user', async function() {
 
-  it('b2.10 If an user check another checkbox, it is checked, the previous checked checkboxes remains checked', function() {
-    expect($('input:checkbox:checked')).to.have.lengthOf(1);
-    $('.input-checkbox-proposal:eq(2)').click();
-    andThen(() => {
-      expect($('input:checkbox:checked')).to.have.lengthOf(2);
-    });
-  });
-
-  it('b2.11 If an user validate the challenge, the api is request to save the answer of the user', async function() {
-    resetTestingState();
+    // When
     await click('.challenge-actions__action-validate');
+
+    $('input:checkbox:checked');
+
+    // Then
     expect(urlOfLastPostRequest()).to.equal('/api/answers');
-    expect(_.get(bodyOfLastPostRequest(), 'data.attributes.value')).to.equal('2,3');
+    expect(_.get(bodyOfLastPostRequest(), 'data.attributes.value')).to.equal('2,4');
   });
 });
