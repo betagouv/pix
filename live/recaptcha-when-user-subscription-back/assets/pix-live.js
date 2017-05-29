d765875c58f0cfbf727b6a61da2296c2d879fd87
Jeremy Buget <jbuget@gmail.com>
2017-05-29 19:44:50 +0000

    Release of recaptcha-when-user-subscription-back with env integration (via commit hash: f5635f151eac5e469b7a0eb6ce091f4c572c107f)

--- live/recaptcha-when-user-subscription-back/assets/pix-live.js
+++ live/recaptcha-when-user-subscription-back/assets/pix-live.js
@@ -6969,6 +6969,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.1+76c2fb85"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.1+f5635f15"});
 }
 //# sourceMappingURL=pix-live.map
