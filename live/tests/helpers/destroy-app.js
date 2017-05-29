41ae2be1d11595fb9802f8a6e59be5fa709d39a8
Jérémy Buget <jbuget@gmail.com>
2017-05-29 19:07:50 +0200

    Replace one of the removed tests with a test using Mirage / Pretender features, as an example

--- live/tests/helpers/destroy-app.js
+++ live/tests/helpers/destroy-app.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default function destroyApp(application) {
   Ember.run(application, 'destroy');
-  server.shutdown();
+  if (window.server) {
+    window.server.shutdown();
+  } else {
+    server.shutdown();
+  }
 }
