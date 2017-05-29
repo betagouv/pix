8989d893650f33aaa8cb1f400e7091e0ff94430c
hypernikao <fenoh@octo.com>
2017-05-29 10:56:23 +0200

    Add google recaptcha key to settings

--- api/tests/unit/infrastructure/validators/grecaptcha-validator_test.js
+++ api/tests/unit/infrastructure/validators/grecaptcha-validator_test.js
@@ -1,5 +1,13 @@
-const {describe, it, expect} = require('../../../../test-helper');
+const {describe, it, expect} = require('../../../test-helper');
+const gRecaptcha = require('../../../../lib/infrastructure/validators/grecaptcha-validator');
 
-describe('Unit | Service | recaptcha-verifier', () => {
+describe.only('Unit | Service | google-recaptcha-validator', () => {
 
+  describe('#google reCaptcha validator rendering', () => {
+    it('should be a function', function() {
+      // then
+      expect(gRecaptcha.verify).to.be.a('function');
+    });
+
+  });
 });
