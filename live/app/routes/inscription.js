7350c478a5c7edabd72c7fb72b3a5b56fb3be194
hypernikao <fenoh@octo.com>
2017-06-06 14:56:45 +0200

    Initialize user model in route with empty values

--- live/app/routes/inscription.js
+++ live/app/routes/inscription.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
   model() {
-    return this.store.createRecord('user');
+    // XXX: Model needs to be initialize with empty to handle validations on all fields from Api
+    return this.store.createRecord('user', {
+      lastName: '',
+      firstName: '',
+      email: '',
+      password: '',
+      cgu: false
+    });
   },
 
   actions: {
