23b50d396f4d3155c1a6875c35d46d107c2a0698
hypernikao <fenoh@octo.com>
2017-06-08 12:30:58 +0200

    Amend endpoint handler

--- api/lib/application/users/index.js
+++ api/lib/application/users/index.js
@@ -11,7 +11,7 @@ exports.register = function(server, options, next) {
     {
       method: 'GET',
       path: '/api/users',
-      config: {handler: UserController.getProfil, tags: ['api']}
+      config: {handler: UserController.getProfile, tags: ['api']}
     }
   ]);
 
