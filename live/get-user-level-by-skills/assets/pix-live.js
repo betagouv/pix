1e940209658130010f616e1d91da7e1f0cecdcc7
Jeremy Buget <jbuget@gmail.com>
2017-06-12 10:40:56 +0000

    Release of get-user-level-by-skills with env integration (via commit hash: ab0c50a5f1b4deecdd43ff493d045762af8a443a)

--- live/get-user-level-by-skills/assets/pix-live.js
+++ live/get-user-level-by-skills/assets/pix-live.js
@@ -6912,6 +6912,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.1+9c1e672c"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.1+ab0c50a5"});
 }
 //# sourceMappingURL=pix-live.map
