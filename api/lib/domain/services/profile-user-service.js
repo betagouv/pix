1664a2cc83b19b34e961c3c4b1b21cb4eaf636bf
hypernikao <fenoh@octo.com>
2017-06-22 17:51:54 +0200

    Fix profile user test

--- api/lib/domain/services/profile-user-service.js
+++ api/lib/domain/services/profile-user-service.js
@@ -1,8 +1,20 @@
-const UserService = {
-  getUser(){
-    return new Promise((resolve, reject) => {
-      resolve();
-    });
+const userRepository = require('../../../lib/infrastructure/repositories/user-repository');
+const competenceRepository = require('../../../lib/infrastructure/repositories/competence-repository');
+const areaRepository = require('../../../lib/infrastructure/repositories/area-repository');
+
+const Profile = require('../../../lib/domain/models/data/profile');
+
+const profileService = {
+  buildUserProfile(user_id){
+    const user = userRepository.findUserById(user_id);
+    const competences = competenceRepository.list();
+    const areas = areaRepository.list();
+
+    return Promise.all([user, competences, areas])
+      .then(([user, competences, areas]) => {
+        const profile = new Profile(user, competences, areas);
+        return profile;
+      });
   }
 }
-module.exports = UserService;
+module.exports = profileService;
--- api/tests/unit/domain/services/profile-user-service_test.js
+++ api/tests/unit/domain/services/profile-user-service_test.js
@@ -1,31 +1,87 @@
-const {describe, it, expect} = require('../../../test-helper');
-const userService = require('../../../../lib/domain/services/profile-user-service');
+const {describe, it, expect, sinon} = require('../../../test-helper');
+const profileService = require('../../../../lib/domain/services/profile-user-service');
+const userRepository = require('../../../../lib/infrastructure/repositories/user-repository');
+const competenceRepository = require('../../../../lib/infrastructure/repositories/competence-repository');
+const areaRepository = require('../../../../lib/infrastructure/repositories/area-repository');
+const faker = require('faker');
 
 describe('Unit | Service | Profil User Service', function() {
+
+  const fakeUserRecords = {
+    'first-name': faker.name.findName(),
+    'last-name': faker.name.findName(),
+    'email': faker.internet.email
+  };
+  const fakeCompetenceRecords = [
+    {
+      id: 'recsvLDFHShyfDXXXXX',
+      name: '1.1 Mener une recherche d’information',
+      areaId: 'recvoGdo0z0z0pXWZ'
+    },
+    {
+      id: 'recsvLDFHShyfDXXXXX',
+      name: '1.1 Mener une recherche d’information',
+      areaId: 'recvoGdo0z0z0pXWZ'
+    }];
+
+  const fakeAreaRecords = [
+    {
+      id: 1,
+      name: 'Domaine 1'
+    },
+    {
+      id: 2,
+      name: 'Domaine 2'
+    }
+  ];
 
   describe('#getUser', () => {
 
     it('should exist', () => {
-      expect(userService.getUser).to.exist;
+      expect(profileService.buildUserProfile).to.exist;
     });
 
-    it('should return a resolved promise', () => {
-      // when
-      const promise = userService.getUser('user-id');
-      // then
-      return expect(promise).to.be.fulfilled;
-    });
+    describe('Enhanced user', () => {
 
-    it('should return a user with all competences (areas included)', () => {
-      // Given
-      const expectedUser = {};
-      // When
-      const
-        promise = userService.getUser('user-id');
-      // Then
-      return promise.then((user) => {
-        expect(user).to.deep.equal(expectedUser);
+      let userStub;
+      let competencesStub;
+      let areasStub;
+
+      beforeEach(() => {
+        userStub = sinon.stub(userRepository, 'findUserById').resolves(fakeUserRecords);
+        competencesStub = sinon.stub(competenceRepository, 'list').resolves(fakeCompetenceRecords);
+        areasStub = sinon.stub(areaRepository, 'list').resolves(fakeAreaRecords);
       });
+
+      afterEach(() => {
+        userStub.restore();
+        competencesStub.restore();
+        areasStub.restore();
+      });
+
+      it('should return a resolved promise', () => {
+        // when
+        const promise = profileService.buildUserProfile('user-id');
+        // then
+        return expect(promise).to.be.fulfilled;
+      });
+
+      it('should return an enhanced user with all competences and area', () => {
+        // Given
+        const expectedUser = {
+          user: fakeUserRecords,
+          competences: fakeCompetenceRecords,
+          areas: fakeAreaRecords
+        };
+
+        // When
+        const promise = profileService.buildUserProfile('user-id');
+        // Then
+        return promise.then((enhancedUser) => {
+          expect(enhancedUser).to.deep.equal(expectedUser);
+        });
+      });
+
     });
 
   });
