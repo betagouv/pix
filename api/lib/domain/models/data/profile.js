18c0c6b8d2a3ad858934b36b82a7448a8b0161f7
hypernikao <fenoh@octo.com>
2017-06-22 14:34:43 +0200

    Update Profile Class

--- api/lib/domain/models/data/profile.js
+++ api/lib/domain/models/data/profile.js
@@ -1,3 +1,9 @@
-const Profile = {};
+class Profile {
+  constructor(user, competences, areas) {
+    this.user = user;
+    this.competences = competences;
+    this.areas = areas;
+  }
+}
 
 module.exports = Profile;
