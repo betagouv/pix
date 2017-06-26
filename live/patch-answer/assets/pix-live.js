facecb663c4f3564dec78680e9bd1c31ac34e3ad
Jeremy Buget <jbuget@gmail.com>
2017-06-26 15:47:08 +0000

    Release of patch-answer with env integration (via commit hash: 076be97972dea21c8115aedb47b4d85319769f75)

--- live/patch-answer/assets/pix-live.js
+++ live/patch-answer/assets/pix-live.js
@@ -7347,6 +7347,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","name":"pix-live","version":"1.12.0+74f84fb8"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","name":"pix-live","version":"1.12.0+076be979"});
 }
 //# sourceMappingURL=pix-live.map
