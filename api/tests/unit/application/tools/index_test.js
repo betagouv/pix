0cd60233d20d5e65c1d9d8a4f42277271bd4661a
hypernikao <fenoh@octo.com>
2017-06-26 16:35:12 +0200

    Add Mocha to esLint

---
+++ api/tests/unit/application/tools/index_test.js
@@ -0,0 +1,36 @@
+const {expect, sinon} = require('../../../test-helper');
+const Hapi = require('hapi');
+const ToolsController = require('../../../../lib/application/tools/tools-controller');
+
+describe.only('Unit | Router | tools-router', () => {
+
+  let server;
+
+  beforeEach(() => {
+    server = new Hapi.Server();
+    server.connection({port: null});
+    server.register({register: require('../../../../lib/application/tools')});
+  });
+
+  function expectRouteToExist(routeOptions, done) {
+    server.inject(routeOptions, (res) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  }
+
+  describe('DELETE /api/tools/cache', function() {
+    before(() => {
+      sinon.stub(ToolsController, 'removeCacheEntry', (request, reply) => reply('ok'));
+    });
+
+    after(() => {
+      ToolsController.removeCacheEntry.restore();
+    });
+
+    it('should exist', (done) => {
+      return expectRouteToExist({method: 'DELETE', url: '/api/tools/cache'}, done);
+    });
+  });
+
+});
