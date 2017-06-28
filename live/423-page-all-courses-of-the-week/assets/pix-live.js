33fabddabd3bd795f9d2b837e44e01e35266f105
Jeremy Buget <jbuget@gmail.com>
2017-06-28 09:35:40 +0000

    Release of 423-page-all-courses-of-the-week with env integration (via commit hash: 22c094e111a649fde68642feec26be89038dbfb8)

--- live/423-page-all-courses-of-the-week/assets/pix-live.js
+++ live/423-page-all-courses-of-the-week/assets/pix-live.js
@@ -7188,6 +7188,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.1+439f54d6"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"name":"pix-live","version":"1.11.1+22c094e1"});
 }
 //# sourceMappingURL=pix-live.map
