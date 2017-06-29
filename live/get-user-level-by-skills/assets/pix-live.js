1eb8dd1c48e1ecd88ff2b80961a04349e1ebcec3
Jeremy Buget <jbuget@gmail.com>
2017-06-29 16:33:45 +0000

    Release of get-user-level-by-skills with env integration (via commit hash: ff2c09eb5dea73610b117df0c36b431741318b7d)

--- live/get-user-level-by-skills/assets/pix-live.js
+++ live/get-user-level-by-skills/assets/pix-live.js
@@ -7307,6 +7307,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","name":"pix-live","version":"1.12.0+dc24b313"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","name":"pix-live","version":"1.12.0+ff2c09eb"});
 }
 //# sourceMappingURL=pix-live.map
