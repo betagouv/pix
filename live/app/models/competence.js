06f251f3574520b6f9301df5882d84a939764ed6
Jérémy Buget <jbuget@gmail.com>
2017-07-03 10:19:31 +0200

    tmp

--- live/app/models/competence.js
+++ live/app/models/competence.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
-const {Model, attr, belongsTo} = DS;
+const { Model, attr, belongsTo } = DS;
 
 export default Model.extend({
   name: attr('string'),
-  area: belongsTo('area')
+  area: belongsTo('area', { inverse: null }),
+  user: belongsTo('user')
 });
