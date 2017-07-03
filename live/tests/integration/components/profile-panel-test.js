d163c88bb77e6186b657e68b6ac3a369e926b853
hypernikao <fenoh@octo.com>
2017-07-03 14:53:57 +0200

    pending profile

--- live/tests/integration/components/profile-panel-test.js
+++ live/tests/integration/components/profile-panel-test.js
@@ -3,7 +3,7 @@ import {describe, it} from 'mocha';
 import {setupComponentTest} from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describe.only('Integration | Component | profile panel', function() {
+describe('Integration | Component | profile panel', function() {
   setupComponentTest('profile-panel', {
     integration: true
   });
@@ -46,21 +46,6 @@ describe.only('Integration | Component | profile panel', function() {
       const COMPETENCY_BLOCK = '.profile-panel__competence-areas';
       expect(this.$(COMPETENCY_BLOCK)).to.have.length(1);
     });
-
-    it('should render 5 competence areas, when there are 5 competences with different area for each one', () => {
-      // given
-      const competences = {};
-      this.set('competences', competences);
-
-      // when
-      this.render(hbs`{{profile-panel competences=competences}}`);
-
-      // then
-      expect(this.$('.profile-panel__competence-area')).to.have.lengthOf(3);
-    });
-
-    it('should render only 1 competence area, when there are 5 competences with the same area');
-    it('should render 2 competence areas, when there are 5 competences related to 2 different areas');
 
   });
 });
