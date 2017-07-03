1855fc7fd5fd7834160c096b7ba5fbfc13f05a07
hypernikao <fenoh@octo.com>
2017-07-03 15:51:54 +0200

    Add Ember-group-by and new component competence-area-list

---
+++ live/app/components/competence-area-list.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import groupBy from 'ember-group-by';
+
+export default Ember.Component.extend({
+
+  classNames: ['competence-area-list'],
+
+  competences: null,
+
+  _sanitizedCompetences: Ember.computed('competences', function() {
+    const _competences = this.get('competences');
+    return _competences ? _competences : [];
+  }),
+  _competencesByArea: groupBy('_sanitizedCompetences', 'area'),
+});
