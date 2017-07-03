7f0ef202f88619b41992cb803fae4792af85cbc3
Jérémy Buget <jbuget@gmail.com>
2017-07-03 10:56:33 +0200

    Add integration tests for component profile-panel

--- live/tests/integration/components/profile-panel-test.js
+++ live/tests/integration/components/profile-panel-test.js
@@ -3,28 +3,34 @@ import {describe, it} from 'mocha';
 import {setupComponentTest} from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describe('Integration | Component | profile panel', function() {
+describe.only('Integration | Component | profile panel', function() {
   setupComponentTest('profile-panel', {
     integration: true
   });
 
   describe('(Rendering behavior) Component: ', function() {
 
-    beforeEach(function() {
-      this.render(hbs`{{profile-panel}}`);
-    });
-
     it('should be rendered', function() {
+      // when
+      this.render(hbs`{{profile-panel}}`);
+
+      // then
       expect(this.$()).to.have.length(1);
     });
 
     it('should render a wrapper', function() {
+      // when
+      this.render(hbs`{{profile-panel}}`);
+
       // then
       const WRAPPER_CLASS = '.profile-panel';
       expect(this.$(WRAPPER_CLASS)).to.have.length(1);
     });
 
     it('should render a profile header', function() {
+      // when
+      this.render(hbs`{{profile-panel}}`);
+
       // Then
       const HEADER_CLASS = '.profile-panel__header';
       const HEADER_TITLE = '.profile-header__title';
@@ -33,10 +39,28 @@ describe('Integration | Component | profile panel', function() {
     });
 
     it('should render a competency profile block', function() {
+      // when
+      this.render(hbs`{{profile-panel}}`);
+
       // Then
-      const COMPETENCY_BLOCK = '.profile-panel__competency-profile';
+      const COMPETENCY_BLOCK = '.profile-panel__competence-areas';
       expect(this.$(COMPETENCY_BLOCK)).to.have.length(1);
     });
+
+    it('should render 5 competence areas, when there are 5 competences with different area for each one', () => {
+      // given
+      const competences = {};
+      this.set('competences', competences);
+
+      // when
+      this.render(hbs`{{profile-panel competences=competences}}`);
+
+      // then
+      expect(this.$('.profile-panel__competence-area')).to.have.lengthOf(3);
+    });
+
+    it('should render only 1 competence area, when there are 5 competences with the same area');
+    it('should render 2 competence areas, when there are 5 competences related to 2 different areas');
 
   });
 });
