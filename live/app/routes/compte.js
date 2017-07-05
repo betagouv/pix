5fa07fba98644fc3e6ca2abe87eb04f2edc3b01a
Jérémy Buget <jbuget@gmail.com>
2017-07-05 11:03:50 +0200

    Add mirage models and implement user profile adapter

--- live/app/routes/compte.js
+++ live/app/routes/compte.js
@@ -9,7 +9,10 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model() {
     const store = this.get('store');
     const userAdapter = store.adapterFor('user');
-    return userAdapter.getAuthenticatedUserProfile(store);
+    return userAdapter.getAuthenticatedUserProfile().then(payload => {
+      const user = store.push(payload);
+      return user;
+    });
   },
 
 });
