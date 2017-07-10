0025d3e829bbe73d0b57349c0adec69fc6f30c4a
Jeremy Buget <jbuget@gmail.com>
2017-07-10 13:26:05 +0000

    Release of staging with env staging (via commit hash: 5eb5d323f836d29893e4c04a04327f75f96edaf5)

--- live/staging/assets/pix-live.js
+++ live/staging/assets/pix-live.js
@@ -7383,6 +7383,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","name":"pix-live","version":"1.14.0+a2b1016e"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","name":"pix-live","version":"1.14.0+5eb5d323"});
 }
 //# sourceMappingURL=pix-live.map
