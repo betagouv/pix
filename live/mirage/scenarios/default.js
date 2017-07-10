d5df70341a094c65c3ce4c8feb817b01503de105
hypernikao <fenoh@octo.com>
2017-07-10 10:17:54 +0200

    Remove comments

--- live/mirage/scenarios/default.js
+++ live/mirage/scenarios/default.js
@@ -1,12 +1,5 @@
 export default function(server) {
 
-  /*
-   Seed your development database using your factories.
-   This data will not be loaded in your tests.
-
-   Make sure to define a factory for each model you want to create.
-   */
-
   server.loadFixtures('areas');
   server.loadFixtures('competences');
 
