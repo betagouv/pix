6764a3f269be700ba1d48c0dc77d29b574131156
hypernikao <fenoh@octo.com>
2017-07-24 15:33:42 +0200

    Add user details to navbar

--- live/tests/unit/components/navbar-header-test.js
+++ live/tests/unit/components/navbar-header-test.js
@@ -1,18 +1,18 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
-import { setupTest } from 'ember-mocha';
+import {expect} from 'chai';
+import {describe, it} from 'mocha';
+import {setupTest} from 'ember-mocha';
 
 describe('Unit | Component | Navar Header Component', function() {
   setupTest('component:navbar-header', {});
 
   describe('#isUserLogged', function() {
     [
-      { given: '', expected: false },
-      { given: ' ', expected: false },
-      { given: null, expected: false },
-      { given: undefined, expected: false },
-      { given: { firstName: 'FHI' }, expected: true }
-    ].forEach(({ given, expected }) => {
+      {given: '', expected: false},
+      {given: ' ', expected: false},
+      {given: null, expected: false},
+      {given: undefined, expected: false},
+      {given: {firstName: 'FHI'}, expected: true}
+    ].forEach(({given, expected}) => {
       it(`should return ${expected}, when "${given}" provided`, function() {
         // given
         const component = this.subject();
