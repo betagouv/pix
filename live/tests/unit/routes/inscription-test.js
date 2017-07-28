e505a7d8bd000d8c0c22e38efab149414e8871f4
hypernikao <fenoh@octo.com>
2017-07-28 14:30:00 +0200

    Add action redirectToCompte to authenticate user and redirect him to his compte page

--- live/tests/unit/routes/inscription-test.js
+++ live/tests/unit/routes/inscription-test.js
@@ -2,13 +2,40 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
 
+class SessionStub {
+  authenticate() {
+    this.callArgs = Array.from(arguments);
+    return Promise.resolve();
+  }
+}
+
 describe('Unit | Route | inscription', function() {
   setupTest('route:inscription', {
-    needs: ['service:current-routed-modal']
+    needs: ['service:current-routed-modal', 'service:session']
   });
 
   it('exists', function() {
     const route = this.subject();
     expect(route).to.be.ok;
+  });
+
+  const expectedEmail = 'email@example.net';
+  const expectedPassword = 'azerty';
+  const sessionStub = new SessionStub();
+
+  it('should redirect to /compte authenticated user', function() {
+    // Given
+    const route = this.subject();
+    route.set('session', sessionStub);
+    route.transitionTo = () => {
+    };
+
+    // When
+    const promise = route.actions.redirectToCompte.call(route, expectedEmail, expectedPassword);
+
+    // Then
+    return promise.then(() => {
+      expect(sessionStub.callArgs).to.deep.equal(['authenticator:simple', expectedEmail, expectedPassword]);
+    });
   });
 });
