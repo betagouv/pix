f213f52c3a3d508b0acfa19c36a4fd0d691f899a
hypernikao <fenoh@octo.com>
2017-07-31 16:55:37 +0200

    Add test for session in navbar component

--- live/tests/integration/components/navbar-header-test.js
+++ live/tests/integration/components/navbar-header-test.js
@@ -3,6 +3,7 @@ import { beforeEach, describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 import wait from 'ember-test-helpers/wait';
+import Ember from 'ember';
 
 describe('Integration | Component | navbar-header', function() {
 
@@ -10,26 +11,31 @@ describe('Integration | Component | navbar-header', function() {
     integration: true
   });
 
-  beforeEach(function() {
-    this.render(hbs`{{navbar-header}}`);
-  });
+  describe('Rendering when user is not logged', function() {
+    beforeEach(function() {
+      this.register('service:session', Ember.Service.extend({ isAuthenticated: false }));
+      this.inject.service('session', { as: 'session' });
 
-  it('renders', function() {
-    expect(this.$()).to.have.length(1);
-  });
+      this.render(hbs`{{navbar-header}}`);
+    });
 
-  it('should display the Pix logo', function() {
-    expect(this.$('.navbar-header-logo')).to.have.lengthOf(1);
-    expect(this.$('.pix-logo')).to.have.lengthOf(1);
-  });
+    it('renders', function() {
+      expect(this.$()).to.have.length(1);
+    });
 
-  it('should display a link to "project" page', function() {
-    expect(this.$('.navbar-header-links__link--project')).to.have.lengthOf(1);
-  });
+    it('should display the Pix logo', function() {
+      expect(this.$('.navbar-header-logo')).to.have.lengthOf(1);
+      expect(this.$('.pix-logo')).to.have.lengthOf(1);
+    });
 
-  it('should display a link to "referential" page', function() {
-    expect(this.$('.navbar-header-links__link--competences')).to.have.lengthOf(1);
-    expect(this.$('.navbar-header-links--user-logged')).to.have.length(0);
+    it('should display a link to "project" page', function() {
+      expect(this.$('.navbar-header-links__link--project')).to.have.lengthOf(1);
+    });
+
+    it('should display a link to "referential" page', function() {
+      expect(this.$('.navbar-header-links__link--competences')).to.have.lengthOf(1);
+      expect(this.$('.navbar-header-links--user-logged')).to.have.length(0);
+    });
   });
 
   describe('Display user details', function() {
@@ -37,6 +43,8 @@ describe('Integration | Component | navbar-header', function() {
     describe('When user is logged', function() {
 
       beforeEach(function() {
+        this.register('service:session', Ember.Service.extend({ isAuthenticated: true }));
+        this.inject.service('session', { as: 'session' });
         this.set('user', { firstName: 'FHI', lastName: '4EVER' });
       });
 
