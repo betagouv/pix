362f6e63b3ad86ac52343af1cd0b57aca42ceccc
Jeremy Buget <jbuget@gmail.com>
2017-08-01 08:15:03 +0000

    Release of 573-page-sco-sup-enrollment with env integration (via commit hash: dd78f70f98ff32922431c788ecfe605d57859446)

--- live/573-page-sco-sup-enrollment/assets/pix-live.js
+++ live/573-page-sco-sup-enrollment/assets/pix-live.js
@@ -8184,6 +8184,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","FEEDBACK_PANEL_SCROLL_DURATION":800,"name":"pix-live","version":"1.15.0+eb5b9a3e"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","FEEDBACK_PANEL_SCROLL_DURATION":800,"name":"pix-live","version":"1.15.0+dd78f70f"});
 }
 //# sourceMappingURL=pix-live.map
