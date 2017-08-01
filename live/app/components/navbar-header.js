1bb846b046bebb8bfea63b045dafc404d0b94417
hypernikao <fenoh@octo.com>
2017-08-01 11:11:21 +0200

    Extract user menu details to other component

--- live/app/components/navbar-header.js
+++ live/app/components/navbar-header.js
@@ -2,18 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   session: Ember.inject.service(),
+  store: Ember.inject.service(),
   classNames: ['navbar-header'],
-  user: null,
   _canDisplayMenu: false,
 
   isUserLogged: Ember.computed('session', function() {
     return this.get('session.isAuthenticated');
-  }),
+  })
 
-  actions: {
-    toggleUserMenu() {
-      const canShowMenu = !this.get('_canDisplayMenu');
-      this.set('_canDisplayMenu', canShowMenu);
-    }
-  }
 });
