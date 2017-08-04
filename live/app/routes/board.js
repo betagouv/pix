bf14dc86a53fbe03313b4905fdce132b4760b1d6
Brandone Martins <brandone.martins@gmail.com>
2017-08-04 14:58:52 +0200

    [US-590] Add real Api route to get informations for the board page (adapter and model)

--- live/app/routes/board.js
+++ live/app/routes/board.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-    return this.get('store').findRecord('organization', 1);
+    return this.get('store').queryRecord('organization', {});
   }
 });
