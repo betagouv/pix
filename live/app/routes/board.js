560a31a28c16a0ec8df5bd7d0a853fb712d7fae0
Brandone Martins <brandone.martins@gmail.com>
2017-08-08 15:10:25 +0200

    [US-590] implement board route and unit test with its

--- live/app/routes/board.js
+++ live/app/routes/board.js
@@ -5,8 +5,6 @@ export default Ember.Route.extend({
   model() {
     return this.get('store').queryRecord('user', {})
       .then((user) => {
-      console.log(user);
-      console.log(user.get('organizations'));
         return user.get('organizations.firstObject');
       })
       .catch(_ => {
