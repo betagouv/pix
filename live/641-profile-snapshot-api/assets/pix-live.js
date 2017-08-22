49687cdd58b8182fdd35a5895685544af0ef0605
Jeremy Buget <jbuget@gmail.com>
2017-08-22 20:09:11 +0000

    Release of 641-profile-snapshot-api with env integration (via commit hash: 6e902bfe79268fe15fc30d0fc4c220e20f453ec9)

--- live/641-profile-snapshot-api/assets/pix-live.js
+++ live/641-profile-snapshot-api/assets/pix-live.js
@@ -8379,6 +8379,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","FEEDBACK_PANEL_SCROLL_DURATION":800,"name":"pix-live","version":"1.17.0+68437fa3"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","FEEDBACK_PANEL_SCROLL_DURATION":800,"name":"pix-live","version":"1.17.0+6e902bfe"});
 }
 //# sourceMappingURL=pix-live.map
