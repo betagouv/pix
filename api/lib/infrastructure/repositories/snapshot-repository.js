371aaf0356116099c6753a1a86fbe9c84050f34b
hypernikao <fenoh@octo.com>
2017-08-29 18:25:10 +0200

    implement getSharedProfiles in organizationController

--- api/lib/infrastructure/repositories/snapshot-repository.js
+++ api/lib/infrastructure/repositories/snapshot-repository.js
@@ -3,5 +3,9 @@ const Snapshot = require('../../domain/models/data/snapshot');
 module.exports = {
   save(snapshotRawData) {
     return new Snapshot(snapshotRawData).save();
+  },
+
+  getSnapshotsByOrganizationId() {
+
   }
 };
