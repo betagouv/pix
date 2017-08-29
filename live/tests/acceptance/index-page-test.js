d01ffc461e6edb891bd787654987c06bbc751eb3
Jérémy Buget <jbuget@gmail.com>
2017-08-29 15:50:07 +0200

    Hide weekly challenges when user is not authenticated

--- live/tests/acceptance/index-page-test.js
+++ live/tests/acceptance/index-page-test.js
@@ -14,7 +14,16 @@ describe('Acceptance | index page', function() {
     destroyApp(application);
   });
 
-  describe('Hero section', function() {
+  function authenticateUser() {
+    server.create('user');
+
+    visit('/connexion');
+    fillIn('#pix-email', 'samurai.jack@aku.world');
+    fillIn('#pix-password', 'B@ck2past');
+    click('.signin-form__submit_button');
+  }
+
+  describe('"Hero" section', function() {
 
     it('should have a link to sign-up page when user is not authenticated', function() {
       // when
@@ -27,15 +36,6 @@ describe('Acceptance | index page', function() {
       });
     });
 
-    function authenticateUser() {
-      server.create('user');
-
-      visit('/connexion');
-      fillIn('#pix-email', 'samurai.jack@aku.world');
-      fillIn('#pix-password', 'B@ck2past');
-      click('.signin-form__submit_button');
-    }
-
     it('should not have a link to sign-up page when user is yet authenticated', function() {
       // given
       authenticateUser();
@@ -46,6 +46,46 @@ describe('Acceptance | index page', function() {
       // then
       return andThen(function() {
         expect(find('.index-page-hero__inscription-link')).to.have.lengthOf(0);
+      });
+    });
+  });
+
+  describe('"Weekly challenges" section', function() {
+
+    beforeEach(function() {
+      visit('/deconnexion');
+    });
+
+    describe('when user is not authenticated', function() {
+
+      beforeEach(function() {
+        visit('/');
+      });
+
+      it('should not be rendered when user is not authenticated', function() {
+        expect(find('.index-page__section--challenges')).to.have.lengthOf(0);
+      });
+    });
+
+    describe('when user is authenticated', function() {
+
+      beforeEach(function() {
+        authenticateUser();
+        visit('/');
+      });
+
+      it('should be rendered when user is yet authenticated', function() {
+        findWithAssert('.index-page__section--challenges');
+      });
+
+      it('should have a title', function() {
+        const $title = findWithAssert('.index-page-challenges__presentation-title');
+        expect($title.text().trim()).to.equal('Les défis Pix de la semaine');
+      });
+
+      it('should have a description', function() {
+        const $description = findWithAssert('.index-page-challenges__presentation-text');
+        expect($description.text().trim()).to.equal('Chaque semaine, testez vos compétences numériques sur un nouveau sujet.');
       });
     });
   });
