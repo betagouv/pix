f37e9d9cc308e829c5db8db7eb3f907a11fd3418
hypernikao <fenoh@octo.com>
2017-08-29 17:00:19 +0200

    Add profile completion service$

--- api/lib/domain/services/profile-completion-service.js
+++ api/lib/domain/services/profile-completion-service.js
@@ -1,1 +1,24 @@
-module.exports = {}
+const JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
+
+module.exports = {
+
+  getPercentage(profile) {
+
+    return new JSONAPIDeserializer({ keyForAttribute: 'camelCase' })
+      .deserialize(profile)
+      .then((deserializedProfile) => {
+        const numberOfEvaluatedCompetences = _getEvalutedCompetences(deserializedProfile.competences);
+        return Math.round((numberOfEvaluatedCompetences / 16) * 100);
+      });
+  }
+};
+
+function _getEvalutedCompetences(competences) {
+  return competences.reduce((assessed, competence) => {
+    if(competence.level != -1) {
+      ++assessed;
+    }
+
+    return assessed;
+  }, 0);
+}
