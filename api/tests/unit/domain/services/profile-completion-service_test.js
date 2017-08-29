d1689c2202c442d316ac05431334abcbdd363ab2
hypernikao <fenoh@octo.com>
2017-08-29 16:43:30 +0200

    add a service to compute completion percentation

---
+++ api/tests/unit/domain/services/profile-completion-service_test.js
@@ -0,0 +1,12 @@
+const {describe, it, expect, beforeEach, afterEach, sinon} = require('../../../test-helper');
+
+describe('Unit | Service | profile completion percentage service', function() {
+
+  it('should return ', function() {
+    // given
+
+    // when
+
+    // then
+  });
+});
