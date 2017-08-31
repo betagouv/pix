ee18cdaa777b84a32d0aa45e3d5e9e0068d14171
Jeremy Buget <jbuget@gmail.com>
2017-08-31 16:09:24 +0000

    Release of 594-show-shared-snapshots-for-logged-prescriber with env integration (via commit hash: 1d06ec21c6097cffc9f14c38cf6070e74cb62426)

--- live/594-show-shared-snapshots-for-logged-prescriber/assets/pix-live.js
+++ live/594-show-shared-snapshots-for-logged-prescriber/assets/pix-live.js
@@ -8871,6 +8871,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","FEEDBACK_PANEL_SCROLL_DURATION":800,"name":"pix-live","version":"1.18.0+0d8b7f51"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","FEEDBACK_PANEL_SCROLL_DURATION":800,"name":"pix-live","version":"1.18.0+1d06ec21"});
 }
 //# sourceMappingURL=pix-live.map
