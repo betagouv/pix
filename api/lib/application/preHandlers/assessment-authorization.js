d078ca652b766833aa63a7eebc9cfbdac23e8592
hypernikao <fenoh@octo.com>
2017-09-15 10:46:30 +0200

    Add new pre-handler

--- api/lib/application/preHandlers/assessment-authorization.js
+++ api/lib/application/preHandlers/assessment-authorization.js
@@ -1,5 +1,26 @@
+const jsonwebtokenUtils = require('../../infrastructure/utils/jsonwebtoken-utils');
+const assessmentRepository = require('../../infrastructure/repositories/assessment-repository');
+const validationErrorSerializer = require('../../infrastructure/serializers/jsonapi/validation-error-serializer');
+
 module.exports = {
-  verify() {
-    
+  verify(request, reply) {
+    const userId = jsonwebtokenUtils.extractUserId(request.headers);
+    const assessmentId = request.params.id;
+
+    return assessmentRepository
+      .getByUserIdAndAssessmentId(assessmentId, userId)
+      .then(reply)
+      .catch(() => {
+        const buildedError = _handleWhenInvalidAuthorization('Vous n’êtes pas autorisé à accéder à cette évaluation');
+        return reply(validationErrorSerializer.serialize(buildedError)).code(401).takeover();
+      });
   }
 };
+
+function _handleWhenInvalidAuthorization(errorMessage) {
+  return {
+    data: {
+      authorization: [errorMessage]
+    }
+  };
+}
--- api/tests/unit/application/preHandlers/assessment-authorization_test.js
+++ api/tests/unit/application/preHandlers/assessment-authorization_test.js
@@ -1,12 +1,124 @@
 const { describe, it, expect, sinon, beforeEach, afterEach } = require('../../../test-helper');
 const AssessmentAuhorization = require('../../../../lib/application/preHandlers/assessment-authorization');
+const jsonwebtokenUtils = require('../../../../lib/infrastructure/utils/jsonwebtoken-utils');
+const assessmentRepository = require('../../../../lib/infrastructure/repositories/assessment-repository');
 
-describe.only('Unit | Pre-handler | Assessment Authorization', () => {
+describe('Unit | Pre-handler | Assessment Authorization', () => {
 
   describe('#verify', () => {
+
+    let sandbox;
+    let replyStub;
+    let codeStub;
+    const request = {
+      headers: { authorization: 'VALID_TOKEN' },
+      params: {
+        id: 8
+      }
+    };
+
+    beforeEach(() => {
+      sandbox = sinon.sandbox.create();
+      sandbox.stub(jsonwebtokenUtils, 'extractUserId');
+      sandbox.stub(assessmentRepository, 'getByUserIdAndAssessmentId');
+      codeStub = sandbox.stub();
+      replyStub = sandbox.stub().returns({
+        code: codeStub
+      });
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
     it('should be a function', () => {
       // then
       expect(AssessmentAuhorization.verify).to.be.a('function');
     });
+
+    it('should get userId from token', (done) => {
+      // given
+      jsonwebtokenUtils.extractUserId.returns('userId');
+      assessmentRepository.getByUserIdAndAssessmentId.resolves();
+
+      // when
+      const promise = AssessmentAuhorization.verify(request, replyStub);
+
+      // then
+      promise.then(() => {
+        sinon.assert.calledOnce(jsonwebtokenUtils.extractUserId);
+        sinon.assert.calledWith(jsonwebtokenUtils.extractUserId, request.headers);
+        done();
+      });
+    });
+
+    describe('When assessment is linked to userId (userId exist)', () => {
+
+      it('should reply with assessment', (done) => {
+        // given
+        const fetchedAssessment = {};
+        const extractedUserId = 'userId';
+        jsonwebtokenUtils.extractUserId.returns(extractedUserId);
+        assessmentRepository.getByUserIdAndAssessmentId.resolves(fetchedAssessment);
+
+        // when
+        const promise = AssessmentAuhorization.verify(request, replyStub);
+
+        // then
+        promise.then(() => {
+          sinon.assert.calledOnce(assessmentRepository.getByUserIdAndAssessmentId);
+          sinon.assert.calledWith(assessmentRepository.getByUserIdAndAssessmentId, request.params.id, extractedUserId);
+          sinon.assert.calledOnce(replyStub);
+          sinon.assert.calledWith(replyStub, fetchedAssessment);
+          done();
+        });
+      });
+    });
+
+    describe('When assessment is linked a null userId', () => {
+
+      it('should reply with assessment', (done) => {
+        // given
+        const fetchedAssessment = {};
+        const extractedUserId = null;
+        jsonwebtokenUtils.extractUserId.returns(extractedUserId);
+        assessmentRepository.getByUserIdAndAssessmentId.resolves(fetchedAssessment);
+
+        // when
+        const promise = AssessmentAuhorization.verify(request, replyStub);
+
+        // then
+        promise.then(() => {
+          sinon.assert.calledOnce(assessmentRepository.getByUserIdAndAssessmentId);
+          sinon.assert.calledWith(assessmentRepository.getByUserIdAndAssessmentId, request.params.id, extractedUserId);
+          sinon.assert.calledOnce(replyStub);
+          sinon.assert.calledWith(replyStub, fetchedAssessment);
+          done();
+        });
+      });
+    });
+
+    describe('When userId (from token) is not linked to assessment', () => {
+      it('should take over the request and response with 401 status code', () => {
+        // given
+        const extractedUserId = null;
+        const takeOverSpy = sinon.spy();
+        codeStub.returns({
+          takeover: takeOverSpy
+        });
+        jsonwebtokenUtils.extractUserId.returns(extractedUserId);
+        assessmentRepository.getByUserIdAndAssessmentId.rejects();
+        // when
+        const promise = AssessmentAuhorization.verify(request, replyStub);
+
+        // then
+        return promise.then(() => {
+          sinon.assert.calledOnce(replyStub);
+          sinon.assert.calledWith(codeStub, 401);
+          sinon.assert.calledOnce(takeOverSpy);
+        });
+      });
+    });
+
   });
 });
