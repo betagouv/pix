939dcc61d8327494bd1bb6bdd06d72b469b74980
Jérémy Buget <jbuget@gmail.com>
2017-09-18 00:26:21 +0200

    Replace last Bower dependencies by NPM ones

--- live/ember-cli-build.js
+++ live/ember-cli-build.js
@@ -24,8 +24,9 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-  app.import('bower_components/js-yaml/dist/js-yaml.js');
+  app.import('node_modules/bootstrap/dist/js/bootstrap.js');
+  app.import('node_modules/bootstrap/dist/css/bootstrap.css');
+  app.import('node_modules/js-yaml/dist/js-yaml.js');
 
   return app.toTree();
 };
