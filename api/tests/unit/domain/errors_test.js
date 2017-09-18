437e6fbd6b712a0482a4034e5aa5ada3ca3692c1
hypernikao <fenoh@octo.com>
2017-09-18 16:44:23 +0200

    Add new errors internalError and userNotFoundError

--- api/tests/unit/domain/errors_test.js
+++ api/tests/unit/domain/errors_test.js
@@ -15,4 +15,46 @@ describe('Unit | Domain | Errors', () => {
     expect(errors.InvaliOrganizationIdError).to.exist;
   });
 
+  describe('#userNotFoundError', () => {
+    it('should export a userNotFoundError', () => {
+      expect(errors.userNotFoundError).to.exist;
+    });
+
+    it('should have a getErrorMessage method', (done) => {
+      // given
+      const expectedErrorMessage = {
+        data: {
+          email: ['Cette adresse email n’existe pas.']
+        }
+      };
+
+      // then
+      const userNotFoundError = errors.userNotFoundError;
+      expect(userNotFoundError.getErrorMessage).to.be.a('function');
+      expect(userNotFoundError.getErrorMessage()).to.eql(expectedErrorMessage);
+      done();
+    });
+  });
+
+  describe('#internalError', () => {
+    it('should export a internalError', () => {
+      expect(errors.internalError).to.exist;
+    });
+
+    it('should have a getErrorMessage method', (done) => {
+      // given
+      const expectedErrorMessage = {
+        data: {
+          error: ['Une erreur interne est survenue.']
+        }
+      };
+
+      // then
+      const internalError = errors.internalError;
+      expect(internalError.getErrorMessage).to.be.a('function');
+      expect(internalError.getErrorMessage()).to.eql(expectedErrorMessage);
+      done();
+    });
+  });
+
 });
