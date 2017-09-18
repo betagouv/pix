72ae71ffaf423385522b60676c558c995d02ec6e
Jérémy Buget <jbuget@gmail.com>
2017-09-18 16:44:15 +0000

    Release of 798-ask-password-reset-api with env integration (via commit hash: b2cacdd5a0e467c684e18169625b5efc01667e0a)

--- live/798-ask-password-reset-api/assets/pix-live.js
+++ live/798-ask-password-reset-api/assets/pix-live.js
@@ -8624,6 +8624,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.23.0+82d18973"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.23.0+b2cacdd5"});
 }
 //# sourceMappingURL=pix-live.map
