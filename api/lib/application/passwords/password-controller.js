42ddbd463e14da9e5795b73f7c619a8a0355211d
hypernikao <fenoh@octo.com>
2017-09-19 18:36:51 +0200

    WIP on password-controller

--- api/lib/application/passwords/password-controller.js
+++ api/lib/application/passwords/password-controller.js
@@ -1,11 +1,22 @@
+const Boom = require('boom');
 const userService = require('../../domain/services/user-service');
+const tokenService = require('../../domain/services/token-service');
+const { UserNotFoundError } = require('../../domain/errors');
+const errorSerializer = require('../../infrastructure/serializers/jsonapi/validation-error-serializer');
 
 module.exports = {
   resetDemand(request, reply) {
     if (!(request.hasOwnProperty('payload') && ('email' in request.payload))) {
-      return reply().code(400);
+      return reply(Boom.badRequest());
     }
 
-    return userService.isUserExisting(request.payload.email);
+    return userService
+      .isUserExisting(request.payload.email)
+      .then(() => tokenService.generateTemporaryKey())
+      .catch((err) => {
+        if (err instanceof UserNotFoundError) {
+          return reply(errorSerializer.serialize(UserNotFoundError.getErrorMessage()));
+        }
+      });
   }
 };
