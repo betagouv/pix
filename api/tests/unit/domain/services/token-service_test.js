d6b413a6dd59cdaf2b471781f51bab0d111d8f27
hypernikao <fenoh@octo.com>
2017-09-19 12:09:49 +0200

    Add token service test

--- api/tests/unit/domain/services/token-service_test.js
+++ api/tests/unit/domain/services/token-service_test.js
@@ -1,6 +1,8 @@
-const { describe, it, expect } = require('../../../test-helper');
+const { describe, it, expect, sinon, beforeEach, afterEach } = require('../../../test-helper');
 const tokenService = require('../../../../lib/domain/services/token-service');
 const User = require('../../../../lib/domain/models/data/user');
+const settings = require('../../../../lib/settings');
+const jsonwebtoken = require('jsonwebtoken');
 
 describe('Unit | Service | Token Service', function() {
 
@@ -33,5 +35,44 @@ describe('Unit | Service | Token Service', function() {
       expect(result).to.equal(null);
     });
 
+  });
+
+  describe('#generateTemporaryKey', function() {
+
+    beforeEach(() => {
+      sinon.stub(jsonwebtoken, 'sign');
+    });
+
+    afterEach(() => {
+      jsonwebtoken.sign.restore();
+    });
+
+    it('should be a function', () => {
+      expect(tokenService.generateTemporaryKey).to.exist.and.to.be.a('function');
+    });
+
+    it('should call sign function from jwt', () => {
+      // given
+      const signParams = {
+        payload: { data: settings.temporaryKey.payload },
+        secret: settings.temporaryKey.secret,
+        expiration: { expiresIn: settings.temporaryKey.tokenLifespan }
+      };
+
+      // when
+      tokenService.generateTemporaryKey();
+
+      // then
+      sinon.assert.calledOnce(jsonwebtoken.sign);
+      sinon.assert.calledWith(jsonwebtoken.sign, signParams.payload, signParams.secret, signParams.expiration);
+    });
+
+    it('should return a token generated from jsonWebToken', function() {
+      // given
+
+      // when
+
+      // then
+    });
   });
 });
