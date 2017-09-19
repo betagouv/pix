d78301039ebaab8a961b90e723f758fe79e6c014
hypernikao <fenoh@octo.com>
2017-09-19 12:08:30 +0200

    Add new method generateTemporaryToken

--- api/lib/domain/services/user-service.js
+++ api/lib/domain/services/user-service.js
@@ -1,10 +1,13 @@
 const userRepository = require('../../../lib/infrastructure/repositories/user-repository');
+const { UserNotFoundError } = require('../errors');
 
 module.exports = {
   isUserExisting(email) {
     return userRepository
       .findByEmail(email)
       .then(_ => true)
-      .catch(_ => false);
+      .catch(_ => {
+        throw new UserNotFoundError();
+      });
   }
 };
