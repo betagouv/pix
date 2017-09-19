fb5df46131532eb911502256dc892f13b852303c
hypernikao <fenoh@octo.com>
2017-09-19 18:36:51 +0200

    WIP on password-controller

--- api/tests/unit/application/passwords/password-controller_test.js
+++ api/tests/unit/application/passwords/password-controller_test.js
@@ -1,8 +1,12 @@
+const Boom = require('boom');
 const { describe, it, expect, beforeEach, afterEach, sinon } = require('../../../test-helper');
 const passwordController = require('../../../../lib/application/passwords/password-controller');
 const userService = require('../../../../lib/domain/services/user-service');
+const tokenService = require('../../../../lib/domain/services/token-service');
+const errorSerializer = require('../../../../lib/infrastructure/serializers/jsonapi/validation-error-serializer');
+const { UserNotFoundError } = require('../../../../lib/domain/errors');
 
-describe('Unit | Controller | PasswordController', () => {
+describe.only('Unit | Controller | PasswordController', () => {
 
   describe('#resetDemand', () => {
 
@@ -14,13 +18,12 @@ describe('Unit | Controller | PasswordController', () => {
     describe('Payload bad format cases: ', () => {
 
       let replyStub;
-      let codeSpy;
       let sandbox;
 
       beforeEach(() => {
         sandbox = sinon.sandbox.create();
         replyStub = sandbox.stub();
-        codeSpy = sandbox.spy();
+        sandbox.stub(Boom, 'badRequest').returns({});
       });
 
       afterEach(() => {
@@ -36,13 +39,15 @@ describe('Unit | Controller | PasswordController', () => {
         it(`should reply with 400 status, when ${description} provided`, () => {
           // given
           replyStub.returns({
-            code: codeSpy
+            code: () => {
+            }
           });
           // when
           passwordController.resetDemand(request, replyStub);
 
           // then
-          sinon.assert.calledWith(codeSpy, 400);
+          sinon.assert.calledOnce(Boom.badRequest);
+          sinon.assert.calledWith(replyStub, Boom.badRequest());
         });
       });
     });
@@ -52,25 +57,74 @@ describe('Unit | Controller | PasswordController', () => {
       const request = { payload: { email: 'shif@fu.me' } };
 
       let replyStub;
-      let codeSpy;
       let sandbox;
 
       beforeEach(() => {
         sandbox = sinon.sandbox.create();
         replyStub = sandbox.stub();
-        codeSpy = sandbox.spy();
         sandbox.stub(userService, 'isUserExisting');
+        sandbox.stub(tokenService, 'generateTemporaryKey');
+        sandbox.stub(errorSerializer, 'serialize');
       });
 
       afterEach(() => {
         sandbox.restore();
       });
 
-      it('should verify user existence (by email)', () => {
+      describe('User existence test cases', () => {
+
+        it('should verify user existence (by email)', () => {
+          // given
+          userService.isUserExisting.resolves();
+          replyStub.returns({
+            code: () => {
+            }
+          });
+
+          //when
+          const promise = passwordController.resetDemand(request, replyStub);
+
+          // then
+          return promise.then(() => {
+            sinon.assert.calledOnce(userService.isUserExisting);
+            sinon.assert.calledWith(userService.isUserExisting, request.payload.email);
+          });
+        });
+
+        it('should rejects an error, when user is not found', () => {
+          // given
+          const error = new UserNotFoundError();
+          const expectedErrorMessage = UserNotFoundError.getErrorMessage();
+          const serializedError = {};
+          errorSerializer.serialize.returns(serializedError);
+          userService.isUserExisting.rejects(error);
+          replyStub.returns({
+            code: () => {
+            }
+          });
+
+          //when
+          const promise = passwordController.resetDemand(request, replyStub);
+
+          // then
+          return promise.then(() => {
+            sinon.assert.calledOnce(errorSerializer.serialize);
+            sinon.assert.calledWith(errorSerializer.serialize, expectedErrorMessage);
+            sinon.assert.calledOnce(replyStub);
+            sinon.assert.calledWith(replyStub, serializedError);
+          });
+        });
+
+      });
+
+      it('should ask for a temporary token generation', () => {
         // given
+        const generatedToken = 'token';
         userService.isUserExisting.resolves();
+        tokenService.generateTemporaryKey.resolves(generatedToken);
         replyStub.returns({
-          code: codeSpy
+          code: () => {
+          }
         });
 
         //when
@@ -78,11 +132,9 @@ describe('Unit | Controller | PasswordController', () => {
 
         // then
         return promise.then(() => {
-          sinon.assert.calledOnce(userService.isUserExisting);
-          sinon.assert.calledWith(userService.isUserExisting, request.payload.email);
+          sinon.assert.calledOnce(tokenService.generateTemporaryKey);
         });
       });
-
     });
   });
 
