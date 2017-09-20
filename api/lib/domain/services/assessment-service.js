4ac5b1f0f1e5d08341b3a9c10962b5b167bb238c
Brandone Martins <brandone.martins@gmail.com>
2017-09-20 18:20:41 +0200

    implement condition for first question in an adaptive test

--- api/lib/domain/services/assessment-service.js
+++ api/lib/domain/services/assessment-service.js
@@ -54,6 +54,7 @@ function _selectNextChallengeId(course, currentChallengeId, assessment) {
   }
 
   return Promise.resolve(_selectNextInNormalMode(currentChallengeId, challenges));
+
 }
 
 function getScoredAssessment(assessmentId) {
