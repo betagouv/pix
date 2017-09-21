95d9f6a8a5850ea750263f38842134017386dcb8
hypernikao <fenoh@octo.com>
2017-09-21 12:11:46 +0200

    Fix workflow in password-controller

--- api/lib/application/passwords/password-controller.js
+++ api/lib/application/passwords/password-controller.js
@@ -1,7 +1,9 @@
 const Boom = require('boom');
 const userService = require('../../domain/services/user-service');
-const tokenService = require('../../domain/services/token-service');
-const { UserNotFoundError } = require('../../domain/errors');
+const mailService = require('../../domain/services/mail-service');
+const resetPasswordService = require('../../domain/services/reset-password-service');
+const resetPasswordDemandRepository = require('../../infrastructure/repositories/reset-password-demands-repository');
+const { UserNotFoundError, InternalError } = require('../../domain/errors');
 const errorSerializer = require('../../infrastructure/serializers/jsonapi/validation-error-serializer');
 
 module.exports = {
@@ -10,13 +12,27 @@ module.exports = {
       return reply(Boom.badRequest());
     }
 
+    const { email } = request.payload;
+    let temporarykey;
+
     return userService
-      .isUserExisting(request.payload.email)
-      .then(() => tokenService.generateTemporaryKey())
+      .isUserExisting(email)
+      .then(() => resetPasswordService.invalidOldResetPasswordDemand(email))
+      .then(() => {
+        temporarykey = resetPasswordService.generateTemporaryKey();
+        return temporarykey;
+      })
+      .then((temporaryKey) => resetPasswordDemandRepository.create({ email, temporaryKey }))
+      .then(() => {
+        mailService.sendResetPasswordDemandEmail(email, temporarykey);
+        reply();
+      })
       .catch((err) => {
         if (err instanceof UserNotFoundError) {
           return reply(errorSerializer.serialize(UserNotFoundError.getErrorMessage()));
         }
+
+        return reply(errorSerializer.serialize(InternalError.getErrorMessage()));
       });
   }
 };
