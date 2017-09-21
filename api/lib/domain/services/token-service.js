7490cea8c3600747b5260b664cc06fe673ac38fb
hypernikao <fenoh@octo.com>
2017-09-21 12:11:25 +0200

    Remove temporarykey generation from token service

--- api/lib/domain/services/token-service.js
+++ api/lib/domain/services/token-service.js
@@ -32,15 +32,8 @@ function extractUserId(token) {
   return user_id;
 }
 
-function generateTemporaryKey() {
-  return jsonwebtoken.sign({
-    data: settings.temporaryKey.payload
-  }, settings.temporaryKey.secret, { expiresIn: settings.temporaryKey.tokenLifespan });
-}
-
 module.exports = {
   createTokenFromUser,
   extractUserId,
   extractTokenFromAuthChain,
-  generateTemporaryKey
 };
