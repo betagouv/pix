12fd2771ffa1c160465ef84e154c62ce4294ed36
Jérémy Buget <jbuget@gmail.com>
2017-10-05 13:07:09 +0000

    Release of bsr-improve-mirage with env integration (via commit hash: d47c7f14979d86933269e8029043f45cd261eba0)

--- live/bsr-improve-mirage/assets/pix-live.js
+++ live/bsr-improve-mirage/assets/pix-live.js
@@ -8515,6 +8515,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.23.0+828da8e5"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.23.0+d47c7f14"});
 }
 //# sourceMappingURL=pix-live.map
