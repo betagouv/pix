9a84539ef2c5359803d3604db4b4952e922ab906
Jérémy Buget <jbuget@gmail.com>
2017-10-09 18:04:54 +0000

    Release of 712-filling-student-number-when-sharing-profile with env integration (via commit hash: 20c2c19b61082e7f78d0b68520095f82f89d66f2)

--- live/712-filling-student-number-when-sharing-profile/assets/pix-live.js
+++ live/712-filling-student-number-when-sharing-profile/assets/pix-live.js
@@ -8529,6 +8529,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.24.0+dedb1722"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.24.0+20c2c19b"});
 }
 //# sourceMappingURL=pix-live.map
