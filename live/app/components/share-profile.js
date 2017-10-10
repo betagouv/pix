4e2f8d9736f1be61ed10ee3f0f94173305ffcfb7
Jérémy Buget <jbuget@gmail.com>
2017-10-10 19:41:04 +0200

    Add style

--- live/app/components/share-profile.js
+++ live/app/components/share-profile.js
@@ -32,13 +32,13 @@ export default Ember.Component.extend({
     if (this.get('_organization.type') === 'PRO') {
       return {
         text1: 'Vous vous apprêtez à transmettre une copie de votre profil Pix à l\'organisation :',
-        text2: 'En cliquant sur le bouton «Envoyer», elle recevra les informations suivantes :',
+        text2: 'En cliquant sur le bouton « Envoyer », elle recevra les informations suivantes :',
         text3: 'Elle ne recevra les évolutions futures de votre profil que si vous le partagez à nouveau.'
       };
     }
     return {
       text1: 'Vous vous apprêtez à transmettre une copie de votre profil Pix à l\'établissement :',
-      text2: 'En cliquant sur le bouton «Envoyer», il recevra les informations suivantes :',
+      text2: 'En cliquant sur le bouton « Envoyer », il recevra les informations suivantes :',
       text3: 'Il ne recevra les évolutions futures de votre profil que si vous le partagez à nouveau.'
     };
   }),
