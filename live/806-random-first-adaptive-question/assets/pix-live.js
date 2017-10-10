62e5e75df2d344f750faa04a77c8416183f19720
Jérémy Buget <jbuget@gmail.com>
2017-10-10 09:46:20 +0000

    Release of 806-random-first-adaptive-question with env integration (via commit hash: ad919a1489bca2e6496bfc520cd00bc99f6029c0)

--- live/806-random-first-adaptive-question/assets/pix-live.js
+++ live/806-random-first-adaptive-question/assets/pix-live.js
@@ -8653,6 +8653,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.23.0+7b5ac232"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.23.0+ad919a14"});
 }
 //# sourceMappingURL=pix-live.map
