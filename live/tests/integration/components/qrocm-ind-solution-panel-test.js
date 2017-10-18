64ffa3aac6a45f42da7883641e5ec036e80ec68b
hypernikao <fenoh@octo.com>
2017-10-18 16:34:54 +0200

    little lint

--- live/tests/integration/components/qrocm-ind-solution-panel-test.js
+++ live/tests/integration/components/qrocm-ind-solution-panel-test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { describe, it, beforeEach } from 'mocha';
+import { beforeEach, describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
