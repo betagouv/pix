41c092c6b59cd39474ce7fbf579a70c039046129
Jérémy Buget <jbuget@gmail.com>
2017-10-24 13:58:33 +0000

    Release of 596-download-csv-with-shared-profiles with env integration (via commit hash: 3734338cfa26200dc975f0aad56bf36aeca517a0)

--- live/596-download-csv-with-shared-profiles/assets/pix-live.js
+++ live/596-download-csv-with-shared-profiles/assets/pix-live.js
@@ -8742,6 +8742,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.24.0+be26d577"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.24.0+3734338c"});
 }
 //# sourceMappingURL=pix-live.map
