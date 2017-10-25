065256178163249231918a75b4608c84b6f044af
Brandone Martins <brandone.martins@gmail.com>
2017-10-25 12:19:14 +0200

    [US-565] implement route assessment resume and make a little change on adapter

--- live/app/adapters/challenge.js
+++ live/app/adapters/challenge.js
@@ -1,6 +1,21 @@
 import ApplicationAdapter from './application';
+import RSVP from 'rsvp';
 
 export default ApplicationAdapter.extend({
+
+  queryNext(store, assessmentId, challengeId) {
+    const currentChallengeSuffix = challengeId ? `/${challengeId}` : '';
+    const url = `${this.host}/${this.namespace}/assessments/${assessmentId}/next${currentChallengeSuffix}`;
+
+    return this.ajax(url, 'GET').then(payload => {
+      let challenge = null;
+      if (payload) {
+        challenge = store.push(payload);
+        return RSVP.resolve(challenge);
+      }
+
+      return RSVP.reject(new Error(`There is no next challenge for assessment ${assessmentId}`));
+    });
 
   queryRecord(store, type, query) {
     let url = `${this.host}/${this.namespace}/assessments/${query.assessmentId}/next`;
