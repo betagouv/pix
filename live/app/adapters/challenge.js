abbfc6d8e54439f6f4399ddaa903906117807de4
Brandone Martins <brandone.martins@gmail.com>
2017-10-27 18:34:19 +0200

    [US-565] change api and adapter front

--- live/app/adapters/challenge.js
+++ live/app/adapters/challenge.js
@@ -1,14 +1,14 @@
+import Ember from 'ember';
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
 
-  // https://guides.emberjs.com/v2.16.0/models/finding-records/#toc_querying-for-a-single-record
-  urlForQueryRecord({ assessmentId, challengeId }) {
-    let url = `${this.host}/${this.namespace}/assessments/${assessmentId}/next`;
-    if (challengeId) {
-      url += `/${challengeId}`;
+  queryRecord(store, type, query) {
+    let url = `${this.host}/${this.namespace}/assessments/${query.assessmentId}/next`;
+    if (query.challengeId) {
+      url += `/${query.challengeId}`;
     }
-    return url;
+    return Ember.$.getJSON(url);
   }
 
 });
