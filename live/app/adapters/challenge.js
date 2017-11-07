7aae6d8c5013ce79b1478f19d12b744cf1343ed9
Brandone Martins <brandone.martins@gmail.com>
2017-11-07 12:37:29 +0100

    fix errors from rebase

--- live/app/adapters/challenge.js
+++ live/app/adapters/challenge.js
@@ -27,4 +27,15 @@ export default ApplicationAdapter.extend({
     return this.ajax(url, 'GET');
   }
 
+  /*
+    // https://guides.emberjs.com/v2.16.0/models/finding-records/#toc_querying-for-a-single-record
+    urlForQueryRecord({ assessmentId, challengeId }) {
+      let url = `${this.host}/${this.namespace}/assessments/${assessmentId}/next`;
+      if (challengeId) {
+        url += `/${challengeId}`;
+      }
+      return url;
+    }
+  */
+
 });
