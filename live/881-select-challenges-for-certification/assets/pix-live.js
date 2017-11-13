caac57879cb2db83fc469b99ba20a05628536c93
Jérémy Buget <jbuget@gmail.com>
2017-11-13 13:52:17 +0000

    Release of 881-select-challenges-for-certification with env integration (via commit hash: badec27b903c4fbee77d132a44a2646a58472708)

--- live/881-select-challenges-for-certification/assets/pix-live.js
+++ live/881-select-challenges-for-certification/assets/pix-live.js
@@ -8747,6 +8747,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.27.0+ca499280"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.27.0+badec27b"});
 }
 //# sourceMappingURL=pix-live.map
