bffb56942fad14354a080082674db988fafdd208
MelanieMEB <mboudard@octo.com>
2017-11-13 10:48:37 +0100

    Put verification into function and remove possibility of three-level error

--- api/lib/infrastructure/converter/snapshots-csv-converter.js
+++ api/lib/infrastructure/converter/snapshots-csv-converter.js
@@ -57,7 +57,7 @@ function _createProfileLine(snapshot) {
 
 function _cleanArrayCompetences(arrayCompetences) {
   return _.sortBy(arrayCompetences, 'attributes.index')
-    .filter(competence => (competence.type === 'competences' && competence.attributes.name))
+    .filter(competence => _verifyCorrectCompetence(competence))
     .map(competence => {
       return {
         name: `"${_cleanCompetenceName(competence.attributes.name)}"`,
@@ -67,6 +67,10 @@ function _cleanArrayCompetences(arrayCompetences) {
     });
 }
 
+function _verifyCorrectCompetence(competence) {
+  return (competence.type && competence.attributes) ?
+    (competence.type === 'competences' && competence.attributes.name) : false;
+}
 function _emptyData(jsonData) {
   return !jsonData[0];
 }
