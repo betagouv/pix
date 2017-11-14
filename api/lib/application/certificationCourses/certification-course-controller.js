a769d9406cca6422c4019706f2a862c526ce9d0a
Brandone Martins <brandone.martins@gmail.com>
2017-11-14 14:47:46 +0100

    call assessment repo in controller

--- api/lib/application/certificationCourses/certification-course-controller.js
+++ api/lib/application/certificationCourses/certification-course-controller.js
@@ -1,5 +1,19 @@
+const CertificationCourseRepository = require('../../infrastructure/repositories/certification-course-repository');
+const AssessmentRepository = require('../../infrastructure/repositories/assessment-repository');
+const AuthorizationToken = require('../../../lib/infrastructure/validators/jsonwebtoken-verify');
+
 module.exports = {
-  save(){
-    return reply();
+  save(request, reply){
+    const userId = request.pre.userId;
+    return CertificationCourseRepository.save()
+      .then((certificateCourse) => {
+        const assessmentCertificate = {
+          type: 'CERTIFICATION',
+          courseId: certificateCourse.id,
+          userId: userId
+        };
+        return AssessmentRepository.save(assessmentCertificate);
+      })
+      .then((assessmentSaved) => console.log(assessmentSaved))
   }
 }
--- api/tests/unit/application/certificationCourses/certification-course-controller_test.js
+++ api/tests/unit/application/certificationCourses/certification-course-controller_test.js
@@ -1,11 +1,68 @@
-describe('', function() {
+const { describe, it, before, expect, sinon } = require('../../../test-helper');
+const Hapi = require('hapi');
+const CertificationCourseController = require('../../../../lib/application/certificationCourses/certification-course-controller');
+const CertificationCourseRepository = require('../../../../lib/infrastructure/repositories/certification-course-repository');
+const AssessmentRepository = require('../../../../lib/infrastructure/repositories/assessment-repository');
 
-  it('', function() {
-    // given
+describe('Unit | Controller | certification-course-controller', function() {
 
-    // when
+  let server;
+  let sandbox;
+  const request = {
+    pre : {
+      userId : 'userId'
+    }
+  };
+  before(function() {
+    server = this.server = new Hapi.Server();
+    server.connection({ port: null });
+    server.register({ register: require('../../../../lib/application/certificationCourses') });
+  });
 
-    // then
+  describe('#save', function() {
+    beforeEach(() => {
+      sandbox = sinon.sandbox.create();
+      sandbox.stub(AssessmentRepository, 'save');
+      sandbox.stub(CertificationCourseRepository, 'save').resolves({id: 'CertificationCourseId'});
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it('should call repository to create certification-course', function() {
+      // when
+      const promise = CertificationCourseController.save(request);
+
+      // then
+      return promise.then(() => {
+        sinon.assert.calledOnce(CertificationCourseRepository.save);
+      })
+    });
+
+    it('should call repository to create assessment for certification-course with correct assessment', function() {
+      // given
+
+      // when
+      const promise = CertificationCourseController.save(request);
+
+      // then
+      return promise.then(() => {
+        sinon.assert.calledOnce(AssessmentRepository.save);
+        sinon.assert.calledWith(AssessmentRepository.save, {type: 'CERTIFICATION', courseId: 'CertificationCourseId', userId: 'userId'});
+      })
+
+    });
+
+    it('should reply the certification-course id and assessment id', function() {
+      // given
+
+      // when
+
+      // then
+
+    });
 
   });
+
 });
