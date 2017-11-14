b362183a06d10eb90538200a88a8a8840320fe0c
Jérémy Buget <jbuget@gmail.com>
2017-11-14 15:52:28 +0000

    Release of 885-evaluation-profile-matching-ratio with env integration (via commit hash: 0905ce42998aedcb66efed2a2aeac7e1cff1e6c5)

--- live/885-evaluation-profile-matching-ratio/assets/pix-live.js
+++ live/885-evaluation-profile-matching-ratio/assets/pix-live.js
@@ -8749,6 +8749,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.27.0+33eda10c"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.27.0+0905ce42"});
 }
 //# sourceMappingURL=pix-live.map
