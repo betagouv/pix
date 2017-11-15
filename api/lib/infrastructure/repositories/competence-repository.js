767b8efd1595955969b7a1f6c324e6ba772f8cf7
Jérémy Buget <jbuget@gmail.com>
2017-11-15 14:13:06 +0100

    Refactor unit/competence-repository and its tests (with add of sinon-chai)

--- api/lib/infrastructure/repositories/competence-repository.js
+++ api/lib/infrastructure/repositories/competence-repository.js
@@ -8,26 +8,18 @@ const cacheKey = 'competence-repository_list';
 module.exports = {
 
   list() {
-    return new Promise((resolve, reject) => {
+    const cachedCompetences = cache.get(cacheKey);
 
-      cache.get(cacheKey, (err, cachedCompetences) => {
-        if(err) {
-          return reject(err);
-        }
+    if (cachedCompetences) {
+      return Promise.resolve(cachedCompetences);
+    }
 
-        if(cachedCompetences) {
-          return resolve(cachedCompetences);
-        }
-
-        airtable.getRecords(AIRTABLE_TABLE_NAME, {}, serializer)
-          .then((competences) => {
-            cache.set(cacheKey, competences);
-            resolve(competences);
-          })
-          .catch(reject);
+    return airtable
+      .getRecords(AIRTABLE_TABLE_NAME, {}, serializer)
+      .then((competences) => {
+        cache.set(cacheKey, competences);
+        return competences;
       });
-
-    });
   },
 
 };
