fafab735458ac58e5943174241b407b7b3575d39
Brandone Martins <brandone.martins@gmail.com>
2017-11-16 17:47:50 +0100

    fix lint

--- api/tests/unit/infrastructure/repositories/certification-challenge-repository_test.js
+++ api/tests/unit/infrastructure/repositories/certification-challenge-repository_test.js
@@ -2,7 +2,6 @@ const { describe, it, expect, sinon, beforeEach, afterEach } = require('../../..
 const certificationChallengeRepository = require('../../../../lib/infrastructure/repositories/certification-challenge-repository');
 const CertificationChallenge = require('../../../../lib/domain/models/data/certification-challenge');
 
-
 describe('Unit | Repository | certification-challenge-repository', () => {
 
   const challengeObject = {
@@ -37,7 +36,7 @@ describe('Unit | Repository | certification-challenge-repository', () => {
     });
 
   });
-  
+
   it('should return certification challenge object', () => {
     // when
     const promise = certificationChallengeRepository.save(challengeObject, certificationCourseObject);
