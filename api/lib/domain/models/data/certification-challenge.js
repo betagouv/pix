a2919a0cd5f97463265a82f8f6c93f9dd7c83708
Brandone Martins <brandone.martins@gmail.com>
2017-11-22 11:07:10 +0100

    add new certification model challenge

--- api/lib/domain/models/data/certification-challenge.js
+++ api/lib/domain/models/data/certification-challenge.js
@@ -1,5 +1,16 @@
 const Bookshelf = require('../../../infrastructure/bookshelf');
+const CertificationChallenge = require('../../../domain/models/CertificationChallenge');
 
 module.exports = Bookshelf.Model.extend({
-  tableName: 'certification-challenges'
+  tableName: 'certification-challenges',
+
+  toDomain() {
+    return new CertificationChallenge({
+      id: this.get('id'),
+      challengeId: this.get('challengeId'),
+      competenceId: this.get('competenceId'),
+      associatedSkill: this.get('associatedSkill'),
+      courseId: this.get('courseId')
+    });
+  }
 });
