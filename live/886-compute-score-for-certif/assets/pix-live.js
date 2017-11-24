b54b5d0fc6636f64a5ba48a9cf60c53069df5a08
Jérémy Buget <jbuget@gmail.com>
2017-11-24 11:15:14 +0000

    Release of 886-compute-score-for-certif with env integration (via commit hash: 3017d9198a2b634d9e571e8bfa8c412f513a049d)

--- live/886-compute-score-for-certif/assets/pix-live.js
+++ live/886-compute-score-for-certif/assets/pix-live.js
@@ -8869,6 +8869,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.28.0+c2c4ba62"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.28.0+3017d919"});
 }
 //# sourceMappingURL=pix-live.map
