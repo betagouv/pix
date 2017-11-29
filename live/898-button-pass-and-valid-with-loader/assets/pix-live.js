55babf1a206dc6d0aee5fd6a98b3c8be06738721
Jérémy Buget <jbuget@gmail.com>
2017-11-29 17:00:56 +0000

    Release of 898-button-pass-and-valid-with-loader with env integration (via commit hash: ff5a7852da3bd59297922c8022fbb8a8f1ee4e24)

--- live/898-button-pass-and-valid-with-loader/assets/pix-live.js
+++ live/898-button-pass-and-valid-with-loader/assets/pix-live.js
@@ -8882,6 +8882,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.28.0+2f7277b0"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.28.0+ff5a7852"});
 }
 //# sourceMappingURL=pix-live.map
