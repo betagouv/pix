985ac8fb49cc88dfa36f6e42428f0848127a8354
Jérémy Buget <jbuget@gmail.com>
2017-12-04 11:09:22 +0000

    Release of 980-bugfix-certificate-skill-without-associated-challenge with env integration (via commit hash: fe95f6011a2ac98b7910bbdf305005195bc75ea8)

--- live/980-bugfix-certificate-skill-without-associated-challenge/assets/pix-live.js
+++ live/980-bugfix-certificate-skill-without-associated-challenge/assets/pix-live.js
@@ -8820,6 +8820,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.29.1+4f588891"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.29.1+fe95f601"});
 }
 //# sourceMappingURL=pix-live.map
