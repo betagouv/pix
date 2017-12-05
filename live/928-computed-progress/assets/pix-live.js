182cda45e0ae505ce5b94d1f54619bdef3752fe2
Jérémy Buget <jbuget@gmail.com>
2017-12-05 15:29:36 +0000

    Release of 928-computed-progress with env integration (via commit hash: 8f97fedf46fafab5ec099da61ac3b67f0efb0555)

--- live/928-computed-progress/assets/pix-live.js
+++ live/928-computed-progress/assets/pix-live.js
@@ -8847,6 +8847,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.29.2+cb270737"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.29.2+8f97fedf"});
 }
 //# sourceMappingURL=pix-live.map
