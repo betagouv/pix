fec42b157d9becb51853a82019d669fd64b5e35b
MelanieMEB <mboudard@octo.com>
2017-12-06 12:36:45 +0100

    Add api route to get Certification-Course with associate Assessment

--- api/tests/unit/infrastructure/serializers/jsonapi/certification-course-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/certification-course-serializer_test.js
@@ -1,5 +1,6 @@
 const { describe, it, expect } = require('../../../../test-helper');
 const serializer = require('../../../../../lib/infrastructure/serializers/jsonapi/certification-course-serializer');
+const Assessment = require('../../../../../lib/domain/models/data/assessment');
 const CertificationCourse = require('../../../../../lib/domain/models/CertificationCourse');
 
 describe('Unit | Serializer | JSONAPI | certification-course-serializer', function() {
@@ -9,7 +10,9 @@ describe('Unit | Serializer | JSONAPI | certification-course-serializer', functi
       id: 'certification_id',
       userId : 2
     });
-
+    const assessment = new Assessment({
+      'id': '2'
+    });
     const jsonCertificationCourse = {
       data: {
         type: 'certification-courses',
@@ -20,6 +23,23 @@ describe('Unit | Serializer | JSONAPI | certification-course-serializer', functi
       }
     };
 
+    const jsonCertificationCourseWithAssessment = {
+      data: {
+        type: 'certification-courses',
+        id: 'certification_id',
+        attributes : {
+          'user-id': '2'
+        },
+        relationships: {
+          assessment: {
+            data: {
+              id: '2',
+              type: 'assessments'
+            }
+          }
+        }
+      }
+    };
     it('should convert a Certification Course model object into JSON API data', function() {
 
       // when
@@ -27,6 +47,16 @@ describe('Unit | Serializer | JSONAPI | certification-course-serializer', functi
 
       // then
       expect(json).to.deep.equal(jsonCertificationCourse);
+
+    });
+
+    it('should convert a Certification Course model with Assessment object into JSON API data', function() {
+
+      // when
+      const json = serializer.serialize({ id: certificationCourse.id, assessment: assessment });
+
+      // then
+      expect(json).to.deep.equal(jsonCertificationCourseWithAssessment);
 
     });
 
