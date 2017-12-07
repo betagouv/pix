50d037b27a166045ab691a61f02cffc873464f6f
Brandone Martins <brandone.martins@gmail.com>
2017-12-07 14:23:30 +0100

    rename snapshot to record

--- api/lib/infrastructure/serializers/jsonapi/course-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/course-serializer.js
@@ -2,17 +2,17 @@ const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 
 module.exports = {
 
-  serialize(snapshots) {
+  serialize(courses) {
     return new JSONAPISerializer('course', {
       attributes: ['name', 'description', 'duration', 'isAdaptive', 'nbChallenges', 'imageUrl'],
-      transform(snapshot) {
-        const course = Object.assign({}, snapshot);
-        if (snapshot.challenges) {
-          course.nbChallenges = snapshot.challenges.length;
+      transform(record) {
+        const course = Object.assign({}, record);
+        if (record.challenges) {
+          course.nbChallenges = record.challenges.length;
         }
         return course;
       }
-    }).serialize(snapshots);
+    }).serialize(courses);
   }
 
 };
