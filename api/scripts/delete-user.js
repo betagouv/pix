a87001c37c0567a38966459cd112e464792035d9
Thomas Wickham <mackwic@gmail.com>
2017-12-08 16:32:23 +0100

    #get_user_id_from_email

--- api/scripts/delete-user.js
+++ api/scripts/delete-user.js
@@ -29,13 +29,13 @@ function main() {
   }
 }
 
-class User {
+class ScriptUserDelete {
   constructor(client) {
     this.client = client;
   }
 
   get_user_id_from_email(email) {
-    this.client.query();
+    return this.client.query(`SELECT id FROM users WHERE email = '${email}'`);
   }
 }
 
@@ -43,30 +43,41 @@ class User {
 
 if (process.env.TEST != null) {
 
-  const { describe, it } = require('mocha');
+  const { describe, it, afterEach, beforeEach } = require('mocha');
   const { expect } = require('chai');
   const sinon = require('sinon');
 
   describe('#get_user_id_from_email', () => {
     const email = 'jean.paul@pix.fr';
     const client = { query() {} };
+    let subject;
+
+    beforeEach(() => {
+      sinon.stub(client, 'query');
+      subject = new ScriptUserDelete(client);
+    });
+
+    afterEach(() => {
+      client.query.restore();
+    });
 
     it('should exists', () => {
       // arrange
-
+      client.query.returns(12);
       // act
-      const userId = get_user_id_from_email(email);
+      const userId = subject.get_user_id_from_email(email);
       // assert
       expect(Number.isInteger(userId)).to.be.true;
     });
 
     it('should call client.query with correct arguments', () => {
       // arrange
-      sinon.stub(client, 'query').returns();
+      const expectedQuery = `SELECT id FROM users WHERE email = '${email}'`;
       // act
-      get_user_id_from_email(email);
+      subject.get_user_id_from_email(email);
       // assert
       sinon.assert.calledOnce(client.query);
+      sinon.assert.calledWith(client.query, expectedQuery);
     });
   });
 
