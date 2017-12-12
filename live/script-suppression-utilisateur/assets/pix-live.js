5ccf02d27a4da34b6e638aaf16731b9dc87986cd
Jérémy Buget <jbuget@gmail.com>
2017-12-12 14:47:19 +0000

    Release of script-suppression-utilisateur with env integration (via commit hash: dbd17d94f18445c4a27ff16b9ae94b9b9e76c41d)

--- live/script-suppression-utilisateur/assets/pix-live.js
+++ live/script-suppression-utilisateur/assets/pix-live.js
@@ -9114,6 +9114,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.32.0+1e935a73"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.32.0+dbd17d94"});
 }
 //# sourceMappingURL=pix-live.map
