f1c259ecf2f8b4caf9460940c2f208db5b207ece
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2017-12-19 16:52:31 +0100

    Moving score computation to the service

--- api/lib/domain/services/certification-service.js
+++ api/lib/domain/services/certification-service.js
@@ -5,6 +5,12 @@ const _ = require('lodash');
 const answerServices = require('./answer-service');
 const AnswerStatus = require('../models/AnswerStatus');
 
+const userService = require('../../../lib/domain/services/user-service');
+const assessmentRepository = require('../../../lib/infrastructure/repositories/assessment-repository');
+const answersRepository = require('../../../lib/infrastructure/repositories/answer-repository');
+const certificationChallengesRepository = require('../../../lib/infrastructure/repositories/certification-challenge-repository');
+const certificationCourseRepository = require('../../infrastructure/repositories/certification-course-repository');
+
 function _computeSumPixFromCompetences(listCompetences) {
   return  _.sumBy(listCompetences, c => c.pixScore);
 }
@@ -71,20 +77,59 @@ function _getCompetenceWithFailedLevel(listCompetences) {
       level: -1 };
   });
 }
+
+function _getResult(listAnswers, listChallenges, listCompetences) {
+  const reproductibilityRate = answerServices.getAnswersSuccessRate(listAnswers);
+  if (reproductibilityRate < minimumReproductibilityRateToBeCertified) {
+    return { listCertifiedCompetences: _getCompetenceWithFailedLevel(listCompetences), totalScore: 0 };
+  }
+
+  const actualPix = _computeSumPixFromCompetences(listCompetences);
+  const answersByCompetences = _enhanceAnswersWithCompetenceId(listAnswers, listChallenges);
+  const pixToRemove = _getMalusPix(answersByCompetences, listCompetences, reproductibilityRate);
+  const listCertifiedCompetences = _getCompetencesWithCertifiedLevel(answersByCompetences, listCompetences, reproductibilityRate);
+  const totalScore = actualPix - pixToRemove;
+
+  return { listCertifiedCompetences, totalScore };
+}
+
 module.exports = {
 
-  getResult(listAnswers, listChallenges, listCompetences) {
-    const reproductibilityRate = answerServices.getAnswersSuccessRate(listAnswers);
-    if (reproductibilityRate < minimumReproductibilityRateToBeCertified) {
-      return { listCertifiedCompetences: _getCompetenceWithFailedLevel(listCompetences), totalScore: 0 };
-    }
+  getCertificationResult(certificationCourseId) {
+    let userId;
+    let dateOfCertification;
+    let listAnswers;
+    let listCertificationChallenges;
+    let certificationCourseStatus;
 
-    const actualPix = _computeSumPixFromCompetences(listCompetences);
-    const answersByCompetences = _enhanceAnswersWithCompetenceId(listAnswers, listChallenges);
-    const pixToRemove = _getMalusPix(answersByCompetences, listCompetences, reproductibilityRate);
-    const listCertifiedCompetences = _getCompetencesWithCertifiedLevel(answersByCompetences, listCompetences, reproductibilityRate);
-    const totalScore = actualPix - pixToRemove;
+    return assessmentRepository.getByCertificationCourseId(certificationCourseId)
+      .then((assessment) => {
+        userId = assessment.get('userId');
+        dateOfCertification = assessment.get('createdAt');
 
-    return { listCertifiedCompetences,  totalScore };
+        return answersRepository.findByAssessment(assessment.get('id'));
+      })
+      .then((answersByAssessments) => {
+        listAnswers = answersByAssessments;
+        return certificationChallengesRepository.findByCertificationCourseId(certificationCourseId);
+      })
+      .then((certificationChallenges) => {
+        listCertificationChallenges = certificationChallenges;
+        return certificationCourseRepository.get(certificationCourseId);
+      })
+      .then((certificationCourse) => {
+        certificationCourseStatus = certificationCourse.status;
+        return userService.getProfileToCertify(userId, dateOfCertification);
+      })
+      .then((listCompetences) => {
+        const testedCompetences = listCompetences.filter(competence => competence.challenges.length > 0);
+
+        const result = _getResult(listAnswers, listCertificationChallenges, testedCompetences);
+        result.createdAt = dateOfCertification;
+        result.userId = userId;
+        result.status = certificationCourseStatus;
+        return result;
+      });
   },
+
 };
