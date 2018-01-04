eab64f12761b2d7c744e42262c7d2bcd263afa0d
Brandone Martins <brandone.martins@gmail.com>
2018-01-04 16:59:04 +0100

    BSR test index certification course controller

--- api/tests/unit/application/certificationCourses/index_test.js
+++ api/tests/unit/application/certificationCourses/index_test.js
@@ -1,5 +1,32 @@
+const { describe, it, before, after, beforeEach, expect, sinon } = require('../../../test-helper');
+const Hapi = require('hapi');
+const certificationCoursesController = require('../../../../lib/application/certificationCourses/certification-course-controller');
+
 describe('Unit | Router | certification-course-router', function() {
 
-  // TODO TEST MY INDEX PLEASE (get certification-courses/id et get certification-courses/id/result)
+  let server;
 
+  beforeEach(() => {
+    server = new Hapi.Server();
+    server.connection({ port: null });
+    server.register({ register: require('../../../../lib/application/certificationCourses') });
+  });
+
+  describe('GET /api/certification-courses/id/result', () => {
+
+    before(() => {
+      sinon.stub(certificationCoursesController, 'getResult').callsFake((request, reply) => reply('ok'));
+    });
+
+    after(() => {
+      certificationCoursesController.getResult.restore();
+    });
+
+    it('should exist', (done) => {
+      return server.inject({ method: 'GET', url: '/api/certification-courses/:id/result' }, (res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
 });
