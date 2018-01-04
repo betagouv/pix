2933df0d8787d83516dbb4a7a97d22f8399edaa5
Jérémy Buget <jbuget@gmail.com>
2018-01-04 10:51:11 +0000

    Release of 986-finished-time-for-certification with env integration (via commit hash: b637a14ff6bad1613b186f29e3bef145287266b9)

--- live/986-finished-time-for-certification/assets/pix-live.js
+++ live/986-finished-time-for-certification/assets/pix-live.js
@@ -9128,6 +9128,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.34.0+9b919bf1"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.34.0+b637a14f"});
 }
 //# sourceMappingURL=pix-live.map
