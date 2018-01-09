fb9063054df61f3733b7fbfc60fcd92288b8c61d
Jérémy Buget <jbuget@gmail.com>
2018-01-09 13:29:39 +0000

    Release of 311-select-randomly-equally-informative-challenges with env integration (via commit hash: e9216b3d67fab41f8556b0a147514bc743d149fa)

--- live/311-select-randomly-equally-informative-challenges/assets/pix-live.js
+++ live/311-select-randomly-equally-informative-challenges/assets/pix-live.js
@@ -9128,6 +9128,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.34.0+8b681bb4"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.34.0+e9216b3d"});
 }
 //# sourceMappingURL=pix-live.map
