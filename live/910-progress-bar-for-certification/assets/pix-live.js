97af3069c69a6528fcd7ce6bf14a733dc04a4e1a
Jérémy Buget <jbuget@gmail.com>
2018-01-15 15:14:55 +0000

    Release of 910-progress-bar-for-certification with env integration (via commit hash: d1e9fd67f979d4e0b354a2679b93ae0c1a2cae50)

--- live/910-progress-bar-for-certification/assets/pix-live.js
+++ live/910-progress-bar-for-certification/assets/pix-live.js
@@ -9132,6 +9132,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.35.0+4c677328"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.35.0+d1e9fd67"});
 }
 //# sourceMappingURL=pix-live.map
