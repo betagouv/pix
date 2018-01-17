445d3949e7a22b7691c0653545aecdc126d492dd
MelanieMEB <mboudard@octo.com>
2018-01-17 10:47:13 +0100

    Display testsFinished in front and round value to avoid decimal

--- live/app/models/snapshot.js
+++ live/app/models/snapshot.js
@@ -2,17 +2,17 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 const { Model, belongsTo, attr } = DS;
-const TOTAL_NUMBER_OF_COMPETENCES = 16;
 
 export default Model.extend({
-  completionPercentage: attr('string'),
+  testsFinished: attr('string'),
   score: attr('number'),
   createdAt: attr('date'),
   organization: belongsTo('organization'),
   user: belongsTo('user'),
   studentCode: attr('string'),
   campaignCode: attr('string'),
-  testsFinished: Ember.computed('completionPercentage', function() {
-    return Math.round(this.get('completionPercentage')*TOTAL_NUMBER_OF_COMPETENCES/100);
-  })
+  numberOfFinishedTests: Ember.computed('testsFinished', function() {
+  return `${Math.round(this.get('testsFinished'))}`;
+})
+
 });
--- live/tests/unit/models/snapshot-test.js
+++ live/tests/unit/models/snapshot-test.js
@@ -1,4 +1,3 @@
-import { run } from '@ember/runloop';
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupModelTest } from 'ember-mocha';
@@ -14,25 +13,5 @@ describe('Unit | Model | snapshot', function() {
     const model = this.subject();
     // var store = this.store();
     expect(model).to.be.ok;
-  });
-
-  describe('@testsFinished', function() {
-    it('should return the number of finished test ', function() {
-      return run(() => {
-        // given
-        const model = this.subject();
-        const TOTAL_COMPETENCES = 16;
-        const numberOfEvaluatedCompetences = 5;
-        const completionPercentage =  Math.round((numberOfEvaluatedCompetences / TOTAL_COMPETENCES) * 100);
-        model.set('completionPercentage', completionPercentage);
-
-        // when
-        const testsFinished = model.get('testsFinished');
-
-        // then
-        expect(testsFinished).to.equal(numberOfEvaluatedCompetences);
-      });
-
-    });
   });
 });
