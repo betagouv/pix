a50a7600262f87488dd805c3cef7d9687be21fd6
MelanieMEB <mboudard@octo.com>
2018-01-18 16:13:51 +0100

    Change migration to have integer column for realised tests

--- api/db/migrations/20180116161332_modify_column_completedPercentage_to_tests_finished_in_snapshots.js
+++ api/db/migrations/20180116161332_modify_column_completedPercentage_to_tests_finished_in_snapshots.js
@@ -1,25 +1,32 @@
 const TABLE_NAME = 'snapshots';
 
 exports.up = function(knex, Promise) {
-  return knex(TABLE_NAME)
-    .update({
-      completionPercentage: knex.raw('ROUND(??*0.16, 0)', ['completionPercentage'])
-    })
-    .then(() => {
-      return knex.schema.table(TABLE_NAME, function(table) {
-        table.renameColumn('completionPercentage', 'testsFinished');
+  return knex.schema.table(TABLE_NAME, function(table){
+    table.integer('testsFinished');
+  }).then(() => {
+    return knex(TABLE_NAME)
+      .update({
+        testsFinished: knex.raw('ROUND(??*0.16, 0)', ['completionPercentage'])
       });
+  }).then(() => {
+    return knex.schema.table(TABLE_NAME, function(table) {
+      table.dropColumn('completionPercentage');
     });
+  });
 };
 
 exports.down = function(knex, Promise) {
-  return knex(TABLE_NAME)
-    .update({
-      testsFinished: knex.raw('??*6.25', ['testsFinished'])
-    })
+  return knex.schema.table(TABLE_NAME, function(table){
+    table.integer('completionPercentage');
+  })
+    .then(() => {
+      return knex(TABLE_NAME)
+        .update({
+          completionPercentage: knex.raw('??*6.25', ['testsFinished'])
+        })})
     .then(() => {
       return knex.schema.table(TABLE_NAME, function(table) {
-        table.renameColumn('testsFinished', 'completionPercentage');
+        table.dropColumn('testsFinished');
       });
     });
 };
