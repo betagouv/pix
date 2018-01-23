8261143197c44d88f7d89e6b208a8b42e6d639ba
MelanieMEB <mboudard@octo.com>
2018-01-23 11:18:49 +0100

    Extract function + add tests

--- api/scripts/get-changelog.js
+++ api/scripts/get-changelog.js
@@ -27,22 +27,33 @@ function displayPullRequest(pr) {
   return `- [#${pr.number}](${pr.html_url}) ${title}\n`;
 }
 
+function filterPullRequest(pullrequests, milestone) {
+  return pullrequests.filter(pr => {
+    if(pr.milestone) return pr.milestone.number === milestone;
+  });
+}
+
+function getHeadOfChangelog(pullrequest) {
+  // VERSION
+  let headOfChangelog = '## '+pullrequest.milestone.title;
+
+  // DATE
+  headOfChangelog +=' ('+moment().format('DD/MM/YYYY')+') \n\n';
+
+  return headOfChangelog;
+}
+
 function main() {
   const milestone = Number(process.argv[2]);
-  let listOfPRForChangeLog = '';
+  let changeLogForMilestone = '';
 
   makeRequest(buildRequestObject())
-    .then((listOfPullRequests)=> {
-      const listOfPullRequestForMilestone = listOfPullRequests.filter(pr => {
-        if(pr.milestone) return pr.milestone.number === milestone;
-      });
-
-      // VERSION
-      listOfPRForChangeLog +='## '+listOfPullRequestForMilestone[0].milestone.title;
-      // DATE
-      listOfPRForChangeLog +=' ('+moment().format('DD/MM/YYYY')+') \n\n';
+    .then((pullRequests)=> {
+      const pullRequestsInMilestone = filterPullRequest(pullRequests, milestone);
+      // HEAD
+      changeLogForMilestone += getHeadOfChangelog(pullRequestsInMilestone[0]);
       // PR
-      listOfPullRequestForMilestone.forEach(pr => listOfPRForChangeLog += displayPullRequest(pr));
+      pullRequestsInMilestone.forEach(pr => changeLogForMilestone += displayPullRequest(pr));
 
       console.log(listOfPRForChangeLog);
     });
@@ -55,33 +66,90 @@ if (process.env.NODE_ENV !== 'test') {
 } else {
   const { describe, it } = require('mocha');
   const { expect } = require('chai');
+  const sinon = require('sinon');
+  describe('GET CHANGELOG', () => {
 
-  describe('displayPullRequest', () => {
-    const expectedLine = '- [#111](http://git/111) [BUGFIX] Résolution du problème de surestimation du niveau (US-389).\n'
+    describe('displayPullRequest', () => {
+      const expectedLine = '- [#111](http://git/111) [BUGFIX] Résolution du problème de surestimation du niveau (US-389).\n'
 
-    it('should return a line with correct format from correct PR name', () => {
-      // given
-      const pullRequest = {
-        title : '[#111] [BUGFIX] Résolution du problème de surestimation du niveau (US-389).',
-        number: 111,
-        html_url : 'http://git/111'
-      };
-      // when
-      const result = displayPullRequest(pullRequest);
-      // then
-      expect(result).to.equal(expectedLine);
+      it('should return a line with correct format from correct PR name', () => {
+        // given
+        const pullRequest = {
+          title : '[#111] [BUGFIX] Résolution du problème de surestimation du niveau (US-389).',
+          number: 111,
+          html_url : 'http://git/111'
+        };
+        // when
+        const result = displayPullRequest(pullRequest);
+        // then
+        expect(result).to.equal(expectedLine);
+      });
+      it('should return a line with correct format from PR name with error', () => {
+        // given
+        const pullRequest = {
+          title : '[#111][BUGFIX] Résolution du problème de surestimation du niveau (US-389) ',
+          number: 111,
+          html_url : 'http://git/111'
+        };
+        // when
+        const result = displayPullRequest(pullRequest);
+        // then
+        expect(result).to.equal(expectedLine);
+      });
     });
-    it('should return a line with correct format from PR name with error', () => {
-      // given
-      const pullRequest = {
-        title : '[#111][BUGFIX] Résolution du problème de surestimation du niveau (US-389) ',
-        number: 111,
-        html_url : 'http://git/111'
-      };
-      // when
-      const result = displayPullRequest(pullRequest);
-      // then
-      expect(result).to.equal(expectedLine);
+
+    describe('filterPullRequest', () => {
+      it('should return only PR with the correct milestone', () => {
+        // given
+        const milestone = 1;
+        const pullRequests = [
+          {
+            title: '[#111] [BUGFIX] TEST (US-11).',
+            milestone: {
+              number: milestone
+            }
+          },
+          {
+            title: '[#222] [BUGFIX] TRUC (US-22).',
+            milestone: {
+              number: 2
+            }
+          },
+        ];
+        const pullRequestsInMilestone = [
+          {
+            title: '[#111] [BUGFIX] TEST (US-11).',
+            milestone: {
+              number: milestone
+            }
+          }];
+        // when
+        const result = filterPullRequest(pullRequests, milestone);
+        // then
+        expect(result).to.deep.equal(pullRequestsInMilestone);
+      });
     });
+
+    describe('getHeadOfChangelog', () => {
+      it('should return the head of changelog in correct value', () => {
+        // given
+        const clock = sinon.useFakeTimers();
+        const headChangelog = '## v1.0.0 (01/01/1970) \n\n';
+        const pullRequestsInMilestone = {
+          title: '[#111] [BUGFIX] TEST (US-11).',
+          milestone: {
+            number: 1,
+            title: 'v1.0.0'
+          }
+        };
+        // when
+        const result = getHeadOfChangelog(pullRequestsInMilestone);
+        // then
+        expect(result).to.equal(headChangelog);
+        clock.restore();
+      });
+    });
+
   });
+
 }
