528976b287cf4915aea06127bc8f29ef224bb896
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-01-23 16:18:45 +0100

    Last modifications on assessment evaluation

--- api/lib/domain/services/assessment-rating-service.js
+++ api/lib/domain/services/assessment-rating-service.js
@@ -1,70 +1,109 @@
+const _ = require('lodash');
+const moment = require('moment');
+
 const Mark = require('../../domain/models/Mark');
 
 const skillService = require('../../domain/services/skills-service');
 const assessmentService = require('../../domain/services/assessment-service');
+const certificationService = require('../../domain/services/certification-service');
 
 const assessmentRepository = require('../../infrastructure/repositories/assessment-repository');
 const courseRepository = require('../../infrastructure/repositories/course-repository');
 const competenceRepository = require('../../infrastructure/repositories/competence-repository');
 const markRepository = require('../../infrastructure/repositories/mark-repository');
+const certificationCourseRepository = require('../../infrastructure/repositories/certification-course-repository');
 
-async function evaluateFromAssessmentId(assessmentId) {
+const { AlreadyRatedAssessmentError } = require('../errors');
 
-  let assessmentWithScore;
-  let evaluatedSkillsInAssessment;
+function _getMarksToSaveForCertificationAssessment(assessmentId) {
+  return Promise
+    .all([competenceRepository.list(), certificationService.calculateCertificationResultByAssessmentId(assessmentId)])
+    .then(([competences, { listCertifiedCompetences }]) => {
 
-  const treatmentChain = await assessmentService.fetchAssessment(assessmentId)
-    .then(({ assessmentPix, skills }) => {
-      assessmentWithScore = assessmentPix;
-      evaluatedSkillsInAssessment = skills;
+      return listCertifiedCompetences.map((certifiedCompetence) => {
+
+        const area_code = _(competences).find((competence) => {
+          return competence.index === certifiedCompetence.index;
+        }).area.code;
+
+        return new Mark({
+          estimatedLevel: certifiedCompetence.level,
+          score: certifiedCompetence.score,
+          area_code,
+          competence_code: certifiedCompetence.index,
+          assessmentId: assessmentId
+        });
+
+      });
+    });
+}
+
+function _getMarksToSaveForPlacementTest(assessmentWithScore) {
+  return courseRepository.get(assessmentWithScore.courseId)
+    .then((course) => {
+      return competenceRepository.get(course.competences[0]);
     })
-    .then(() => assessmentRepository.save(assessmentWithScore))
-    .then(() => skillService.saveAssessmentSkills(evaluatedSkillsInAssessment));
-
-  if(!assessmentService.isPreviewAssessment(assessmentWithScore)
-    && !assessmentService.isDemoAssessment(assessmentWithScore)) {
-    return courseRepository.get(assessmentWithScore.courseId)
-      .then((course) => {
-        return competenceRepository.get(course.competences[0]);
-      })
-      .then(competence => {
-        return markRepository.save(new Mark({
+    .then(competence => {
+      return [
+        new Mark({
           estimatedLevel: assessmentWithScore.estimatedLevel,
           score: assessmentWithScore.pixScore,
           area_code: competence.area.code,
           competence_code: competence.index,
           assessmentId: assessmentWithScore.id
-        }));
-      });
-  }
+        })
+      ];
+    });
+}
 
-  return treatmentChain;
+function evaluateFromAssessmentId(assessmentId) {
+
+  let assessmentWithScore;
+  let evaluatedSkillsInAssessment;
+
+  return assessmentRepository.get(assessmentId)
+    .then((assessment) => {
+
+      if(assessment.isCompleted()) {
+        throw new AlreadyRatedAssessmentError();
+      }
+
+      return assessmentService.fetchAssessment(assessmentId);
+    }).then(({ assessmentPix, skills }) => {
+      assessmentWithScore = assessmentPix;
+      evaluatedSkillsInAssessment = skills;
+    })
+    .then(() => {
+
+      if (assessmentService.isCertificationAssessment(assessmentWithScore)) {
+        return _getMarksToSaveForCertificationAssessment(assessmentWithScore.id);
+      } else if (assessmentService.isPlacementAssessment(assessmentWithScore)) {
+        return _getMarksToSaveForPlacementTest(assessmentWithScore);
+      } else {
+        const noMarksToSaveForPreviewAndDemo = [];
+        return noMarksToSaveForPreviewAndDemo;
+      }
+
+    })
+    .then((marks) => {
+
+      const totalPix = _(marks).map((mark) => mark.score).sum();
+      assessmentWithScore.pixScore = totalPix;
+
+      return Promise.all(marks.map((mark) => markRepository.save(mark)));
+    })
+    .then(() => skillService.saveAssessmentSkills(evaluatedSkillsInAssessment))
+    .then(() => assessmentRepository.save(assessmentWithScore))
+    .then(() => {
+
+      if(assessmentService.isCertificationAssessment(assessmentWithScore)) {
+        return certificationCourseRepository.updateStatus('completed',
+          assessmentWithScore.courseId,
+          moment().toISOString());
+      }
+    });
 }
 
 module.exports = {
   evaluateFromAssessmentId
 };
-
-/*
-if (err instanceof AssessmentEndedError) {
-
-  return assessmentService
-    .fetchAssessment(request.params.id)
-    .then(({ assessmentPix, skills }) => {
-
-      let promise = Promise.resolve();
-      if (assessmentService.isCertificationAssessment(assessmentPix)) {
-        promise = certificationCourseRepository.updateStatus('completed', assessmentPix.courseId);
-      }
-
-      return promise
-        .then(() => assessmentRepository.save(assessmentPix))
-        .then(() => skillsService.saveAssessmentSkills(skills));
-    })
-    .then(() => {
-      throw err;
-    });
-} else {
-  throw err;
-}
-*/
--- api/tests/unit/domain/services/assessment-rating-service_test.js
+++ api/tests/unit/domain/services/assessment-rating-service_test.js
@@ -1,13 +1,16 @@
 const { describe, it, expect, beforeEach, afterEach, sinon } = require('../../../test-helper');
+const faker = require('faker');
 
 const service = require('../../../../lib/domain/services/assessment-rating-service');
 const assessmentService = require('../../../../lib/domain/services/assessment-service');
 const skillsService = require('../../../../lib/domain/services/skills-service');
+const certificationService = require('../../../../lib/domain/services/certification-service');
 
 const assessmentRepository = require('../../../../lib/infrastructure/repositories/assessment-repository');
 const courseRepository = require('../../../../lib/infrastructure/repositories/course-repository');
 const competenceRepository = require('../../../../lib/infrastructure/repositories/competence-repository');
 const markRepository = require('../../../../lib/infrastructure/repositories/mark-repository');
+const certificationCourseRepository = require('../../../../lib/infrastructure/repositories/certification-course-repository');
 
 const Assessment = require('../../../../lib/domain/models/Assessment');
 const Area = require('../../../../lib/domain/models/Area');
@@ -16,7 +19,25 @@ const Mark = require('../../../../lib/domain/models/Mark');
 const AirtableCourse = require('../../../../lib/domain/models/referential/course');
 const Skill = require('../../../../lib/cat/skill');
 
-const { NotFoundError } = require('../../../../lib/domain/errors');
+const { NotFoundError, AlreadyRatedAssessmentError } = require('../../../../lib/domain/errors');
+
+function _buildCompetence(competence_code, area_code) {
+  const competence = new Competence();
+
+  const defaultInfos = {
+    name : faker.random.uuid(),
+    index : `${area_code}.${competence_code}`,
+    areaId : ['recdmN2Exvq2oAPap'],
+    courseId : 'recvNIWtjJRyBCd0P',
+    reference : `${area_code}.${competence_code} bla bla bla`,
+    skills : undefined,
+    area : new Area({ id: 'recdmN2Exvq2oAPap', code: `${area_code}`, title: 'Information et données' })
+  };
+
+  Object.assign(competence, defaultInfos);
+
+  return competence;
+}
 
 describe('Unit | Domain | Services | assessment-rating', () => {
 
@@ -28,10 +49,29 @@ describe('Unit | Domain | Services | assessment-rating', () => {
 
     let evaluatedSkills;
     let course;
-    let competence;
-    let assessmentWithScore;
-    let scoredAsssessment;
     let sandbox;
+
+    let competence11;
+    let competence12;
+    let competence21;
+    let competence22;
+    let listOfAllCompetences = [];
+
+    const assessmentWithScore = new Assessment({
+      id: assessmentId,
+      courseId: assessmentCourseId,
+      userId: 5,
+      estimatedLevel: 2,
+      pixScore: 13,
+      type: 'PLACEMENT'
+    });
+
+    const assessmentWithoutScore = new Assessment({
+      id: assessmentId,
+      courseId: assessmentCourseId,
+      userId: 5,
+      type: 'PLACEMENT'
+    });
 
     beforeEach(() => {
 
@@ -41,28 +81,12 @@ describe('Unit | Domain | Services | assessment-rating', () => {
         failedSkills: _generateFailedSkills()
       };
 
-      assessmentWithScore = new Assessment({
-        id: assessmentId,
-        courseId: assessmentCourseId,
-        userId: 5,
-        estimatedLevel: 2,
-        pixScore: 13,
-      });
+      competence11 = _buildCompetence('1', '1');
+      competence12 = _buildCompetence('1', '2');
+      competence21 = _buildCompetence('2', '1');
+      competence22 = _buildCompetence('2', '2');
 
-      scoredAsssessment = {
-        assessmentPix: assessmentWithScore,
-        skills: evaluatedSkills
-      };
-
-      competence = new Competence();
-      competence.id = competenceId;
-      competence.name = 'Mener une recherche et une veille d’information';
-      competence.index = '1.1';
-      competence.areaId = ['recdmN2Exvq2oAPap'];
-      competence.courseId = 'recvNIWtjJRyBCd0P';
-      competence.reference = '1.1 Mener une recherche et une veille d’information';
-      competence.skills = undefined;
-      competence.area = new Area({ id: 'recdmN2Exvq2oAPap', code: '1', title: 'Information et données' });
+      listOfAllCompetences = [competence11, competence12, competence21, competence22];
 
       course = new AirtableCourse();
       course.id = assessmentCourseId;
@@ -71,12 +95,20 @@ describe('Unit | Domain | Services | assessment-rating', () => {
 
       sandbox = sinon.sandbox.create();
 
-      sandbox.stub(assessmentService, 'fetchAssessment').resolves(scoredAsssessment);
+      sandbox.stub(assessmentService, 'fetchAssessment').resolves({
+        assessmentPix: assessmentWithScore,
+        skills: evaluatedSkills
+      });
       sandbox.stub(assessmentRepository, 'save').resolves();
+      sandbox.stub(assessmentRepository, 'get').resolves(assessmentWithoutScore);
       sandbox.stub(skillsService, 'saveAssessmentSkills').resolves();
       sandbox.stub(courseRepository, 'get').resolves(course);
-      sandbox.stub(competenceRepository, 'get').resolves(competence);
+      sandbox.stub(competenceRepository, 'get').resolves(competence11);
+      sandbox.stub(competenceRepository, 'list').resolves(listOfAllCompetences);
       sandbox.stub(markRepository, 'save').resolves();
+      sandbox.stub(certificationService, 'calculateCertificationResultByAssessmentId').resolves();
+      sandbox.stub(certificationCourseRepository, 'updateStatus').resolves();
+
     });
 
     afterEach(() => {
@@ -85,10 +117,27 @@ describe('Unit | Domain | Services | assessment-rating', () => {
 
     it('should retrieve assessment informations', () => {
       // when
-      service.evaluateFromAssessmentId(assessmentId);
+      const promise = service.evaluateFromAssessmentId(assessmentId);
 
       // then
-      expect(assessmentService.fetchAssessment).to.have.been.calledWith(assessmentId);
+      return promise.then(() => {
+        expect(assessmentService.fetchAssessment).to.have.been.calledWith(assessmentId);
+      });
+    });
+
+    context('when the assessment is already evaluated', () => {
+
+      it('should reject an AlreadyRatedAssessmentError', () => {
+        // given
+        assessmentRepository.get.resolves(assessmentWithScore);
+
+        // when
+        const promise = service.evaluateFromAssessmentId(assessmentId);
+
+        // then
+        return expect(promise).to.be.rejectedWith(AlreadyRatedAssessmentError);
+      });
+
     });
 
     it('should reject a not found error when the placement does not exists', () => {
@@ -190,11 +239,24 @@ describe('Unit | Domain | Services | assessment-rating', () => {
     });
 
     context('when the assessment is a PREVIEW', () => {
-      it('should try to save the related marks', () => {
-        // given
-        assessmentWithScore.courseId = 'nullCourseId';
-        assessmentWithScore.type = 'PREVIEW';
 
+      const assessmentWithScore = new Assessment({
+        id: assessmentId,
+        courseId: 'nullCourseId',
+        userId: 5,
+        estimatedLevel: 2,
+        pixScore: 13,
+        type: 'PREVIEW'
+      });
+
+      beforeEach(() => {
+        assessmentService.fetchAssessment.resolves({
+          assessmentPix: assessmentWithScore,
+          skills: evaluatedSkills
+        });
+      });
+
+      it('should try to save the related marks', () => {
         // when
         const promise = service.evaluateFromAssessmentId(assessmentId);
 
@@ -206,10 +268,24 @@ describe('Unit | Domain | Services | assessment-rating', () => {
     });
 
     context('when the assessment is a DEMO', () => {
-      it('should try to save the related marks', () => {
-        // given
-        assessmentWithScore.type = 'DEMO';
 
+      const assessmentWithScore = new Assessment({
+        id: assessmentId,
+        courseId: 'nullCourseId',
+        userId: 5,
+        estimatedLevel: 0,
+        pixScore: 0,
+        type: 'DEMO'
+      });
+
+      beforeEach(() => {
+        assessmentService.fetchAssessment.resolves({
+          assessmentPix: assessmentWithScore,
+          skills: evaluatedSkills
+        });
+      });
+
+      it('should not try to save the related marks', () => {
         // when
         const promise = service.evaluateFromAssessmentId(assessmentId);
 
@@ -219,8 +295,158 @@ describe('Unit | Domain | Services | assessment-rating', () => {
         });
       });
 
+      it('should not update the certification status', () => {
+        // when
+        const promise = service.evaluateFromAssessmentId(assessmentId);
+
+        // then
+        return promise.then(() => {
+          expect(certificationCourseRepository.updateStatus).not.to.have.been.called;
+        });
+      });
     });
 
+    context('when the assessment is a CERTIFICATION', () => {
+
+      let clock;
+
+      const certificationResults = {
+        listCertifiedCompetences: [{
+          index: '1.1',
+          id: 'competence_1',
+          name: 'Mener une recherche',
+          level: 0,
+          score: 7
+        }, {
+          index: '2.1',
+          id: 'competence_2',
+          name: 'Partager',
+          level: 2,
+          score: 19
+        }, {
+          index: '2.2',
+          id: 'competence_3',
+          name: 'Adapter',
+          level: -1,
+          score: 0
+        }],
+        totalScore: 26
+      };
+
+      const assessmentWithScore = new Assessment({
+        id: assessmentId,
+        courseId: assessmentCourseId,
+        userId: 5,
+        estimatedLevel: 0,
+        pixScore: 0,
+        type: 'CERTIFICATION'
+      });
+
+      beforeEach(() => {
+        assessmentService.fetchAssessment.resolves({
+          assessmentPix: assessmentWithScore,
+          skills: evaluatedSkills
+        });
+        certificationService.calculateCertificationResultByAssessmentId.resolves(certificationResults);
+
+        clock = sinon.useFakeTimers(new Date('2018-02-04T01:00:00.000+01:00'));
+      });
+
+      afterEach(() => clock.restore());
+
+      it('should retrieve all the evaluated competences', () => {
+        // when
+        const promise = service.evaluateFromAssessmentId(assessmentId);
+
+        // then
+        return promise.then(() => {
+          expect(certificationService.calculateCertificationResultByAssessmentId).to.have.been.calledWith(assessmentId);
+        });
+      });
+
+      it('should persists a mark for each evaluated competence', () => {
+        // when
+        const promise = service.evaluateFromAssessmentId(assessmentId);
+
+        // then
+        return promise.then(() => {
+          expect(markRepository.save).to.have.been.calledThrice;
+
+          const firstSavedMark = markRepository.save.firstCall.args;
+          expect(firstSavedMark[0]).to.deep.equal(new Mark({
+            estimatedLevel: 0,
+            score: 7,
+            area_code: '1',
+            competence_code: '1.1',
+            assessmentId: assessmentId
+          }));
+
+          const secondSavedMark = markRepository.save.secondCall.args;
+          expect(secondSavedMark[0]).to.deep.equal(new Mark({
+            estimatedLevel: 2,
+            score: 19,
+            area_code: '2',
+            competence_code: '2.1',
+            assessmentId: assessmentId
+          }));
+
+          const thirdSavedMark = markRepository.save.thirdCall.args;
+          expect(thirdSavedMark[0]).to.deep.equal(new Mark({
+            estimatedLevel: -1,
+            score: 0,
+            area_code: '2',
+            competence_code: '2.2',
+            assessmentId: assessmentId
+          }));
+        });
+      });
+
+      it('should save total score', () => {
+        const expectedAssessment = new Assessment({
+          id: assessmentId,
+          courseId: assessmentCourseId,
+          userId: 5,
+          estimatedLevel: 0,
+          pixScore: 26,
+          type: 'CERTIFICATION'
+        });
+
+        // when
+        const promise = service.evaluateFromAssessmentId(assessmentId);
+
+        // then
+        return promise.then(() => {
+          expect(assessmentRepository.save).to.have.been.calledWith(expectedAssessment);
+
+          const savedAssessment = assessmentRepository.save.firstCall.args;
+          expect(savedAssessment[0]).to.deep.equal(expectedAssessment);
+        });
+      });
+
+      it('should update the certification course status', () => {
+        // when
+        const promise = service.evaluateFromAssessmentId(assessmentId);
+
+        // then
+        return promise.then(() => {
+          expect(certificationCourseRepository.updateStatus).to.have.been.calledWith('completed', assessmentCourseId, '2018-02-04T00:00:00.000Z');
+        });
+      });
+
+      context('when updating the certification course status is failing', () => {
+        it('should return the raised error', () => {
+          // given
+          const error = new Error();
+          certificationCourseRepository.updateStatus.rejects(error);
+
+          // when
+          const promise = service.evaluateFromAssessmentId(assessmentId);
+
+          // then
+          return expect(promise).to.be.rejectedWith(error);
+        });
+      });
+    });
   });
 });
 
