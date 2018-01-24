8c8e1544ee43d15bc5ea9f7f7ee6b1349f7708be
Jérémy Buget <jbuget@gmail.com>
2018-01-24 17:37:22 +0000

    Release of 992-save-profile with env integration (via commit hash: 2ed9018d460e130ea297c7ae4565d32bd665d050)

--- live/992-save-profile/assets/pix-live.js
+++ live/992-save-profile/assets/pix-live.js
@@ -9181,6 +9181,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.36.0+8d82be78"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.36.0+2ed9018d"});
 }
 //# sourceMappingURL=pix-live.map
