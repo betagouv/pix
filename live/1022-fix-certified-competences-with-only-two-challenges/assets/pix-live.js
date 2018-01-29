bc17a6bac91ba67d7d4d5a011afff21bf6b657b6
Jérémy Buget <jbuget@gmail.com>
2018-01-29 16:10:32 +0000

    Release of 1022-fix-certified-competences-with-only-two-challenges with env integration (via commit hash: 50fcdd881ec804c1e7bfc9e382ffb4b81458c9af)

--- live/1022-fix-certified-competences-with-only-two-challenges/assets/pix-live.js
+++ live/1022-fix-certified-competences-with-only-two-challenges/assets/pix-live.js
@@ -9140,6 +9140,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.36.0+3b6dd223"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.36.0+50fcdd88"});
 }
 //# sourceMappingURL=pix-live.map
