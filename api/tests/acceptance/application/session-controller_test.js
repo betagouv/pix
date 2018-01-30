bbd038b7a9c9ce1785382bae92435a17783a43cc
David Riviere <driviere@octo.com>
2018-01-30 18:05:18 +0100

    Add POST endpoint in acceptance/application/session controller test. Red for now

--- api/tests/acceptance/application/session-controller_test.js
+++ api/tests/acceptance/application/session-controller_test.js
@@ -1,13 +1,13 @@
-const { describe, it, after, expect } = require('../../test-helper');
+const {describe, it, after, expect, knex} = require('../../test-helper');
 const server = require('../../../server');
 
-describe('Acceptance | Controller | session-controller', function() {
+describe('Acceptance | Controller | session-controller', function () {
 
-  after(function(done) {
+  after(function (done) {
     server.stop(done);
   });
 
-  describe('GET /sessions', function() {
+  describe('GET /sessions', function () {
 
     const options = {
       method: 'GET', url: '/api/sessions', payload: {}
@@ -33,6 +33,40 @@ describe('Acceptance | Controller | session-controller', function() {
         .then((response) => {
           const code = response.result;
           expect(code).to.have.lengthOf(6);
+        });
+    });
+  });
+
+  describe('POST /sessions', () => {
+    const options = {
+      method: 'POST', url: '/api/sessions', payload: {
+        data: {
+          type: 'sessions',
+          attributes: {
+            'certification-center': 'Université Nice-Sophia-Antipolis',
+            address: 'Nice',
+            room: '28D',
+            examiner: 'Sophie Rapetti',
+            date: '08/12/2017',
+            time: '14:30',
+            description:''
+          }
+        }
+      }
+    };
+
+    it('should return an OK status after saving in database', () => {
+      // when
+      const promise = server.inject(options);
+
+      // then
+      return promise
+        .then((response) => {
+          expect(response.statusCode).to.equal(200);
+        })
+        .then(() => knex('sessions').select())
+        .then((sessions) => {
+          expect(sessions).to.have.lengthOf(1);
         });
     });
   });
