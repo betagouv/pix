6d7518bd7216bbac16ab0aa42dcc015d7ec8d6f7
David Riviere <driviere@octo.com>
2018-01-31 18:00:00 +0100

    Testing the route existence for POST /api/sessions

--- api/lib/application/sessions/session-controller.js
+++ api/lib/application/sessions/session-controller.js
@@ -3,5 +3,9 @@ const sessionService = require('../../domain/services/session-service');
 module.exports = {
   get(request, reply) {
     reply(sessionService.getCurrentCode());
+  },
+
+  save(request, reply) {
+    reply({});
   }
 };
