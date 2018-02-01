02864c450c5cf094ecaaa7b3fad0b324a042353e
Jérémy Buget <jbuget@gmail.com>
2018-02-01 17:00:47 +0000

    Release of 1012-profil-not-admissible-for-certification with env integration (via commit hash: 8299e7a55b4447b1dabd98ca38190f19a5070d60)

--- live/1012-profil-not-admissible-for-certification/assets/pix-live.js
+++ live/1012-profil-not-admissible-for-certification/assets/pix-live.js
@@ -9110,6 +9110,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.37.0+5f521fc5"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.37.0+8299e7a5"});
 }
 //# sourceMappingURL=pix-live.map
