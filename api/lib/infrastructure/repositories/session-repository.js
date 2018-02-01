e4cf6c939f1d8805c66640cfd290122d080a3828
David Riviere <driviere@octo.com>
2018-02-01 13:31:23 +0100

    create repository session with save method

--- api/lib/infrastructure/repositories/session-repository.js
+++ api/lib/infrastructure/repositories/session-repository.js
@@ -1,5 +1,27 @@
+const _ = require('lodash');
+const BookshelfSession = require('../data/session');
+const Session = require('../../domain/models/Session')
+
+function _toDomain(bookshelfSession) {
+  const sessionReturned = bookshelfSession.toJSON();
+  sessionReturned.certificationCenter = sessionReturned['certification-center'];
+
+  return new Session(sessionReturned);
+}
+
+function _fromModelToBookshelf(sessionModel) {
+  sessionModel['certification-center'] = sessionModel.certificationCenter;
+
+  return _.omit(sessionModel, ['certificationCenter']);
+}
+
 module.exports = {
-  save: () => {
-    return Promise.resolve();
+  save: (sessionModel) => {
+
+    const sessionToBeSaved = _fromModelToBookshelf(sessionModel);
+
+    return new BookshelfSession(sessionToBeSaved)
+      .save()
+      .then(_toDomain);
   }
 };
