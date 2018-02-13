b2f341aa3c7ec9f356eac57005830dbb559ba11a
Jérémy Buget <jbuget@gmail.com>
2018-02-13 11:30:53 +0000

    Release of 1069-fix-email-case-insensitive with env integration (via commit hash: 95791fe73b2036e540f1520496d200c4ce99715c)

--- live/1069-fix-email-case-insensitive/assets/pix-live.js
+++ live/1069-fix-email-case-insensitive/assets/pix-live.js
@@ -9110,6 +9110,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.38.0+ece5d421"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.38.0+95791fe7"});
 }
 //# sourceMappingURL=pix-live.map
