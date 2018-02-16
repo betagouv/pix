9e6c9e2f76733a889ad4322f5e8b79254910e42b
MelanieMEB <mboudard@octo.com>
2018-02-16 11:39:21 +0100

    Add correct value for certification

--- api/db/seeds/assessments.js
+++ api/db/seeds/assessments.js
@@ -11,10 +11,8 @@ exports.seed = (knex) => {
       { id:3, courseId: 'recNPB7dTNt5krlMA', createdAt: '2018-02-15 15:03:18', updatedAt: '2018-02-15 15:03:18', userId: 1, estimatedLevel: 5, pixScore: 47, type:'PLACEMENT' },
       { id:4, courseId: 'recR9yCEqgedB0LYQ', createdAt: '2018-02-15 15:04:26', updatedAt: '2018-02-15 15:04:26', userId: 1, estimatedLevel: 4, pixScore: 34, type:'PLACEMENT' },
       { id:5, courseId: 'rec5gEPqhxYjz15eI', createdAt: '2018-02-15 15:07:02', updatedAt: '2018-02-15 15:07:02', userId: 1, estimatedLevel: 5, pixScore: 48, type:'PLACEMENT' },
-      { id:6, courseId: '1', createdAt: '2018-02-15 15:14:46', updatedAt: '2018-02-15 15:14:46', userId: 1, estimatedLevel: 0, pixScore: 0 , type:'CERTIFICATION' }
+      { id:6, courseId: '1', createdAt: '2018-02-15 15:14:46', updatedAt: '2018-02-15 15:14:46', userId: 1, estimatedLevel: 0, pixScore: 157 , type:'CERTIFICATION' }
 
     ]);
-
   });
-
 };
