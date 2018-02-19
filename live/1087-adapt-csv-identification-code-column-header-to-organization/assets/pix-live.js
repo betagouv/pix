b5c0a7d0ef7d29bd22f63ac22331f060b8f4e5d9
Jérémy Buget <jbuget@gmail.com>
2018-02-19 08:58:40 +0000

    Release of 1087-adapt-csv-identification-code-column-header-to-organization with env integration (via commit hash: 97e3b934124526e39a8d3e76e7d9f23a36356242)

--- live/1087-adapt-csv-identification-code-column-header-to-organization/assets/pix-live.js
+++ live/1087-adapt-csv-identification-code-column-header-to-organization/assets/pix-live.js
@@ -9122,6 +9122,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.38.0+ae89d7a2"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.38.0+97e3b934"});
 }
 //# sourceMappingURL=pix-live.map
