efda96cfc65f9b090ce97ce880198bd9f0de493f
Jérémy Buget <jbuget@gmail.com>
2018-02-22 15:07:50 +0000

    Release of 1100-add-review-app-to-trello with env integration (via commit hash: ae6ba6f32d7886bedc55c3a657dbc7091068ac9b)

--- live/1100-add-review-app-to-trello/assets/pix-live.js
+++ live/1100-add-review-app-to-trello/assets/pix-live.js
@@ -9114,6 +9114,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.39.0+4d6b512c"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.39.0+ae6ba6f3"});
 }
 //# sourceMappingURL=pix-live.map
