aad47f4e363d157ff77298afde3eb5a7b6aff3bd
Jérémy Buget <jbuget@gmail.com>
2018-02-23 17:07:26 +0000

    Release of 1029-add-user-information-and-context-in-certification-courses with env integration (via commit hash: 3820e84e9bfdf5c0644e69113632ce820cadbf0d)

--- live/1029-add-user-information-and-context-in-certification-courses/assets/pix-live.js
+++ live/1029-add-user-information-and-context-in-certification-courses/assets/pix-live.js
@@ -9114,6 +9114,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.39.0+707ad49b"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.39.0+3820e84e"});
 }
 //# sourceMappingURL=pix-live.map
