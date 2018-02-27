f6a6691f32f4fa67cc3a9b2a0a2bbde0e9f4a9d3
Quentin Cattez <qcattez@octo.com>
2018-02-27 15:08:39 +0100

    avoid posting several time the same review app url

--- scripts/trello/comment_with_review_app_url.sh
+++ scripts/trello/comment_with_review_app_url.sh
@@ -3,14 +3,18 @@
 set -e
 set -u
 
-API_URL="https://api.trello.com/1"
+API_VERSION="1"
+API_URL="https://api.trello.com/$API_VERSION"
 BOARD_ID="577a53d10c4ba2372878ed14"
-CREDENTIALS="key=$TRELLO_API_KEY&token=$TRELLO_TOKEN"
-
 CARD_SHORT_ID=${CIRCLE_BRANCH%%-*}
-CARD=$(curl "$API_URL/boards/$BOARD_ID/cards/$CARD_SHORT_ID?$CREDENTIALS")
+CREDENTIALS="key=$TRELLO_API_KEY&token=$TRELLO_TOKEN"
+CARD_ID=$(curl "$API_URL/boards/$BOARD_ID/cards/$CARD_SHORT_ID?$CREDENTIALS" | jq .id | tr -d '"')
 
-CARD_ID=$(echo "$CARD" | jq .id | tr -d '"')
+CARD_COMMENTS=$(curl "$API_URL/cards/$CARD_ID/actions?$CREDENTIALS" | jq '.[].data.text')
 REVIEW_APP_URL="http://$CIRCLE_BRANCH.pix-dev.ovh"
-
-curl -X POST "$API_URL/cards/$CARD_ID/actions/comments?text='$REVIEW_APP_URL'&$CREDENTIALS"
+if [[ $CARD_COMMENTS =~ .*$REVIEW_APP_URL.* ]]
+then
+    echo "Review app url already found in card comments. No need to add it again"
+else
+    curl -X POST "$API_URL/cards/$CARD_ID/actions/comments?text='$REVIEW_APP_URL'&$CREDENTIALS"
+fi
