72636f3d3f2798a3ccc0d3bc59191a2130dd6cea
Jérémy Buget <jbuget@gmail.com>
2018-03-05 14:06:28 +0000

    Release of 1101-bug-result-computing-missing-data with env integration (via commit hash: d13cdd3fba21336c9aeba6579d5dbcbb2ec99473)

--- live/1101-bug-result-computing-missing-data/assets/pix-live.js
+++ live/1101-bug-result-computing-missing-data/assets/pix-live.js
@@ -9114,6 +9114,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.40.0+7c9afb2b"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.40.0+d13cdd3f"});
 }
 //# sourceMappingURL=pix-live.map
