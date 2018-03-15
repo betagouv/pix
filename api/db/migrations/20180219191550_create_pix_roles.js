db4a80568d048d426a5d961d3828cce7c0caa5a8
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-03-15 16:42:31 +0100

    [CODE_REVIEW] Modification on tables are done in line not in parallele

--- api/db/migrations/20180219191550_create_pix_roles.js
+++ api/db/migrations/20180219191550_create_pix_roles.js
@@ -1,29 +1,29 @@
-exports.up = (knex, Promise) => {
+exports.up = (knex) => {
 
-  return Promise.all([
-    knex.schema.createTable('pix_roles', (table) => {
+  return knex.schema
+    .createTable('pix_roles', (table) => {
       table.increments('id').primary();
       table.string('name');
-    }),
+    })
+    .then(() => {
+      return knex.schema.createTable('users_pix_roles', (table) => {
+        table.increments('id').primary();
+        table.bigInteger('user_id').references('users.id');
+        table.bigInteger('pix_role_id').references('pix_roles.id');
+      });
+    })
+    .then(() => {
+      const roles = [
+        { name: 'PIX_MASTER' },
+        { name: 'PIX_READER' },
+      ];
 
-    knex.schema.createTable('users_pix_roles', (table) => {
-      table.increments('id').primary();
-      table.bigInteger('user_id').references('users.id');
-      table.bigInteger('pix_role_id').references('pix_roles.id');
-    }),
-  ]).then(() => {
-    const roles = [
-      { name: 'PIX_MASTER' },
-      { name: 'PIX_READER' },
-    ];
-    return knex.batchInsert('pix_roles', roles);
-  });
+      return knex.batchInsert('pix_roles', roles);
+    });
 };
 
-exports.down = (knex, Promise) => {
-
-  return Promise.all([
-    knex.schema.dropTable('pix_roles'),
-    knex.schema.dropTable('users_pix_roles'),
-  ]);
+exports.down = (knex) => {
+  return knex.schema.dropTable('pix_roles').then(() => {
+    return knex.schema.dropTable('users_pix_roles');
+  });
 };
