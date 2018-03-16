5e8e9c6c2f8b195417fa7f3c1148b38fe499c3c9
Jérémy Buget <jbuget@gmail.com>
2018-03-16 08:44:55 +0000

    Release of 1128-fixing-wrong-user-validation-on-account-creation with env integration (via commit hash: 48b573f9f21a36a4e641ffa800c551a78c074eb6)

--- live/1128-fixing-wrong-user-validation-on-account-creation/assets/pix-live.js
+++ live/1128-fixing-wrong-user-validation-on-account-creation/assets/pix-live.js
@@ -9114,6 +9114,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.42.0+418d4b9b"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.43.0+48b573f9"});
 }
 //# sourceMappingURL=pix-live.map
