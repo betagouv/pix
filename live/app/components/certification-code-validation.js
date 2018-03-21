2f3ac5b5c2e6a1fcf99e386907121b879384012e
MelanieMEB <mboudard@octo.com>
2018-03-21 10:33:59 +0100

    Add loader button to certification-code-validation page

--- live/app/components/certification-code-validation.js
+++ live/app/components/certification-code-validation.js
@@ -7,9 +7,11 @@ export default Component.extend({
   store: service(),
 
   accessCode: '',
+  loadingCertification: false,
 
   actions: {
     submit() {
+      this.set('loadingCertification', true);
       return this.get('store').createRecord('course', { accessCode: this.get('accessCode') }).save()
         .then((certificationCourse) => {
           this.get('onSubmit')(certificationCourse);
@@ -17,7 +19,9 @@ export default Component.extend({
         .catch((error) => {
           if (error.errors[0].status === '404') {
             this.set('displayErrorMessage', true);
+            this.set('loadingCertification', false);
           } else {
+            this.set('loadingCertification', false);
             this.get('error')(error);
           }
         });
