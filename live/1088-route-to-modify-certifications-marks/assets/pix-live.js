88da5e8fc274f8dbfe5052cc49e7a4ee1d7422f2
Jérémy Buget <jbuget@gmail.com>
2018-03-23 15:17:39 +0000

    Release of 1088-route-to-modify-certifications-marks with env integration (via commit hash: d958b82a5032500ca26c346ecee189a6588bee1f)

--- live/1088-route-to-modify-certifications-marks/assets/pix-live.js
+++ live/1088-route-to-modify-certifications-marks/assets/pix-live.js
@@ -9151,6 +9151,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.44.0+e3b67d99"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.44.0+d958b82a"});
 }
 //# sourceMappingURL=pix-live.map
