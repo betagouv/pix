bbbeca5a93cbaceb29e43e946e106373e8322166
Jérémy Buget <jbuget@gmail.com>
2018-04-05 20:40:19 +0000

    Release of 1205-improve-authentication-mechanism with env integration (via commit hash: 3aacd3d33e207d4072019335d8c4561331bd32f5)

--- live/1205-improve-authentication-mechanism/assets/pix-live.js
+++ live/1205-improve-authentication-mechanism/assets/pix-live.js
@@ -9153,6 +9153,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.45.0+da457302"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.45.0+3aacd3d3"});
 }
 //# sourceMappingURL=pix-live.map
