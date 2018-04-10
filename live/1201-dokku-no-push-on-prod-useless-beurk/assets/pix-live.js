2ee59df77f0817d7cf1a39f0e6cb9d85470afa66
Jérémy Buget <jbuget@gmail.com>
2018-04-10 14:27:19 +0000

    Release of 1201-dokku-no-push-on-prod-useless-beurk with env integration (via commit hash: 66ed8ee5794cd5ff3b03a1c20e5961b250940c3b)

--- live/1201-dokku-no-push-on-prod-useless-beurk/assets/pix-live.js
+++ live/1201-dokku-no-push-on-prod-useless-beurk/assets/pix-live.js
@@ -9153,6 +9153,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.46.0+a2524cf3"});
+  require("pix-live/app")["default"].create({"API_HOST":"","isChallengeTimerEnable":true,"MESSAGE_DISPLAY_DURATION":1500,"isMobileSimulationEnabled":false,"isTimerCountdownEnabled":true,"isMessageStatusTogglingEnabled":true,"LOAD_EXTERNAL_SCRIPT":true,"GOOGLE_RECAPTCHA_KEY":"6LdPdiIUAAAAADhuSc8524XPDWVynfmcmHjaoSRO","SCROLL_DURATION":800,"name":"pix-live","version":"1.46.0+66ed8ee5"});
 }
 //# sourceMappingURL=pix-live.map
